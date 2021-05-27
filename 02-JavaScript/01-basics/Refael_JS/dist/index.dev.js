5dc79d4791eee1d1a9b72e819ec6cdc2777f4f6d
Rafa91as <refael.assor@gmail.com>
2021-05-27 17:58:23 +0300

    JS

--- 02-JavaScript/01-basics/Refael_JS/dist/index.dev.js
+++ 02-JavaScript/01-basics/Refael_JS/dist/index.dev.js
@@ -1,11 +1,39 @@
 "use strict";
 
-var x = "refael";
-var y = "Hello";
-consol;
+// Simple function
+function Name1() {
+  var arg1 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "";
+  var arg2 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "";
+  var arg3 = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : "";
+  return arg1 + arg2 + arg3;
+}
 
-var nameReturn = function nameReturn() {
-  return y + x;
+var x = Name1("Hello", " , ", "Refael!");
+console.log(x); // anonymous function
+
+var Name2 = function Name2(arg1, arg2, arg3) {
+  return arg1 + arg2 + arg3;
 };
 
-nameReturn();
+var Y = Name2("Hello", " , ", "Refael!");
+console.log(Y); // arrow function
+
+var Name3 = function Name3(arg1, arg2, arg3) {
+  return arg1 + arg2 + arg3;
+};
+
+var Z = Name3("Hello", " , ", "Refael!");
+console.log(Z); // Factorial function
+
+var factor = function factor(num) {
+  if (num === 0 || num === 1) return 1;
+
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+
+  return num;
+};
+
+factor();
+console.log(factor(7));
--- 02-JavaScript/01-basics/Refael_JS/index.js
+++ 02-JavaScript/01-basics/Refael_JS/index.js
@@ -1,7 +1,32 @@
-const x = "refael";
-const y = "Hello";
-consol;
-let nameReturn = () => {
-  return y + x;
+// Simple function
+function Name1(arg1 = "", arg2 = "", arg3 = "") {
+  return arg1 + arg2 + arg3;
+}
+const x = Name1("Hello", " , ", "Refael!");
+console.log(x);
+
+// anonymous function
+let Name2 = function (arg1, arg2, arg3) {
+  return arg1 + arg2 + arg3;
 };
-nameReturn();
+
+const Y = Name2("Hello", " , ", "Refael!");
+console.log(Y);
+
+// arrow function
+const Name3 = (arg1, arg2, arg3) => {
+  return arg1 + arg2 + arg3;
+};
+const Z = Name3("Hello", " , ", "Refael!");
+console.log(Z);
+
+// Factorial function
+const factor = (num) => {
+  if (num === 0 || num === 1) return 1;
+  for (let i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+};
+factor();
+console.log(factor(7));
