504b6b55a9be0cc744ab654722c473ab52d12f91
Jonathan Eliel Nisenbaum <47113013+jnisen@users.noreply.github.com>
2021-05-27 17:01:36 +0300

    js changes

--- 02-JavaScript/Jony/dist/function.dev.js
+++ 02-JavaScript/Jony/dist/function.dev.js
@@ -1,49 +1,64 @@
 "use strict";
 
-var name = "Jonathan"; //
+// Point 1
+var name = "Jonathan";
 
-function myName(name) {
+function firstMethod(name) {
   return "My name is " + name;
 }
 
-console.log(myName(name)); //
+console.log(firstMethod(name)); //Point 2. First method above.
 
-var mySecondName = function mySecondName(name) {
+var secondMethod = function secondMethod(name) {
   return "My name is " + name;
 };
 
-x = mySecondName(name);
+x = secondMethod(name);
 console.log(x); //
 
-var myThirdName = function myThirdName(name) {
+var thirdMethod = function thirdMethod(name) {
   return "My name is " + name;
 };
 
-x = myThirdName(name);
-console.log(x);
+x = thirdMethod(name);
+console.log(x); // Point 3 -- three method's
+
+var number = 3;
 
 function Factorial(number) {
   var result = 1;
 
-  for (var i = 0; i < number; i++) {
-    result = result * (i + 1);
+  for (var i = 1; i < number; i++) {
+    result *= i + 1;
   }
 
   return result;
 }
 
-console.log(Factorial(3));
-/*
-function Factoriala(number){
+console.log(Factorial(number));
 
-    if (number === 0 || number === 1)
+function Factorial2(number) {
+  var r = number;
+
+  if (number === 0) {
     return 1;
+  }
 
-    for(let i=number-1;i >= 1;i--){
-        number = number * i;
-    }
-    return number;
+  for (var i = 1; i < number; i++) {
+    r *= number - i;
+  }
+
+  return r;
 }
 
-console.log(Factoriala(4))
-*/
+console.log(Factorial2(number));
+
+function Factorial3(number) {
+  if (number === 0) {
+    return 1;
+  }
+
+  return number * Factorial3(number - 1);
+}
+
+console.log(Factorial3(number));
--- 02-JavaScript/Jony/function.js
+++ 02-JavaScript/Jony/function.js
@@ -1,60 +1,76 @@
 
+// Point 1
 let name = "Jonathan"
 
-//
-function myName(name){
+
+function firstMethod(name){
     return "My name is " + name;
 }
 
-console.log(myName(name))
+console.log(firstMethod(name))
 
-//
-let mySecondName = function (name){
+
+//Point 2. First method above.
+
+let secondMethod = function (name){
     return "My name is " + name;
 }
 
-x = mySecondName(name);
+x = secondMethod(name);
 console.log(x);
 
 //
 
-let myThirdName = (name) =>{
+let thirdMethod = (name) =>{
     return "My name is " + name;
 }
 
-x = myThirdName(name);
+x = thirdMethod(name);
 console.log(x);
 
 
 
+// Point 3 -- three method's
 
+const number = 3;
 
 function Factorial(number){
-    let result=1;
+    let result = 1;
     
-    for (let i=0; i<number; i++){
-        result = result * (i+1);
+    for (let i = 1; i<number; i++){
+        result *= (i + 1);
     }
     return result;
 }
 
-console.log(Factorial(3))
+console.log(Factorial(number))
 
 
 
+function Factorial2(number){
 
+    let r = number;
 
-/*
-function Factoriala(number){
-
-    if (number === 0 || number === 1)
-    return 1;
-
-    for(let i=number-1;i >= 1;i--){
-        number = number * i;
+    if (number === 0){
+        return 1
     }
-    return number;
+    for (let i=1; i<number;i++){
+        r *= number - i
+    }
+    return r;
 }
 
-console.log(Factoriala(4))
-*/
+console.log(Factorial2(number))
+
+
+function Factorial3(number){
+    if (number === 0){
+        return 1
+    }
+    return number * Factorial3(number - 1)    
+}
+
+console.log(Factorial3(number));
+
+
+
