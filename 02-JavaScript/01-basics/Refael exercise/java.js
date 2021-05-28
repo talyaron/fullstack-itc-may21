959a1029dd2b36324437f282ef3b7022ac552fb8
Rafa91as <refael.assor@gmail.com>
2021-05-28 11:24:06 +0300

    dfg

---
+++ 02-JavaScript/01-basics/Refael exercise/java.js
@@ -0,0 +1,11 @@
+const Highestarray = [1, 7, 18, 16, 20, 89, 58, 101];
+
+console.log(Highestarray.max);
+// function array Math.max() {
+//   return Highestarray.Ma;
+// }
+// console.log(highestNumber);
+
+// const highestNumber =()=>{
+//     for(i)
+// }
