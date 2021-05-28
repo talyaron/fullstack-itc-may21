6e7e3aacf782af0801f792e18525401e80cd0997
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-05-28 10:20:08 +0300

    added anonymous + arrow functions

--- 02-JavaScript/01-basics/exercises/theHighest/Leo&Yaniv/dist/exercise.dev.js
+++ 02-JavaScript/01-basics/exercises/theHighest/Leo&Yaniv/dist/exercise.dev.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// classic
 function theHighest(array) {
   try {
     if (!Array.isArray(array)) {
@@ -45,21 +46,129 @@ function theHighest(array) {
   } catch (error) {
     return error;
   }
-}
+} // anonymous
+
+
+var highestNum = function highestNum(array) {
+  try {
+    if (!Array.isArray(array)) {
+      throw new Error('Please enter an array!');
+    } else {
+      notNumbersCounter = 0;
+      isAllEqual = false;
+
+      for (var i = 0; i < array.length; i++) {
+        if (isNaN(array[i])) {
+          notNumbersCounter++;
+        }
+
+        if (i > 1 && array[i] == array[i - 1]) {
+          isAllEqual = true;
+        } else {
+          isAllEqual = false;
+        }
+      }
+
+      if (notNumbersCounter == array.length) {
+        throw new Error('Your array contains no numbers!');
+      }
+
+      if (isAllEqual == true) {
+        throw new Error('Your array elements are all equal!');
+      }
+    }
+
+    var highest = 0;
+
+    for (var _i2 = 1; _i2 < array.length; _i2++) {
+      if (isNaN(array[_i2])) {
+        _i2++;
+      } else {
+        if (array[_i2] > highest) {
+          highest = array[_i2];
+        }
+      }
+    }
+
+    return 'The highest number in the array is: ' + highest + '!';
+  } catch (error) {
+    return error;
+  }
+}; // arrow
+
+
+var highestInArrow = function highestInArrow(array) {
+  try {
+    if (!Array.isArray(array)) {
+      throw new Error('Please enter an array!');
+    } else {
+      notNumbersCounter = 0;
+      isAllEqual = false;
+
+      for (var i = 0; i < array.length; i++) {
+        if (isNaN(array[i])) {
+          notNumbersCounter++;
+        }
+
+        if (i > 1 && array[i] == array[i - 1]) {
+          isAllEqual = true;
+        } else {
+          isAllEqual = false;
+        }
+      }
+
+      if (notNumbersCounter == array.length) {
+        throw new Error('Your array contains no numbers!');
+      }
+
+      if (isAllEqual == true) {
+        throw new Error('Your array elements are all equal!');
+      }
+    }
+
+    var highest = 0;
+
+    for (var _i3 = 1; _i3 < array.length; _i3++) {
+      if (isNaN(array[_i3])) {
+        _i3++;
+      } else {
+        if (array[_i3] > highest) {
+          highest = array[_i3];
+        }
+      }
+    }
+
+    return 'The highest number in the array is: ' + highest + '!';
+  } catch (error) {
+    return error;
+  }
+};
 
 var arrayToCheck = [8, -1, 4, 200, 'a'];
 console.log(theHighest(arrayToCheck));
+console.log(highestNum(arrayToCheck));
+console.log(highestInArrow(arrayToCheck));
 arrayToCheck = -1;
 console.log(theHighest(arrayToCheck));
+console.log(highestNum(arrayToCheck));
+console.log(highestInArrow(arrayToCheck));
 arrayToCheck = 'This is a string';
 console.log(theHighest(arrayToCheck));
+console.log(highestNum(arrayToCheck));
+console.log(highestInArrow(arrayToCheck));
 arrayToCheck = {
   string: 'This is a string',
   arr: [1, 2, 3],
   "boolean": false
 };
 console.log(theHighest(arrayToCheck));
+console.log(highestNum(arrayToCheck));
+console.log(highestInArrow(arrayToCheck));
 arrayToCheck = ['Hello', 'Leo', 'Yaniv'];
 console.log(theHighest(arrayToCheck));
+console.log(highestNum(arrayToCheck));
+console.log(highestInArrow(arrayToCheck));
 arrayToCheck = [-1, -1, -1, -1, -1];
 console.log(theHighest(arrayToCheck));
+console.log(highestNum(arrayToCheck));
+console.log(highestInArrow(arrayToCheck));
--- 02-JavaScript/01-basics/exercises/theHighest/Leo&Yaniv/exercise.js
+++ 02-JavaScript/01-basics/exercises/theHighest/Leo&Yaniv/exercise.js
@@ -1,4 +1,89 @@
+// classic
 function theHighest(array) {
+    try {
+        if (!Array.isArray(array)) {
+            throw new Error('Please enter an array!');
+        } else {
+            notNumbersCounter = 0;
+            isAllEqual = false;
+            for (let i = 0; i < array.length; i++) {
+                if (isNaN(array[i])) {
+                    notNumbersCounter++;
+                }
+                if ((i > 1) && (array[i] == array[i-1])) {
+                    isAllEqual = true;
+                }
+                else {
+                    isAllEqual = false;
+                }
+            }
+            if (notNumbersCounter == array.length) {
+                throw new Error('Your array contains no numbers!');
+            }
+            if (isAllEqual == true) {
+                throw new Error('Your array elements are all equal!');
+            }
+        }
+
+        let highest = 0;
+
+        for (let i = 1; i < array.length; i++) {
+            if (isNaN(array[i])) {i++;}
+            else {
+                if (array[i] > highest) {highest = array[i];}
+            }
+        }
+
+        return 'The highest number in the array is: ' + highest + '!';
+    } catch (error) {
+        return error;
+    }
+}
+
+// anonymous
+let highestNum = function (array) {
+    try {
+        if (!Array.isArray(array)) {
+            throw new Error('Please enter an array!');
+        } else {
+            notNumbersCounter = 0;
+            isAllEqual = false;
+            for (let i = 0; i < array.length; i++) {
+                if (isNaN(array[i])) {
+                    notNumbersCounter++;
+                }
+                if ((i > 1) && (array[i] == array[i-1])) {
+                    isAllEqual = true;
+                }
+                else {
+                    isAllEqual = false;
+                }
+            }
+            if (notNumbersCounter == array.length) {
+                throw new Error('Your array contains no numbers!');
+            }
+            if (isAllEqual == true) {
+                throw new Error('Your array elements are all equal!');
+            }
+        }
+
+        let highest = 0;
+
+        for (let i = 1; i < array.length; i++) {
+            if (isNaN(array[i])) {i++;}
+            else {
+                if (array[i] > highest) {highest = array[i];}
+            }
+        }
+
+        return 'The highest number in the array is: ' + highest + '!';
+    } catch (error) {
+        return error;
+    }
+}
+
+// arrow
+let highestInArrow = (array) => {
     try {
         if (!Array.isArray(array)) {
             throw new Error('Please enter an array!');
@@ -41,18 +126,30 @@ function theHighest(array) {
 
 let arrayToCheck = [8, -1, 4, 200, 'a'];
 console.log(theHighest(arrayToCheck));
+console.log(highestNum(arrayToCheck));
+console.log(highestInArrow(arrayToCheck));
 
 arrayToCheck = -1;
 console.log(theHighest(arrayToCheck));
+console.log(highestNum(arrayToCheck));
+console.log(highestInArrow(arrayToCheck));
 
 arrayToCheck = 'This is a string';
 console.log(theHighest(arrayToCheck));
+console.log(highestNum(arrayToCheck));
+console.log(highestInArrow(arrayToCheck));
 
 arrayToCheck = {string:'This is a string',arr:[1,2,3],boolean:false};
 console.log(theHighest(arrayToCheck));
+console.log(highestNum(arrayToCheck));
+console.log(highestInArrow(arrayToCheck));
 
 arrayToCheck = ['Hello', 'Leo', 'Yaniv'];
 console.log(theHighest(arrayToCheck));
+console.log(highestNum(arrayToCheck));
+console.log(highestInArrow(arrayToCheck));
 
 arrayToCheck = [-1,-1,-1,-1,-1];
 console.log(theHighest(arrayToCheck));
+console.log(highestNum(arrayToCheck));
+console.log(highestInArrow(arrayToCheck));
