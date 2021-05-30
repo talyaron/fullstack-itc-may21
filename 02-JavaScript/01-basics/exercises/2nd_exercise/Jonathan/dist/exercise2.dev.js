8686df3367b91ca6264f6bde434ba77ffda2b7e4
Jonathan Eliel Nisenbaum <47113013+jnisen@users.noreply.github.com>
2021-05-30 14:30:35 +0300

    three ways

--- 02-JavaScript/01-basics/exercises/2nd_exercise/Jonathan/dist/exercise2.dev.js
+++ 02-JavaScript/01-basics/exercises/2nd_exercise/Jonathan/dist/exercise2.dev.js
@@ -3,15 +3,15 @@
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 //1) create a function for finding the median in an array. (https://www.mathsisfun.com/median.html)
-var arr = [4, 3, 2, 1]; //order the list first
+var arr = [4, 3, 2, 1, 5]; //order the list first
 
 try {
   if (!Array.isArray(arr)) throw new Error('the argument is not an array');
   var sortedArr = arr.sort(function (a, b) {
     return a - b;
-  }); //function getMedium
+  }); //function getMedium // ArrowMethod
 
-  var getMedian = function getMedian(arr) {
+  var getMedianbyFirst = function getMedianbyFirst(arr) {
     try {
       var sumi = 0; //odd situation
 
@@ -43,11 +43,105 @@ try {
     }
   };
 
-  median = getMedian(sortedArr);
-  console.log("The median of the array is ".concat(median));
+  median = getMedianbyFirst(sortedArr);
+  console.log("The median is ".concat(median));
+} catch (error) {
+  console.log(error);
+} //Anonymous Method
+
+
+try {
+  if (!Array.isArray(arr)) throw new Error('the argument is not an array');
+
+  var _sortedArr = arr.sort(function (a, b) {
+    return a - b;
+  }); //function getMedium // ArrowMethod
+
+
+  var getMedianbySecond = function getMedianbySecond(arr) {
+    try {
+      var sumi = 0; //odd situation
+
+      if (arr.length % 2 !== 0) {
+        for (var i = 0; i < arr.length; i++) {
+          if (typeof arr[i] !== 'number') throw new Error('an element is not a number');
+          sumi += i;
+        }
+
+        var count = arr.length;
+        var medium = arr[sumi / count];
+        return medium;
+      } else {
+        //even situation  
+        for (var _i2 = 0; _i2 < arr.length; _i2++) {
+          if (typeof arr[_i2] !== 'number') throw new Error('an element is not a number');
+          sumi += _i2;
+        }
+
+        var _count2 = arr.length;
+
+        var _medium2 = (arr[Math.round(sumi / _count2) - 1] + arr[Math.round(sumi / _count2)]) / 2;
+
+        return _medium2;
+      }
+    } catch (error) {
+      console.log(error);
+      return 0;
+    }
+  };
+
+  median = getMedianbySecond(_sortedArr);
+  console.log("The median is ".concat(median));
+} catch (error) {
+  console.log(error);
+} // Simple Method
+
+
+try {
+  //function getMedium // ArrowMethod
+  var getMedianbyThird = function getMedianbyThird(arr) {
+    try {
+      var sumi = 0; //odd situation
+
+      if (arr.length % 2 !== 0) {
+        for (var i = 0; i < arr.length; i++) {
+          if (typeof arr[i] !== 'number') throw new Error('an element is not a number');
+          sumi += i;
+        }
+
+        var count = arr.length;
+        var medium = arr[sumi / count];
+        return medium;
+      } else {
+        //even situation  
+        for (var _i3 = 0; _i3 < arr.length; _i3++) {
+          if (typeof arr[_i3] !== 'number') throw new Error('an element is not a number');
+          sumi += _i3;
+        }
+
+        var _count3 = arr.length;
+
+        var _medium3 = (arr[Math.round(sumi / _count3) - 1] + arr[Math.round(sumi / _count3)]) / 2;
+
+        return _medium3;
+      }
+    } catch (error) {
+      console.log(error);
+      return 0;
+    }
+  };
+
+  if (!Array.isArray(arr)) throw new Error('the argument is not an array');
+
+  var _sortedArr2 = arr.sort(function (a, b) {
+    return a - b;
+  });
+
+  console.log("The median is " + getMedianbyThird(_sortedArr2));
 } catch (error) {
   console.log(error);
 } //2) create a function that take an object and return an array with the values of the object in an array.
+//Arrow
 
 
 var obj = {
@@ -62,7 +156,7 @@ var obj = {
   f: 'e'
 };
 
-var valuesArray = function valuesArray(obj) {
+var firstArray = function firstArray(obj) {
   try {
     if (_typeof(obj) !== "object") throw new Error('the argument is not an object');
     if (Array.isArray(obj)) throw new Error('the argument is an array and not an object');
@@ -79,7 +173,47 @@ var valuesArray = function valuesArray(obj) {
   }
 };
 
-x = valuesArray(obj);
+x = firstArray(obj);
+console.log(x); //Anonymous
+
+var secondArray = function secondArray(obj) {
+  try {
+    if (_typeof(obj) !== "object") throw new Error('the argument is not an object');
+    if (Array.isArray(obj)) throw new Error('the argument is an array and not an object');
+    var _arr2 = [];
+
+    for (var i in obj) {
+      _arr2.push(obj[i]);
+    }
+
+    return _arr2;
+  } catch (error) {
+    console.log(error);
+    return 0;
+  }
+};
+
+x = secondArray(obj);
+console.log(x); //Simple
+
+function thirdArray(obj) {
+  try {
+    if (_typeof(obj) !== "object") throw new Error('the argument is not an object');
+    if (Array.isArray(obj)) throw new Error('the argument is an array and not an object');
+    var _arr3 = [];
+
+    for (var i in obj) {
+      _arr3.push(obj[i]);
+    }
+
+    return _arr3;
+  } catch (error) {
+    console.log(error);
+    return 0;
+  }
+}
+
+x = thirdArray(obj);
 console.log(x);
 /*
 
--- 02-JavaScript/01-basics/exercises/2nd_exercise/Jonathan/exercise2.js
+++ 02-JavaScript/01-basics/exercises/2nd_exercise/Jonathan/exercise2.js
@@ -1,6 +1,6 @@
 //1) create a function for finding the median in an array. (https://www.mathsisfun.com/median.html)
 
-let arr = [4,3,2,1]
+let arr = [4,3,2,1,5]
 
 //order the list first
 
@@ -8,15 +8,13 @@ try {
 
     if (!Array.isArray(arr))  throw new Error('the argument is not an array')
 
-
     let sortedArr = arr.sort((a, b) => {
         return a - b;
     })
 
+    //function getMedium // ArrowMethod
 
-    //function getMedium
-
-    let getMedian = (arr) => {
+    let getMedianbyFirst = (arr) => {
 
         try {
 
@@ -49,20 +47,128 @@ try {
         }
     }
 
-    median = getMedian(sortedArr)
-    console.log(`The median of the array is ${median}`);
+    median = getMedianbyFirst(sortedArr)
+    console.log(`The median is ${median}`);
 
 } catch(error) {
-
     console.log(error);
 }
+
+
+//Anonymous Method
+
+try {
+
+    if (!Array.isArray(arr))  throw new Error('the argument is not an array')
+
+    let sortedArr = arr.sort((a, b) => {
+        return a - b;
+    })
+
+    //function getMedium // ArrowMethod
+
+    let getMedianbySecond = function(arr){
+
+        try {
+
+            let sumi = 0;
+
+            //odd situation
+            if (arr.length % 2 !== 0) {
+                for (let i = 0; i < arr.length; i++) {
+                    if (typeof arr[i] !== 'number') throw new Error('an element is not a number')
+                    sumi += i;
+                }
+                const count = arr.length;
+                const medium = arr[sumi / count]
+                return medium;
+
+            } else {
+                //even situation  
+                for (let i = 0; i < arr.length; i++) {
+                    if (typeof arr[i] !== 'number') throw new Error('an element is not a number')
+                    sumi += i;
+                }
+                const count = arr.length;
+                const medium = (arr[Math.round(sumi / count) - 1] + arr[Math.round(sumi / count)]) / 2
+                return medium;
+            }
+        }
+        catch (error) {
+            console.log(error);
+            return 0;
+        }
+    }
+
+    median = getMedianbySecond(sortedArr)
+    console.log(`The median is ${median}`);
+
+} catch(error) {
+    console.log(error);
+}
+
+// Simple Method
+
+try {
+
+    if (!Array.isArray(arr))  throw new Error('the argument is not an array')
+
+    let sortedArr = arr.sort((a, b) => {
+        return a - b;
+    })
+
+    //function getMedium // ArrowMethod
+
+    function getMedianbyThird(arr){
+
+        try {
+
+            let sumi = 0;
+
+            //odd situation
+            if (arr.length % 2 !== 0) {
+                for (let i = 0; i < arr.length; i++) {
+                    if (typeof arr[i] !== 'number') throw new Error('an element is not a number')
+                    sumi += i;
+                }
+                const count = arr.length;
+                const medium = arr[sumi / count]
+                return medium;
+
+            } else {
+                //even situation  
+                for (let i = 0; i < arr.length; i++) {
+                    if (typeof arr[i] !== 'number') throw new Error('an element is not a number')
+                    sumi += i;
+                }
+                const count = arr.length;
+                const medium = (arr[Math.round(sumi / count) - 1] + arr[Math.round(sumi / count)]) / 2
+                return medium;
+            }
+        }
+        catch (error) {
+            console.log(error);
+            return 0;
+        }
+    }
+
+    console.log("The median is " +  getMedianbyThird(sortedArr));
+
+} catch(error) {
+    console.log(error);
+}
+
+
+
 
 //2) create a function that take an object and return an array with the values of the object in an array.
 
 
+//Arrow
+
 let obj = { a: { a: 1, b: 2 }, b: [1, 2], c: 2, d: false, e: true, f: 'e' }
 
-let valuesArray = (obj) => {
+let firstArray = (obj) => {
 
     try {
 
@@ -82,9 +188,58 @@ let valuesArray = (obj) => {
     }
 }
 
-x = valuesArray(obj)
+x = firstArray(obj)
 console.log(x)
 
+//Anonymous
+
+let secondArray = (obj) => {
+
+    try {
+
+        if (typeof (obj) !== "object") throw new Error('the argument is not an object')
+
+        if (Array.isArray(obj)) throw new Error('the argument is an array and not an object')
+
+        let arr = []
+
+        for (let i in obj) {
+            arr.push(obj[i])
+        }
+        return arr
+    } catch (error) {
+        console.log(error);
+        return 0
+    }
+}
+
+x = secondArray(obj)
+console.log(x)
+
+//Simple
+
+function thirdArray (obj){
+
+    try {
+
+        if (typeof (obj) !== "object") throw new Error('the argument is not an object')
+
+        if (Array.isArray(obj)) throw new Error('the argument is an array and not an object')
+
+        let arr = []
+
+        for (let i in obj) {
+            arr.push(obj[i])
+        }
+        return arr
+    } catch (error) {
+        console.log(error);
+        return 0
+    }
+}
+
+x = thirdArray(obj)
+console.log(x)
 
 
 /*
