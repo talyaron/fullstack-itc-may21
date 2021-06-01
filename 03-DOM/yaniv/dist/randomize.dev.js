69d34b4c0bee4af42f412e38f3e09741c0dc1cb5
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-06-01 15:19:04 +0300

    2nd approach added

--- 03-DOM/yaniv/dist/randomize.dev.js
+++ 03-DOM/yaniv/dist/randomize.dev.js
@@ -1,16 +1,16 @@
 "use strict";
 
+// setInterval(function(){boxesNum1();}, 5000);
 setInterval(function () {
-  boxesNum();
-}, 5000);
+  boxesNum2();
+}, 5000); // approach 1
 
-function boxesNum() {
+function boxesNum1() {
   try {
-    var body = document.querySelector("body");
-
-    if (body === undefinded) {
-      throw new Error("What kind of a HTML document doe'st have a body?!");
-    }
+    var body = document.querySelector("body"); // debugger;
+    // if (body === undefinded) { // not really possible - if not in HTML - the <body> tag is added automatically by the browser
+    //     throw new Error(`What kind of a HTML document doe'st have a body?!`);
+    // }
 
     var howManyBoxes = Math.ceil(Math.random() * 10);
     var bodyHtmlCode = "<div class='container'><h1>Number of boxes: ".concat(howManyBoxes, "</h1></div>");
@@ -40,4 +40,53 @@ function boxPosition() {
   var boxTop = "".concat(Math.ceil(Math.random() * 90), "vh");
   var boxLeft = "".concat(Math.ceil(Math.random() * 90), "vw");
   return "top:".concat(boxTop, ";left:").concat(boxLeft, ";");
+} // approach 2
+
+
+function boxesNum2() {
+  try {
+    var body = document.querySelector("body");
+    var howManyBoxes = Math.ceil(Math.random() * 10);
+    body.innerHTML = "<div class='container'><h1>Number of boxes: ".concat(howManyBoxes, "</h1></div>");
+    var newBox;
+
+    for (var i = 1; i <= howManyBoxes; i++) {
+      newBox = document.createElement("div");
+      newBox.setAttribute("id", "box".concat(i));
+      newBox.style.height = boxHeigt2();
+      newBox.style.width = boxWidth2();
+      newBox.style.backgroundColor = boxColor2();
+      newBox.style.top = boxTop2();
+      newBox.style.left = boxLeft2();
+      newBox.innerHTML = "<p>".concat(i, "</p>");
+      body.appendChild(newBox);
+    }
+  } catch (e) {
+    return e;
+  }
+}
+
+function boxHeigt2() {
+  var boxHeight = "".concat(Math.ceil(Math.random() * 160) + 40, "px");
+  return boxHeight;
+}
+
+function boxWidth2() {
+  var boxWidth = "".concat(Math.ceil(Math.random() * 160) + 40, "px");
+  return boxWidth;
+}
+
+function boxColor2() {
+  var boxBg = "rgb(".concat(Math.ceil(Math.random() * 255), ",").concat(Math.ceil(Math.random() * 255), ",").concat(Math.ceil(Math.random() * 255), ")");
+  return boxBg;
+}
+
+function boxTop2() {
+  var boxTop = "".concat(Math.ceil(Math.random() * 90), "vh");
+  return boxTop;
+}
+
+function boxLeft2() {
+  var boxLeft = "".concat(Math.ceil(Math.random() * 90), "vw");
+  return boxLeft;
 } // I don't really see which errors can occure here..
--- 03-DOM/yaniv/randomize.js
+++ 03-DOM/yaniv/randomize.js
@@ -1,11 +1,15 @@
-setInterval(function(){boxesNum();}, 5000);
+// setInterval(function(){boxesNum1();}, 5000);
+setInterval(function(){boxesNum2();}, 5000);
 
-function boxesNum() {
+
+// approach 1
+function boxesNum1() {
     try {
         const body = document.querySelector(`body`);
-        if (body === undefinded) {
-            throw new Error(`What kind of a HTML document doe'st have a body?!`);
-        }
+        // debugger;
+        // if (body === undefinded) { // not really possible - if not in HTML - the <body> tag is added automatically by the browser
+        //     throw new Error(`What kind of a HTML document doe'st have a body?!`);
+        // }
         const howManyBoxes = Math.ceil(Math.random() * 10);
         let bodyHtmlCode = `<div class='container'><h1>Number of boxes: ${howManyBoxes}</h1></div>`;
         for (let i = 1; i <= howManyBoxes; i++) {
@@ -32,6 +36,54 @@ function boxPosition() {
     const boxTop = `${Math.ceil(Math.random() * 90)}vh`;
     const boxLeft = `${Math.ceil(Math.random() * 90)}vw`;
     return `top:${boxTop};left:${boxLeft};`
+}
+
+// approach 2
+function boxesNum2() {
+    try {
+        const body = document.querySelector(`body`);
+        const howManyBoxes = Math.ceil(Math.random() * 10);
+        body.innerHTML = `<div class='container'><h1>Number of boxes: ${howManyBoxes}</h1></div>`;
+        let newBox;
+        for (let i = 1; i <= howManyBoxes; i++) {
+            newBox = document.createElement(`div`);
+            newBox.setAttribute(`id`,`box${i}`);
+            newBox.style.height = boxHeigt2();
+            newBox.style.width = boxWidth2();
+            newBox.style.backgroundColor = boxColor2();
+            newBox.style.top = boxTop2();
+            newBox.style.left = boxLeft2();
+            newBox.innerHTML = `<p>${i}</p>`;
+            body.appendChild(newBox);
+        }
+    } catch (e) {
+        return e;
+    }
+}
+
+function boxHeigt2() {
+    const boxHeight = `${Math.ceil(Math.random() * 160) + 40}px`;
+    return boxHeight;
+}
+
+function boxWidth2() {
+    const boxWidth = `${Math.ceil(Math.random() * 160) + 40}px`;
+    return boxWidth;
+}
+
+function boxColor2() {
+    const boxBg = `rgb(${Math.ceil(Math.random()*255)},${Math.ceil(Math.random()*255)},${Math.ceil(Math.random()*255)})`;
+    return boxBg;
+}
+
+function boxTop2() {
+    const boxTop = `${Math.ceil(Math.random() * 90)}vh`;
+    return boxTop;
+}
+
+function boxLeft2() {
+    const boxLeft = `${Math.ceil(Math.random() * 90)}vw`;
+    return boxLeft;
 }
 
 // I don't really see which errors can occure here..
