87cf21fb3bfa242ea9013b3498415dbd4dae31ab
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-06-02 20:36:50 +0300

    ready

--- 04-events/tal/exercise01/yaniv/dist/event_box.dev.js
+++ 04-events/tal/exercise01/yaniv/dist/event_box.dev.js
@@ -1,27 +1,27 @@
 "use strict";
 
 // create a box, and put it in a random place on the screen.
-creatBox();
+// every time an event on the object (of your chosing happen), move it to a new random place on the screen.
+// https://www.w3schools.com/jsref/dom_obj_event.asp
+var box = document.querySelector('.box');
+console.log(box);
+box.addEventListener('mouseenter', function (event) {
+  var myBox = event.target;
 
-function creatBox() {
   try {
-    var body = document.querySelector("body");
-    var bodyHtmlCode = "<div class='box' style='".concat(boxPosition(), "'><p>Catch me if you can!!</p></div>");
-    body.innerHTML = bodyHtmlCode;
+    myBox.style.top = boxTop();
+    myBox.style.left = boxLeft();
   } catch (e) {
     return e;
   }
+});
+
+function boxTop() {
+  var boxTop = "".concat(Math.ceil(Math.random() * 90), "vh");
+  return boxTop;
 }
 
-function boxPosition() {
-  var boxTop = "".concat(Math.ceil(Math.random() * 90), "vh");
+function boxLeft() {
   var boxLeft = "".concat(Math.ceil(Math.random() * 90), "vw");
-  return "top:".concat(boxTop, ";left:").concat(boxLeft, ";");
-} // every time an event on the object (of your chosing happen), move it to a new random place on the screen.
-// https://www.w3schools.com/jsref/dom_obj_event.asp
-
-
-var box = document.querySelector('.box');
-box.addEventListener('mouseenter', function (event) {
-  creatBox();
-});
+  return boxLeft;
+}
--- 04-events/tal/exercise01/yaniv/event_box.js
+++ 04-events/tal/exercise01/yaniv/event_box.js
@@ -1,28 +1,26 @@
 // create a box, and put it in a random place on the screen.
-
-creatBox();
-
-function creatBox() {
-    try {
-        const body = document.querySelector(`body`);
-        const bodyHtmlCode = `<div class='box' style='${boxPosition()}'><p>Catch me if you can!!</p></div>`;
-        body.innerHTML = bodyHtmlCode;
-    } catch (e) {
-        return e;
-    }
-}
-
-function boxPosition() {
-    const boxTop = `${Math.ceil(Math.random() * 90)}vh`;
-    const boxLeft = `${Math.ceil(Math.random() * 90)}vw`;
-    return `top:${boxTop};left:${boxLeft};`
-}
-
 // every time an event on the object (of your chosing happen), move it to a new random place on the screen.
 // https://www.w3schools.com/jsref/dom_obj_event.asp
 
-const box = document.querySelector('.box');
 
+const box = document.querySelector('.box');
+console.log(box);
 box.addEventListener('mouseenter',event=>{
-    creatBox();
-})
+    const myBox = event.target;
+    try {
+        myBox.style.top = boxTop();
+        myBox.style.left = boxLeft();
+    } catch (e) {
+        return e;
+    }
+});
+    
+    function boxTop() {
+        const boxTop = `${Math.ceil(Math.random() * 90)}vh`;
+        return boxTop;
+    }
+    
+    function boxLeft() {
+        const boxLeft = `${Math.ceil(Math.random() * 90)}vw`;
+        return boxLeft;
+    }
