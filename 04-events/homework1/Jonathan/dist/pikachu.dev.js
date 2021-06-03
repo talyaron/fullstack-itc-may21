302ab826c527c90f88473e8651acef545a4a16ee
Jonathan Eliel Nisenbaum <47113013+jnisen@users.noreply.github.com>
2021-06-03 23:25:15 +0300

    pika

--- 04-events/homework1/Jonathan/dist/pikachu.dev.js
+++ 04-events/homework1/Jonathan/dist/pikachu.dev.js
@@ -1,63 +1,77 @@
 "use strict";
 
-var pikachu = document.querySelector('.pikachu'); //ver como lograr que sea prederminado por el windows
+pikachuMove();
 
-pikachu.style.position = "relative";
-pikachu.style.left = "500px";
-pikachu.style.top = "200px"; //Get Position X and Y
+function pikachuMove() {
+  try {
+    var pikachu = document.querySelector('.pikachu');
+    if (!pikachu) throw new Error("The image pikachu running can not uploaded"); //ver como lograr que sea prederminado por el windows
 
-var PositionX = parseInt(pikachu.style.left);
-var PositionY = parseInt(pikachu.style.top); //Get speed of moving
+    pikachu.style.position = "relative";
+    pikachu.style.left = "500px";
+    pikachu.style.top = "200px"; //Get Position X and Y
 
-var speed = 10; //Movement with the keyboard
+    var PositionX = parseInt(pikachu.style.left);
+    var PositionY = parseInt(pikachu.style.top); //Get speed of moving
 
-document.addEventListener('keydown', function (ev) {
-  if (ev.key == "ArrowDown") {
-    pikachu.style.top = "".concat(PositionY + speed, "px");
-    PositionY = parseInt(pikachu.style.top);
-    pikachu.style.transform = "rotate(90deg)";
-    console.log(PositionY);
+    var speed = 10; //Movement with the keyboard
 
-    if (PositionY >= 400) {
-      PositionY = 400;
-      pikachu.style.transform = "rotate(270deg)";
-    }
-  } else if (ev.key == "ArrowUp") {
-    pikachu.style.top = "".concat(PositionY - speed, "px");
-    PositionY = parseInt(pikachu.style.top);
-    pikachu.style.transform = "rotate(270deg)";
+    document.addEventListener('keydown', function (ev) {
+      if (!ev) throw new Error("the event keydown does not work");
 
-    if (PositionY <= 15) {
-      PositionY = 15;
-      pikachu.style.transform = "rotate(90deg)";
-    }
-  } else if (ev.key == "ArrowLeft") {
-    pikachu.style.left = "".concat(PositionX - speed, "px");
-    PositionX = parseInt(pikachu.style.left);
-    pikachu.style.transform = "rotate(180deg)";
+      if (ev.key == "ArrowDown") {
+        pikachu.style.top = "".concat(PositionY + speed, "px");
+        PositionY = parseInt(pikachu.style.top); //pikachu.style.transform = "rotate(90deg)"
 
-    if (PositionX <= 15) {
-      PositionX = 15;
-      pikachu.style.transform = "rotate(0deg)";
-    }
-  } else if (ev.key == "ArrowRight") {
-    pikachu.style.left = "".concat(PositionX + speed, "px");
-    PositionX = parseInt(pikachu.style.left);
-    pikachu.style.transform = "rotate(0deg)";
+        pikachu.style.transform = "rotateZ(90deg)";
 
-    if (PositionX >= 1150) {
-      PositionX = 1150;
-      pikachu.style.transform = "rotate(180deg)";
-    }
+        if (PositionY >= 400) {
+          PositionY = 400;
+          pikachu.style.transform = "rotate(270deg)";
+        }
+      } else if (ev.key == "ArrowUp") {
+        pikachu.style.top = "".concat(PositionY - speed, "px");
+        PositionY = parseInt(pikachu.style.top);
+        pikachu.style.transform = "rotate(270deg)";
+
+        if (PositionY <= 15) {
+          PositionY = 15;
+          pikachu.style.transform = "rotateZ(90deg)";
+        }
+      } else if (ev.key == "ArrowLeft") {
+        pikachu.style.left = "".concat(PositionX - speed, "px");
+        PositionX = parseInt(pikachu.style.left); //pikachu.style.transform = "rotate(180deg)"
+
+        pikachu.style.transform = "rotateY(180deg)";
+
+        if (PositionX <= 15) {
+          PositionX = 15;
+          pikachu.style.transform = "rotate(0deg)";
+        }
+      } else if (ev.key == "ArrowRight") {
+        pikachu.style.left = "".concat(PositionX + speed, "px");
+        PositionX = parseInt(pikachu.style.left);
+        pikachu.style.transform = "rotate(0deg)";
+
+        if (PositionX >= 1150) {
+          PositionX = 1150;
+          pikachu.style.transform = "rotateY(180deg)";
+        }
+      }
+    }); //Mouseover
+
+    pikachu.addEventListener('mouseover', function (ev) {
+      if (!ev) throw new Error("the event mouseover does not work");
+      var target = ev.target;
+      pikachu.setAttribute('src', 'img/pilove.gif');
+    }); //MouseOut
+
+    pikachu.addEventListener('mouseout', function (ev) {
+      if (!ev) throw new Error("the event mouseout does not work");
+      var target = ev.target;
+      pikachu.setAttribute('src', 'img/piran.gif');
+    });
+  } catch (e) {
+    console.log(e);
   }
-}); //Mouseover
-
-pikachu.addEventListener('mouseover', function (ev) {
-  var target = ev.target;
-  pikachu.setAttribute('src', 'img/pilove.gif');
-}); //MouseOut
-
-pikachu.addEventListener('mouseout', function (ev) {
-  var target = ev.target;
-  pikachu.setAttribute('src', 'img/piran.gif');
-}); //https://www.section.io/engineering-education/keyboard-events-in-javascript/
+} //https://www.section.io/engineering-education/keyboard-events-in-javascript/
