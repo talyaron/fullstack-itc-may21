12ccf14ec624ac4648f7c4faee328e681072111d
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-06-04 10:31:54 +0300

    .

--- 02-JavaScript/03-classes/yaniv/exercise.js
+++ 02-JavaScript/03-classes/yaniv/exercise.js
@@ -1,17 +1,22 @@
-class GamePlayer {
+class Player {
 
     constructor(playerID,playerName,playerColor,playerShapeClass) {
         this.playerID = playerID;
         this.playerName = playerName;
         this.playerColor = playerColor;
-        this.playerShapeClass = playerShapeClass;        
+        this.playerShapeClass = playerShapeClass;
+        this.playerDesc();
+    }
+
+    playerDesc() {
+        console.log(`Player Name: ${this.playerName}; Player Color: ${this.playerColor}; Player Shape: ${this.playerShapeClass}`);
     }
 }
 
-let player1 = new GamePlayer('#player1','Yaniv','blue','.circle');
+let player1 = new Player('player1','Yaniv','blue','circle');
 
 console.log(player1);
 
-let player2 = new GamePlayer('#player2','Tal','green','.rectangualr');
+let player2 = new Player('player2','Tal','green','rectangualr');
 
 console.log(player2);
