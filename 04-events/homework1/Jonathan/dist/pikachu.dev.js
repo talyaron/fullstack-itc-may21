b869e558731407bda3827ec3285767a5f167acb0
Jonathan Eliel Nisenbaum <47113013+jnisen@users.noreply.github.com>
2021-06-04 10:04:32 +0300

    change

--- 04-events/homework1/Jonathan/dist/pikachu.dev.js
+++ 04-events/homework1/Jonathan/dist/pikachu.dev.js
@@ -5,14 +5,14 @@ pikachuMove();
 function pikachuMove() {
   try {
     var pikachu = document.querySelector('.pikachu');
-    if (!pikachu) throw new Error("The image pikachu running can not uploaded"); //ver como lograr que sea prederminado por el windows
+    if (!pikachu) throw new Error("The image pikachu running can not uploaded"); //Position Pikachu
 
     pikachu.style.position = "relative";
     pikachu.style.left = "500px";
     pikachu.style.top = "200px"; //Get Position X and Y
 
-    var PositionX = parseInt(pikachu.style.left);
-    var PositionY = parseInt(pikachu.style.top); //Get speed of moving
+    var positionX = parseInt(pikachu.style.left);
+    var positionY = parseInt(pikachu.style.top); //Get speed of moving
 
     var speed = 10; //Movement with the keyboard
 
@@ -20,41 +20,41 @@ function pikachuMove() {
       if (!ev) throw new Error("the event keydown does not work");
 
       if (ev.key == "ArrowDown") {
-        pikachu.style.top = "".concat(PositionY + speed, "px");
-        PositionY = parseInt(pikachu.style.top); //pikachu.style.transform = "rotate(90deg)"
+        pikachu.style.top = "".concat(positionY + speed, "px");
+        positionY = parseInt(pikachu.style.top); //pikachu.style.transform = "rotate(90deg)"
 
         pikachu.style.transform = "rotateZ(90deg)";
 
-        if (PositionY >= 400) {
-          PositionY = 400;
+        if (positionY >= 400) {
+          positionY = 400;
           pikachu.style.transform = "rotate(270deg)";
         }
       } else if (ev.key == "ArrowUp") {
-        pikachu.style.top = "".concat(PositionY - speed, "px");
-        PositionY = parseInt(pikachu.style.top);
+        pikachu.style.top = "".concat(positionY - speed, "px");
+        positionY = parseInt(pikachu.style.top);
         pikachu.style.transform = "rotate(270deg)";
 
-        if (PositionY <= 15) {
-          PositionY = 15;
+        if (positionY <= 15) {
+          positionY = 15;
           pikachu.style.transform = "rotateZ(90deg)";
         }
       } else if (ev.key == "ArrowLeft") {
-        pikachu.style.left = "".concat(PositionX - speed, "px");
-        PositionX = parseInt(pikachu.style.left); //pikachu.style.transform = "rotate(180deg)"
+        pikachu.style.left = "".concat(positionX - speed, "px");
+        positionX = parseInt(pikachu.style.left); //pikachu.style.transform = "rotate(180deg)"
 
         pikachu.style.transform = "rotateY(180deg)";
 
-        if (PositionX <= 15) {
-          PositionX = 15;
+        if (positionX <= 15) {
+          positionX = 15;
           pikachu.style.transform = "rotate(0deg)";
         }
       } else if (ev.key == "ArrowRight") {
-        pikachu.style.left = "".concat(PositionX + speed, "px");
-        PositionX = parseInt(pikachu.style.left);
+        pikachu.style.left = "".concat(positionX + speed, "px");
+        positionX = parseInt(pikachu.style.left);
         pikachu.style.transform = "rotate(0deg)";
 
-        if (PositionX >= 1150) {
-          PositionX = 1150;
+        if (positionX >= 1150) {
+          positionX = 1150;
           pikachu.style.transform = "rotateY(180deg)";
         }
       }
