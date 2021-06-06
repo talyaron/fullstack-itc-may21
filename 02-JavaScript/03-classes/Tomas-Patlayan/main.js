12e97bf7a62ffa95eff69a0436733c962eee7c12
unknown <tomipatlayan@gmail.com>
2021-06-06 18:04:14 +0300

    m

--- 02-JavaScript/03-classes/Tomas-Patlayan/main.js
+++ 02-JavaScript/03-classes/Tomas-Patlayan/main.js
@@ -1,4 +1,5 @@
 class Game {
+<<<<<<< Updated upstream
   constructor(marioClass, width, height, marioURl) {
     this.marioClass = marioClass;
     this.width = width;
@@ -36,3 +37,33 @@ class Chat {
         this.
     }
 }
+=======
+ constructor(marioClass,width,height,marioURl){
+     this.marioClass = marioClass;
+     this.width = width;
+     this.height =height;
+     this.marioURl =marioURl;
+
+     this.mario = document.querySelector('.mario');
+     this.createPj() 
+ }
+    
+createPj() {
+    this.marioPj = document.createElement('img');
+    this.marioPj.setAttribute('src',this.marioURl);
+    this.marioPj.setAttribute('width', this.width);
+    this.marioPj.setAttribute('height', this.height);
+    this.mario.appendChild(this.marioPj);
+}
+
+
+// sayHello() {
+//     this.marioChat = document.createElement
+// }
+
+
+
+}
+
+let mario = new Game('.mario', '100px','100px', 'https://www.semana.com/resizer/j42cYUvr34aLn13_CqxlysNH4fM=/1200x675/filters:format(jpg):quality(70)//cloudfront-us-east-1.images.arcpublishing.com/semana/V3EVP6ZUEZFY7AIGESM76I43GE.jpg')
+>>>>>>> Stashed changes
