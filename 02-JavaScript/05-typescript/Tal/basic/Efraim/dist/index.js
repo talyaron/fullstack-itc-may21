c49da1cc1fec0b2bdcfbf01be9e96600ef4eb2a9
Efraim Dimbleby <82100352+EfraimDim@users.noreply.github.com>
2021-06-11 10:38:47 +0300

    78

--- 02-JavaScript/05-typescript/Tal/basic/Efraim/dist/index.js
+++ 02-JavaScript/05-typescript/Tal/basic/Efraim/dist/index.js
@@ -6,4 +6,6 @@ var personB = {
     a: "Dustin Martin",
     b: new Date(1994, 10, 22)
 };
+var f = get;
 console.log(personA);
+var getAge = function (people) { return; };
--- 02-JavaScript/05-typescript/Tal/basic/Efraim/index.ts
+++ 02-JavaScript/05-typescript/Tal/basic/Efraim/index.ts
@@ -11,5 +11,7 @@ var personB: People = {
     a: `Dustin Martin`,
     b: new Date (1994, 10, 22)
 }
-
+var f = get
 console.log(personA);
+var getAge = (people: People): number => 
+{ return };
