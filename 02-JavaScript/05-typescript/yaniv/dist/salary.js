0c0b4d1b5afaea025e614697bbe9736825c5312e
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-06-11 10:16:12 +0300

    spellcheck

--- 02-JavaScript/05-typescript/yaniv/dist/salary.js
+++ 02-JavaScript/05-typescript/yaniv/dist/salary.js
@@ -1,8 +1,8 @@
 // create a function in typescript that gets the sallery as an anual sallery (number), and return a string saying "your avarage monthly sallery is:x";
 // export {}
-function monthlySallery(anualSallery) {
-    var monthly = anualSallery / 12;
+var monthlySalary = function (anualSalary) {
+    var monthly = anualSalary / 12;
     return monthly;
-}
+};
 var yearly = 12000;
-console.log("Your monthly sallery is " + monthlySallery(yearly));
+console.log("Your avarage monthly sallery is " + monthlySalary(yearly));
--- 02-JavaScript/05-typescript/yaniv/salary.ts
+++ 02-JavaScript/05-typescript/yaniv/salary.ts
@@ -1,10 +1,10 @@
 // create a function in typescript that gets the sallery as an anual sallery (number), and return a string saying "your avarage monthly sallery is:x";
 
 // export {}
-function monthlySallery(anualSallery : number) : number {
-    const monthly : number = anualSallery/12;
+var monthlySalary = (anualSalary : number) : number => {
+    const monthly : number = anualSalary/12;
     return monthly;
 }
 
 const yearly = 12000;
-console.log(`Your monthly sallery is ${monthlySallery(yearly)}`);
+console.log(`Your avarage monthly sallery is ${monthlySalary(yearly)}`);
