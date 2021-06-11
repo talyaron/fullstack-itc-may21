e236dff551bab7288c9cc5e39033d7607e3f46a1
Jonathan Eliel Nisenbaum <47113013+jnisen@users.noreply.github.com>
2021-06-11 09:45:00 +0300

    f

--- 02-JavaScript/05-typescript/Tal/basic/Jonathan/index.ts
+++ 02-JavaScript/05-typescript/Tal/basic/Jonathan/index.ts
@@ -1,6 +1,5 @@
 function getsallery(sallery:number):string {
-    let monthly:number = sallery / 12;
-    return `your avarage monthly sallery is: ${monthly}`;
-}
+    let monthly:number = sallery / 12
+    return `your avarage monthly sallery is: ${monthly}`}
 
 console.log(getsallery(12000));
