7c29b0f8577b021b42bf0d5e2d41cbd1f0863f29
Julie Chajes <78927901+Juliechajes@users.noreply.github.com>
2021-06-11 11:24:34 +0300

    Update classwork.ts

--- 02-JavaScript/05-typescript/Julie/classwork.ts
+++ 02-JavaScript/05-typescript/Julie/classwork.ts
@@ -8,39 +8,40 @@ function monthlySalary = (annualSalary : number) : number => {
 const yearly = 100000;
 console.log(`Your avarage monthly salary is ${monthlySalary(yearly)}`);
 
+// Use interface for simple objects, and class for more complex ones, including methods.
+
 interface Person {
     name: string,
-    date: number,
-    month: number;
+    // date: number,
+    // month: number;
     year: number;
 }
 var person1: Person = {
     name: "Julie",
-    birthdate: 31,
-    birthmonth: 10,
+    // birthdate: 31,
+    // birthmonth: 10,
     birthyear: 1979,
 }
 
 var person2: Person = {
     name: "Yossi",
-    birthdate: 11,
-    birthmonth: 6,
+    // birthdate: 11,
+    // birthmonth: 6,
     birthyear: 1965,
 }
 
 var person3: Person = {
     name: "Yishai",
-    birthdate: 26,
-    birthmonth: 10,
+    // birthdate: 26,
+    // birthmonth: 10,
     birthyear: 2012,
 }
 
-var today: number = {
-    name: "Yishai",
-    currentdate: 11,
-    currentmonth: 6,
-    currentyear: 2021,
-}
 
-function getAge 
-// Here, we need the current 
+var getAge = (person:Person):string => {
+    const currentYear:number = new Date().getFullYear();
+    const yearOfBirth: number = person.birthyear;
+    const difference:number = currentYear - yearOfBirth;
+    return `${person.name}, your age is ${difference}`
+} 
+    
