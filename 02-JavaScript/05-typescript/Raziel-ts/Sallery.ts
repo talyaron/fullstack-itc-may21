87308f53ff991a9dbd2c782b8720d5b089573511
RazielAF <82554306+RazielAF@users.noreply.github.com>
2021-06-11 10:42:19 +0300

    k

--- 02-JavaScript/05-typescript/Raziel-ts/Sallery.ts
+++ 02-JavaScript/05-typescript/Raziel-ts/Sallery.ts
@@ -9,3 +9,21 @@ return `Your avrage for month is:${avrgString} `
 }
 
 console.log(Avrg(156000));
+
+interface Person{
+    name:string,
+    bDay:Date
+}
+
+var person1: Person={
+name:'Raziel',
+bDay: new Date (1993, 6, 5)
+}
+var person2: Person={
+    name:'Dan',
+    bDay: new Date (1947, 4, 20)
+    }
+
+
+
+console.log(person1.name);
