66cc3cf165077b75af429133162f14daa4f00a63
Jonathan Eliel Nisenbaum <47113013+jnisen@users.noreply.github.com>
2021-06-13 19:28:27 +0300

    golan and jony

---
+++ 02-JavaScript/11-projects/01-myBook/Golan-Jony/project.ts
@@ -0,0 +1,79 @@
+class Article {
+    imageURL:string;
+    description:string;
+    articleId:number;
+
+    constructor(imageURL:string, description:string, articleId:number){
+        this.imageURL = imageURL;
+        this.description = description;
+        this.articleId = articleId;
+    }
+
+    changeDescription(){
+
+    }
+
+    changeImageURL(){
+
+    }
+}
+
+class ArticleArray{
+
+    articles:Array<string>
+
+    constructor(articles:Array<string>){
+        this.articles = articles;
+
+    }
+
+    Add(){
+
+    }
+
+    Remove(){ //articleId
+
+    }
+
+    renderArray(){
+
+    }
+    renderArticle(){ //articleId
+
+    }
+
+
+}
+
+function handlerSumbit(event) {
+    
+    event.preventDefault();
+
+    var root: HTMLElement = document.querySelector('#boardProject');
+
+    var inputURL = event.target.elements.imageURL.value;
+    var description = event.target.elements.description.value;
+
+    var article = document.createElement('div'); 
+    article.className = "divArt";
+    article.style.width = "100px";
+    article.style.height = "100px";
+    article.style.backgroundColor = "red";
+    container.appendChild(article);
+    
+    var container = document.createElement('div');
+    container.className = "container" 
+    root.appendChild(container)
+
+
+
+
+    
+
+
+}
+
+
+
+
+
