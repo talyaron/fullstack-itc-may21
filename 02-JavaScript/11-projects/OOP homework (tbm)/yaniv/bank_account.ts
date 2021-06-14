fde120ce8d4717ab49bb91505e3332ca35691fd3
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-06-14 20:24:08 +0300

    .

--- 02-JavaScript/11-projects/OOP homework (tbm)/yaniv/bank_account.ts
+++ 02-JavaScript/11-projects/OOP homework (tbm)/yaniv/bank_account.ts
@@ -1,1 +1,52 @@
-// script
+class Transaction {
+    transBiz : string;
+    transAmount : number;
+    transDate : Date;
+    transId : string;
+
+    constructor (transBiz : string, transAmount : number) {
+        this.transBiz = transBiz;
+        this.transAmount = transAmount;
+        this.transDate = new Date();
+        this.transId = "id" + Math.random().toString(16).slice(2);
+    }
+
+}
+
+class Account {
+    profImageUrl : string;
+    allTransactions : Array<Transaction>;
+    totalAmount : number;
+
+    constructor (profImageUrl : string) {
+        this.profImageUrl = profImageUrl;
+    }
+
+    addTrans() : void {
+
+    }
+
+    refreshTotal() : number {
+
+        return this.totalAmount;
+    }
+}
+
+
+const transactions = new Account(img);
+
+var handleSubmit = (ev: any) => {
+    ev.preventDefault();
+
+    const imageUrl: string = ev.target.elements.imageUrl.value;
+    const description: string = ev.target.elements.description.value;
+
+
+    const article = new Article(imageUrl, description);
+
+    articles.add(article);
+    articles.renderArticles();
+
+    console.log(articles)
+
+}
--- 02-JavaScript/11-projects/OOP homework (tbm)/yaniv/dist/bank_account.js
+++ 02-JavaScript/11-projects/OOP homework (tbm)/yaniv/dist/bank_account.js
@@ -1,1 +1,30 @@
-// script
+var Transaction = /** @class */ (function () {
+    function Transaction(transBiz, transAmount) {
+        this.transBiz = transBiz;
+        this.transAmount = transAmount;
+        this.transDate = new Date();
+        this.transId = "id" + Math.random().toString(16).slice(2);
+    }
+    return Transaction;
+}());
+var Account = /** @class */ (function () {
+    function Account(profImageUrl) {
+        this.profImageUrl = profImageUrl;
+    }
+    Account.prototype.addTrans = function () {
+    };
+    Account.prototype.refreshTotal = function () {
+        return this.totalAmount;
+    };
+    return Account;
+}());
+var transactions = new Account(img);
+var handleSubmit = function (ev) {
+    ev.preventDefault();
+    var imageUrl = ev.target.elements.imageUrl.value;
+    var description = ev.target.elements.description.value;
+    var article = new Article(imageUrl, description);
+    articles.add(article);
+    articles.renderArticles();
+    console.log(articles);
+};
