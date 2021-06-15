9c34bf9013d4518560dd8509c433ee4b7792c942
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-06-15 08:37:43 +0300

    finallll

--- 02-JavaScript/06-OOP/yaniv/bank_account.ts
+++ 02-JavaScript/06-OOP/yaniv/bank_account.ts
@@ -2,7 +2,7 @@ class Transaction {
   transAmount: number;
   transDate: Date = new Date();
   transBiz: string;
-  transId: string = 'id' + Math.random().toString(16).slice(2);
+  transId: string = "id" + Math.random().toString(16).slice(2);
 
   constructor(transAmount: number, transBiz: string) {
     this.transBiz = transBiz;
@@ -10,14 +10,14 @@ class Transaction {
   }
 }
 
-document.querySelector('#total_amount').innerHTML = '₪0';
+document.querySelector("#total_amount").innerHTML = "₪0";
 
 class Account {
   customerName: string; // YA: out of scope for this task
   profImageUrl: string; // YA: out of scope for this task
   allTransactions: Array<Transaction> = [];
   totalAmount: number = Number(
-    document.querySelector('#total_amount').innerHTML.replace('₪', '')
+    document.querySelector("#total_amount").innerHTML.replace("₪", "")
   );
 
   // constructor (profImageUrl : string) { // YA: out of scope for this task
@@ -33,50 +33,50 @@ class Account {
   addTransToDOM(transaction: Transaction) {
     try {
       const transContainer: HTMLElement =
-        document.querySelector('.transactions');
+        document.querySelector(".transactions");
 
       const newTrans: HTMLElement = document.createElement(`div`);
       newTrans.classList.add(
-        'transactions__item',
-        'transactions__item--action'
+        "transactions__item",
+        "transactions__item--action"
       );
       transContainer.appendChild(newTrans);
       const newTransSign: HTMLElement = document.createElement(`i`);
-      newTransSign.id = 'sign';
-      newTransSign.classList.add('fas', 'fa-2x');
+      newTransSign.id = "sign";
+      newTransSign.classList.add("fas", "fa-2x");
       if (transaction.transAmount > 0) {
-        newTransSign.classList.add('fa-plus-circle');
-        newTransSign.style.color = 'green';
-        newTransSign.title = 'Income';
+        newTransSign.classList.add("fa-plus-circle");
+        newTransSign.style.color = "green";
+        newTransSign.title = "Income";
       } else {
-        newTransSign.classList.add('fa-minus-circle');
-        newTransSign.style.color = 'red';
-        newTransSign.title = 'Expense';
+        newTransSign.classList.add("fa-minus-circle");
+        newTransSign.style.color = "red";
+        newTransSign.title = "Expense";
       }
       newTrans.appendChild(newTransSign);
       const newTransAmount: HTMLElement = document.createElement(`div`);
-      newTransAmount.id = 'trans_amount';
+      newTransAmount.id = "trans_amount";
       newTransAmount.innerText = `₪${Math.abs(transaction.transAmount)}`;
       if (transaction.transAmount >= 0) {
-        newTransAmount.style.color = 'green';
+        newTransAmount.style.color = "green";
       } else {
-        newTransAmount.style.color = 'red';
+        newTransAmount.style.color = "red";
       }
       newTrans.appendChild(newTransAmount);
       const newTransDate: HTMLElement = document.createElement(`div`);
-      newTransDate.id = 'trans_date';
+      newTransDate.id = "trans_date";
       newTransDate.innerText = `${transaction.transDate.getDate()}-${
         transaction.transDate.getMonth() + 1
       }-${transaction.transDate.getFullYear()}`;
       newTrans.appendChild(newTransDate);
-      const totalDate: HTMLElement = document.querySelector('#total_date');
+      const totalDate: HTMLElement = document.querySelector("#total_date");
       totalDate.innerText = `For Date: ${newTransDate.innerText}`;
       const newTransBiz: HTMLElement = document.createElement(`div`);
-      newTransBiz.id = 'trans_business';
+      newTransBiz.id = "trans_business";
       newTransBiz.innerText = `${transaction.transBiz}`;
       newTrans.appendChild(newTransBiz);
       const newTransId: HTMLElement = document.createElement(`div`);
-      newTransId.id = 'trans_id';
+      newTransId.id = "trans_id";
       newTransId.innerText = `${transaction.transId}`;
       newTrans.appendChild(newTransId);
     } catch (er) {
@@ -91,67 +91,79 @@ class Account {
 
   updateDOMTotal(totalAmount: number) {
     try {
-      const domTotal: HTMLElement = document.querySelector('#total_amount');
+      const domTotal: HTMLElement = document.querySelector("#total_amount");
       domTotal.innerText = `₪${totalAmount}`;
       if (totalAmount >= 0) {
-        domTotal.style.color = 'green';
+        domTotal.style.color = "green";
       } else {
-       domTotal.style.color = 'red';
+        domTotal.style.color = "red";
       }
     } catch (er) {
-     console.error(er);
-   }
+      console.error(er);
+    }
   }
 }
 
 let isModalOpen: boolean = false;
 
 const openModal = (): void => {
-  const addTransBtn: HTMLElement = document.querySelector(
-    `.transactions__item--add`
-  );
-  const modal: HTMLElement = document.querySelector(`.modalWrapper`);
-  const modalBox: HTMLElement = document.querySelector(`.modalBox`);
+  try {
+    const addTransBtn: HTMLElement = document.querySelector(
+      `.transactions__item--add`
+    );
+    const modal: HTMLElement = document.querySelector(`.modalWrapper`);
+    const modalBox: HTMLElement = document.querySelector(`.modalBox`);
 
-  addTransBtn.addEventListener(`click`, (ev) => {
-    isModalOpen = true;
-    modal.style.display = `flex`;
-    modalBox.style.display = `unset`;
-  });
+    addTransBtn.addEventListener(`click`, (ev) => {
+      isModalOpen = true;
+      modal.style.display = `flex`;
+      modalBox.style.display = `unset`;
+    });
+  } catch (er) {
+    console.error(er);
+  }
 };
 
 const closeModal = (): void => {
-  const close: HTMLElement = document.querySelector(`.close`);
-  const modal: HTMLElement = document.querySelector(`.modalWrapper`);
-  const modalBox: HTMLElement = document.querySelector(`.modalBox`);
+  try {
+    const close: HTMLElement = document.querySelector(`.close`);
+    const modal: HTMLElement = document.querySelector(`.modalWrapper`);
+    const modalBox: HTMLElement = document.querySelector(`.modalBox`);
 
-  close.addEventListener(`click`, (ev) => {
-    isModalOpen = false;
-    modal.style.display = `none`;
-    modalBox.style.display = `none`;
-  });
+    close.addEventListener(`click`, (ev) => {
+      isModalOpen = false;
+      modal.style.display = `none`;
+      modalBox.style.display = `none`;
+    });
+  } catch (er) {
+    console.error(er);
+  }
 };
 
 const transactions = new Account();
 
 const handleSubmit = (ev: any) => {
-  ev.preventDefault();
+  try {
+    ev.preventDefault();
 
-  const transAmount: number = Number(ev.target.elements.transAmount.value);
-  const transBiz: string = ev.target.elements.transBiz.value;
+    const transAmount: number = Number(ev.target.elements.transAmount.value);
+    const transBiz: string = ev.target.elements.transBiz.value;
 
-  const transaction = new Transaction(transAmount, transBiz);
+    const transaction = new Transaction(transAmount, transBiz);
 
-  transactions.addTrans(transaction);
+    transactions.addTrans(transaction);
 
-  const modal: HTMLElement = document.querySelector(`.modalWrapper`);
-  const modalBox: HTMLElement = document.querySelector(`.modalBox`);
+    const modal: HTMLElement = document.querySelector(`.modalWrapper`);
+    const modalBox: HTMLElement = document.querySelector(`.modalBox`);
 
-  isModalOpen = false;
-  modal.style.display = `none`;
-  modalBox.style.display = `none`;
+    isModalOpen = false;
+    modal.style.display = `none`;
+    modalBox.style.display = `none`;
 
-  ev.target.reset();
+    ev.target.reset();
+  } catch (er) {
+    console.error(er);
+  }
 };
 
 openModal();
--- 02-JavaScript/06-OOP/yaniv/dist/bank_account.js
+++ 02-JavaScript/06-OOP/yaniv/dist/bank_account.js
@@ -1,17 +1,17 @@
 var Transaction = /** @class */ (function () {
     function Transaction(transAmount, transBiz) {
         this.transDate = new Date();
-        this.transId = 'id' + Math.random().toString(16).slice(2);
+        this.transId = "id" + Math.random().toString(16).slice(2);
         this.transBiz = transBiz;
         this.transAmount = transAmount;
     }
     return Transaction;
 }());
-document.querySelector('#total_amount').innerHTML = '₪0';
+document.querySelector("#total_amount").innerHTML = "₪0";
 var Account = /** @class */ (function () {
     function Account() {
         this.allTransactions = [];
-        this.totalAmount = Number(document.querySelector('#total_amount').innerHTML.replace('₪', ''));
+        this.totalAmount = Number(document.querySelector("#total_amount").innerHTML.replace("₪", ""));
     }
     // constructor (profImageUrl : string) { // YA: out of scope for this task
     //     this.profImageUrl = profImageUrl;
@@ -23,46 +23,46 @@ var Account = /** @class */ (function () {
     };
     Account.prototype.addTransToDOM = function (transaction) {
         try {
-            var transContainer = document.querySelector('.transactions');
+            var transContainer = document.querySelector(".transactions");
             var newTrans = document.createElement("div");
-            newTrans.classList.add('transactions__item', 'transactions__item--action');
+            newTrans.classList.add("transactions__item", "transactions__item--action");
             transContainer.appendChild(newTrans);
             var newTransSign = document.createElement("i");
-            newTransSign.id = 'sign';
-            newTransSign.classList.add('fas', 'fa-2x');
+            newTransSign.id = "sign";
+            newTransSign.classList.add("fas", "fa-2x");
             if (transaction.transAmount > 0) {
-                newTransSign.classList.add('fa-plus-circle');
-                newTransSign.style.color = 'green';
-                newTransSign.title = 'Income';
+                newTransSign.classList.add("fa-plus-circle");
+                newTransSign.style.color = "green";
+                newTransSign.title = "Income";
             }
             else {
-                newTransSign.classList.add('fa-minus-circle');
-                newTransSign.style.color = 'red';
-                newTransSign.title = 'Expense';
+                newTransSign.classList.add("fa-minus-circle");
+                newTransSign.style.color = "red";
+                newTransSign.title = "Expense";
             }
             newTrans.appendChild(newTransSign);
             var newTransAmount = document.createElement("div");
-            newTransAmount.id = 'trans_amount';
+            newTransAmount.id = "trans_amount";
             newTransAmount.innerText = "\u20AA" + Math.abs(transaction.transAmount);
             if (transaction.transAmount >= 0) {
-                newTransAmount.style.color = 'green';
+                newTransAmount.style.color = "green";
             }
             else {
-                newTransAmount.style.color = 'red';
+                newTransAmount.style.color = "red";
             }
             newTrans.appendChild(newTransAmount);
             var newTransDate = document.createElement("div");
-            newTransDate.id = 'trans_date';
+            newTransDate.id = "trans_date";
             newTransDate.innerText = transaction.transDate.getDate() + "-" + (transaction.transDate.getMonth() + 1) + "-" + transaction.transDate.getFullYear();
             newTrans.appendChild(newTransDate);
-            var totalDate = document.querySelector('#total_date');
+            var totalDate = document.querySelector("#total_date");
             totalDate.innerText = "For Date: " + newTransDate.innerText;
             var newTransBiz = document.createElement("div");
-            newTransBiz.id = 'trans_business';
+            newTransBiz.id = "trans_business";
             newTransBiz.innerText = "" + transaction.transBiz;
             newTrans.appendChild(newTransBiz);
             var newTransId = document.createElement("div");
-            newTransId.id = 'trans_id';
+            newTransId.id = "trans_id";
             newTransId.innerText = "" + transaction.transId;
             newTrans.appendChild(newTransId);
         }
@@ -76,13 +76,13 @@ var Account = /** @class */ (function () {
     };
     Account.prototype.updateDOMTotal = function (totalAmount) {
         try {
-            var domTotal = document.querySelector('#total_amount');
+            var domTotal = document.querySelector("#total_amount");
             domTotal.innerText = "\u20AA" + totalAmount;
             if (totalAmount >= 0) {
-                domTotal.style.color = 'green';
+                domTotal.style.color = "green";
             }
             else {
-                domTotal.style.color = 'red';
+                domTotal.style.color = "red";
             }
         }
         catch (er) {
@@ -93,38 +93,53 @@ var Account = /** @class */ (function () {
 }());
 var isModalOpen = false;
 var openModal = function () {
-    var addTransBtn = document.querySelector(".transactions__item--add");
-    var modal = document.querySelector(".modalWrapper");
-    var modalBox = document.querySelector(".modalBox");
-    addTransBtn.addEventListener("click", function (ev) {
-        isModalOpen = true;
-        modal.style.display = "flex";
-        modalBox.style.display = "unset";
-    });
+    try {
+        var addTransBtn = document.querySelector(".transactions__item--add");
+        var modal_1 = document.querySelector(".modalWrapper");
+        var modalBox_1 = document.querySelector(".modalBox");
+        addTransBtn.addEventListener("click", function (ev) {
+            isModalOpen = true;
+            modal_1.style.display = "flex";
+            modalBox_1.style.display = "unset";
+        });
+    }
+    catch (er) {
+        console.error(er);
+    }
 };
 var closeModal = function () {
-    var close = document.querySelector(".close");
-    var modal = document.querySelector(".modalWrapper");
-    var modalBox = document.querySelector(".modalBox");
-    close.addEventListener("click", function (ev) {
-        isModalOpen = false;
-        modal.style.display = "none";
-        modalBox.style.display = "none";
-    });
+    try {
+        var close = document.querySelector(".close");
+        var modal_2 = document.querySelector(".modalWrapper");
+        var modalBox_2 = document.querySelector(".modalBox");
+        close.addEventListener("click", function (ev) {
+            isModalOpen = false;
+            modal_2.style.display = "none";
+            modalBox_2.style.display = "none";
+        });
+    }
+    catch (er) {
+        console.error(er);
+    }
 };
 var transactions = new Account();
 var handleSubmit = function (ev) {
-    ev.preventDefault();
-    var transAmount = Number(ev.target.elements.transAmount.value);
-    var transBiz = ev.target.elements.transBiz.value;
-    var transaction = new Transaction(transAmount, transBiz);
-    transactions.addTrans(transaction);
-    var modal = document.querySelector(".modalWrapper");
-    var modalBox = document.querySelector(".modalBox");
-    isModalOpen = false;
-    modal.style.display = "none";
-    modalBox.style.display = "none";
-    ev.target.reset();
+    try {
+        ev.preventDefault();
+        var transAmount = Number(ev.target.elements.transAmount.value);
+        var transBiz = ev.target.elements.transBiz.value;
+        var transaction = new Transaction(transAmount, transBiz);
+        transactions.addTrans(transaction);
+        var modal = document.querySelector(".modalWrapper");
+        var modalBox = document.querySelector(".modalBox");
+        isModalOpen = false;
+        modal.style.display = "none";
+        modalBox.style.display = "none";
+        ev.target.reset();
+    }
+    catch (er) {
+        console.error(er);
+    }
 };
 openModal();
 closeModal();
