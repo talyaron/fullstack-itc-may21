826b953c0b176a1f54386cf194d6d3d9da4665c0
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2021-06-15 15:18:11 +0300

    up

---
+++ 02-JavaScript/11-projects/OOP homework (tbm)/TomasPatlayan/app.js
@@ -0,0 +1,6 @@
+var Account = /** @class */ (function () {
+    function Account(amount) {
+        this.amount = amount;
+    }
+    return Account;
+}());
---
+++ 02-JavaScript/11-projects/OOP homework (tbm)/TomasPatlayan/app.ts
@@ -0,0 +1,6 @@
+class Account {
+amount: number;
+constructor(amount:number) {
+  this.amount = amount;
+}
+}
