bacf9cb5dac907298760a083d583e4c196cbacd0
Tal <tal.yaron@gmail.com>
2021-06-16 15:23:02 +0300

    full example

--- 02-JavaScript/06-OOP/ex/bank_account/Tal/dist/index.js
+++ 02-JavaScript/06-OOP/ex/bank_account/Tal/dist/index.js
@@ -13,5 +13,13 @@ var Account = /** @class */ (function () {
     Account.prototype.addNewTransaction = function (amount, date, place, description) {
         this.account.push(new Transaction(amount, date, place, description));
     };
+    Account.prototype.calculateSum = function () {
+        var sum = this.account.reduce(function (sum, transaction) { return sum + transaction.amount; }, 0);
+        return sum;
+    };
     return Account;
 }());
+var account = new Account();
+account.addNewTransaction(100, new Date(), 'Tel-Aviv', 'Bank deposit');
+account.addNewTransaction(-300, new Date(), 'Ramat-Gan', 'ATM redrwal');
+console.log(account.calculateSum());
--- 02-JavaScript/06-OOP/ex/bank_account/Tal/index.ts
+++ 02-JavaScript/06-OOP/ex/bank_account/Tal/index.ts
@@ -16,10 +16,23 @@ class Transaction {
 class Account{
     account:Array<Transaction>;
 
-    addNewTransaction(amount: number, date: Date, place: string, description: string){
+    addNewTransaction(amount: number, date: Date, place: string, description: string):void{
         this.account.push(new Transaction(amount, date, place, description))
     }
+
+    calculateSum():number{
+        const sum = this.account.reduce((sum,transaction)=>{return sum + transaction.amount},0)
+        return sum;
+    }
 }
+
+let account = new Account();
+account.addNewTransaction(100,new Date(),'Tel-Aviv','Bank deposit');
+account.addNewTransaction(-300, new Date(),'Ramat-Gan', 'ATM redrwal');
+
+console.log(account.calculateSum());
+
+
 
 
 
