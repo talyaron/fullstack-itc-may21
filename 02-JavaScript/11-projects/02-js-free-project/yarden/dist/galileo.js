ccdab28c4cfb2e14193cd918f39642802f499b0c
Yarden-Tal <yardental1989@gmail.com>
2021-06-20 18:41:27 +0300

    ts start

--- 02-JavaScript/11-projects/02-js-free-project/yarden/dist/galileo.js
+++ 02-JavaScript/11-projects/02-js-free-project/yarden/dist/galileo.js
@@ -1,6 +1,15 @@
-// 
-var star = /** @class */ (function () {
-    function star(name, distanceFromEarth) {
+// Create Planet class:
+var Planet = /** @class */ (function () {
+    function Planet(name, distanceFromEarth, pastExpeditions, pastTravelers, imageSrc) {
+        this.name = name;
+        this.distanceFromEarth = distanceFromEarth;
+        this.pastExpeditions = pastExpeditions;
+        this.pastTravelers = pastTravelers;
+        this.imageSrc = imageSrc;
     }
-    return star;
+    return Planet;
 }());
+//Create the 3 planets:
+var mars = new Planet('Mars', 288, 43, 308, 'https://d2pn8kiwq2w21t.cloudfront.net/images/Mars-800h-v2_02.width-1024.png');
+var venus = new Planet('Venus', 230.64, 18, 167, 'https://upload.wikimedia.org/wikipedia/commons/9/93/Venus_globe_-_transparent_background.png');
+var moon = new Planet('Moon', 384.40, 154, 797, '02-JavaScript\11-projects\02-js-free-project\yarden\images\pngfind.com-full-moon-png-319821.png');
