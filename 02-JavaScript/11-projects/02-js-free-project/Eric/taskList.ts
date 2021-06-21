3b231356b168dd9d84ecad01bb3936acf8a079a7
Eric Kobrinsky <78773449+erickobrinsky@users.noreply.github.com>
2021-06-21 19:47:25 +0300

    test11

--- 02-JavaScript/11-projects/02-js-free-project/Eric/taskList.ts
+++ 02-JavaScript/11-projects/02-js-free-project/Eric/taskList.ts
@@ -1,17 +1,38 @@
 const allItems = JSON.parse(localStorage.getItem('item'));
 const root = document.querySelector('#root')
 
-function renderItems() {
-    let html: string = this.allItems.allItems.map(element => {
-        return (
-            `${element.task}`
-        )
-    })
+//agregar boton check en cada item, que se vea el boton y lo pueda clickear
+function renderItems():void {
+
+    let html: string = "";
+    allItems.forEach(element => {
+       html += `<div onclick="deletes()"> ${element.id} ${element.task} </div>`
+    });    
     root.innerHTML = html;
 }
-
 renderItems();
+
+
+
+//function deletes(){}
 
 function redirectt() {
     window.location.href = 'index.html'
 }
+
+
+//al entrar en esta pagina que me muestre solamente los que estan checked de la pagina anterior
+function checked () {
+    window.location.href = 'completed.html'
+}
+
+
+//localStorage.clear()
+
+
+/*let html: string = this.allItems.allItems.map(element => {
+    console.log(element.task)
+    return (
+        `<div onclick="deletes()"> ${element.task} </div>`)
+})
+console.log(html)*/
