7eae9796d250bd899fac0151bd08457d26e71bb5
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2021-06-21 22:44:32 +0300

    u

---
+++ 02-JavaScript/11-projects/02-js-free-project/TomasPatlayan/TS/cvs.js
@@ -0,0 +1,14 @@
+// const getCv = JSON.stringify(localStorage.getItem("completeCv"));
+// const show: HTMLElement = document.querySelector("#show");
+// function pe() {
+//     let html: string = this.cvs.cvs.map( element => {
+//         return (
+//          `<h1>${element.name}</h1>`
+//         )
+//     }).join('');
+//     show.innerHTML = html;
+// }
+// pe();
+var printCv = localStorage.getItem('completeCv');
+console.log(printCv);
+document.querySelector('#show').innerHTML = printCv;
--- 02-JavaScript/11-projects/02-js-free-project/TomasPatlayan/TS/main.ts
+++ 02-JavaScript/11-projects/02-js-free-project/TomasPatlayan/TS/main.ts
@@ -1,45 +1,75 @@
 class CvGenerator {
   name: string;
-
   constructor(name: string) {
     this.name = name;
   }
 }
+
 class Cv {
   cv: Array<CvGenerator> = [];
 
-  addCv(newCv: CvGenerator): void {
-    this.cv.push(newCv);
-    this.showCv();
-  }
-
-  showCv() {
-    localStorage.getItem("name");
-    const printName = document.getElementById("show");
-
-    let ponerCV: string = "";
-
-    this.cv.forEach((ponerCVs) => {
-      ponerCV += `<h3>${ponerCVs.name}</h3>`;
-    });
-    printName.innerHTML = ponerCV;
+  addCv(add: CvGenerator): void {
+    this.cv.push(add);
   }
 }
+const cvs = new Cv();
 
-const createCV = new Cv();
-
-console.log(createCV);
-
-const handleSubmit = (event: any): void => {
+const handleSubmit = (event) => {
   event.preventDefault();
+  
+  const name:string = event.target.elements.name.value;
 
-  const name: string = event.target.elements.name.value;
-  localStorage.setItem("name", name);
-
-  window.location.href = "cv.html";
-
-  const cv = new CvGenerator(name);
-  console.log(cv);
-
-  createCV.addCv(cv);
+  const generator = new CvGenerator(name);
+  event.target.reset();
+  cvs.addCv(generator)
+  localStorage.setItem("completeCv", JSON.stringify(cvs))
+  window.location.href = 'cv.html'
+  console.log(cvs);
+  //RECORDATORIA PREGUNTAR QUE VALORES ASIGNARLE A DIVERSAS VARIABLES MASS HAYA DE STRING NUMBER ETC Y COMO ME DOY CUENTA DE COMO NOOMBRARLAS O SI EXISTE LA DOCUMENTACION DE QUE VA CON CADA UNA 
 };
+
+// class CvGenerator {
+//   name: string;
+
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
+// class Cv {
+//   cv: Array<CvGenerator> = [];
+
+//   addCv(newCv: CvGenerator): void {
+//     this.cv.push(newCv);
+//     this.showCv();
+//   }
+
+//   showCv() {
+//   localStorage.getItem("name");
+//   const printName = document.getElementById("show");
+
+//     let ponerCV: any = "";
+
+//     this.cv.forEach((ponerCVs) => {
+//       ponerCV += `<h3>${ponerCVs.name}</h3>`;
+//     });
+//     printName.innerHTML = ponerCV;
+//   }
+// }
+
+// const createCV = new Cv();
+
+// console.log(createCV);
+
+// const handleSubmit = (event: any): void => {
+//   event.preventDefault();
+
+//   const name: string = event.target.elements.name.value;
+//   localStorage.setItem("name",JSON.stringify(name) );
+
+//   window.location.href = "cv.html";
+
+//   const cv = new CvGenerator(name);
+//   console.log(cv);
+
+//   createCV.addCv(cv);
+// };
