7b7565c76eed742dbd3876750302c4b228963415
Yarden-Tal <yardental1989@gmail.com>
2021-06-23 13:43:26 +0300

    final commit!

--- 02-JavaScript/11-projects/02-js-free-project/yarden/dist/form.js
+++ 02-JavaScript/11-projects/02-js-free-project/yarden/dist/form.js
@@ -1,9 +1,11 @@
+console.log('***The errors are caused by the Youtube embed for some reason. After a lot of effort I decided to skip solving these and leave the iframe as is!***');
+//Select the locations to insert the data collected at the Homepage:
 var date = document.querySelector('#date').getAttribute('value');
 var expedition = document.querySelector('#expedition');
+//Insert the data from the Homepage:
 var chosenDate = localStorage.getItem('date');
 var chosenExpedition = localStorage.getItem('expedition');
 document.querySelector('#date').setAttribute('value', chosenDate);
-//Match selected expedition with the form Select element Option:
 var selectExpedition = function () {
     try {
         if (chosenExpedition === 'Mars Expedition') {
@@ -21,3 +23,33 @@ var selectExpedition = function () {
     }
 };
 selectExpedition();
+//The user class for the expedition form:
+var Participant = /** @class */ (function () {
+    function Participant(name, date, planet, isCertified) {
+        this.name = name;
+        this.date = date;
+        this.planet = planet;
+        this.isCertified = isCertified;
+    }
+    return Participant;
+}());
+//An array listing the participants booked:
+var participants = [];
+//Handle submit event for the form:
+var handleSubmit = function (event) {
+    try {
+        event.preventDefault();
+        //prepare the input as paramaters for the Participant Class:
+        var name = event.target.elements.name.value;
+        var date_1 = event.target.elements.date.value;
+        var expedition_1 = event.target.elements.expedition.value;
+        var isCertified = event.target.elements.isCertified.checked;
+        //Add participant to the participants array:
+        participants.push(new Participant(name, date_1, expedition_1, isCertified));
+        console.log(participants);
+        alert("Thank you, " + name + "!");
+    }
+    catch (error) {
+        console.error(error);
+    }
+};
--- 02-JavaScript/11-projects/02-js-free-project/yarden/form.ts
+++ 02-JavaScript/11-projects/02-js-free-project/yarden/form.ts
@@ -1,12 +1,12 @@
+console.log('***The errors are caused by the Youtube embed for some reason. After a lot of effort I decided to skip solving these and leave the iframe as is!***');
+
+//Select the locations to insert the data collected at the Homepage:
     const date = document.querySelector('#date').getAttribute('value')
     const expedition = document.querySelector('#expedition')
-
+//Insert the data from the Homepage:
     const chosenDate = localStorage.getItem('date')
-    const chosenExpedition = localStorage.getItem('expedition')
-        
+    const chosenExpedition = localStorage.getItem('expedition')        
     document.querySelector('#date').setAttribute('value', chosenDate)
-    
-    //Match selected expedition with the form Select element Option:
     const selectExpedition = () => {
         try {
             if (chosenExpedition === 'Mars Expedition') {
@@ -23,5 +23,41 @@
             
     }
 }
-
 selectExpedition()
+
+//The user class for the expedition form:
+class Participant {
+    name:string
+    date:Date
+    planet:string
+    isCertified:Boolean
+
+    constructor(name:string, date:Date, planet:string, isCertified:Boolean) {
+        this.name = name
+        this.date = date
+        this.planet = planet
+        this.isCertified = isCertified
+    }
+}   
+//An array listing the participants booked:
+let participants:Array<Participant> = []
+
+//Handle submit event for the form:
+const handleSubmit = (event:any)=> {
+    try {    
+        event.preventDefault();
+        //prepare the input as paramaters for the Participant Class:
+        const name = event.target.elements.name.value;
+        const date = event.target.elements.date.value;
+        const expedition = event.target.elements.expedition.value;
+        const isCertified = event.target.elements.isCertified.checked;        
+        //Add participant to the participants array:
+        participants.push(new Participant(name, date, expedition, isCertified));
+        console.log(participants);
+        alert(`Thank you, ${name}!`)
+
+} 
+     catch (error) {
+        console.error(error)
+    }
+}
