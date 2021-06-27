bb24345c38bd55abfa1a24dd91f09fae951ae383
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2021-06-28 00:43:20 +0300

    up

--- 02-JavaScript/99-projects/03-filltering-CRUD/TomasLeonPatlayan/TS/main.js
+++ 02-JavaScript/99-projects/03-filltering-CRUD/TomasLeonPatlayan/TS/main.js
@@ -10,6 +10,20 @@ var Ids = /** @class */ (function () {
     }
     Ids.prototype.add = function (add) {
         this.id.push(add);
+        this.render();
+    };
+    Ids.prototype.render = function () {
+        var show = document.querySelector(".show");
+        var html = "";
+        this.id.forEach(function (element) {
+            html += "<div class = 'show-id'n onclick=\"btnDelete(event)\"><h1> " + element.name + "</h1>\n      <button class = 'delete'>X</button></div>";
+        });
+        show.innerHTML = html;
+    };
+    Ids.prototype.deleteIds = function (ele) {
+        if (ele.classList.contains("delete")) {
+            ele.parentElement.remove();
+        }
     };
     return Ids;
 }());
@@ -18,4 +32,10 @@ var handleSubmit = function (event) {
     event.preventDefault();
     var name = event.target.elements.name.value;
     var generator = new IdsGenerator(name);
+    console.log(generator);
+    ids.add(generator);
+    event.target.reset();
+};
+var btnDelete = function (event) {
+    ids.deleteIds(event.target);
 };
--- 02-JavaScript/99-projects/03-filltering-CRUD/TomasLeonPatlayan/TS/main.ts
+++ 02-JavaScript/99-projects/03-filltering-CRUD/TomasLeonPatlayan/TS/main.ts
@@ -8,17 +8,43 @@ class IdsGenerator {
 class Ids {
   id: Array<IdsGenerator> = [];
 
-  add(add: IdsGenerator) {
+  add(add: IdsGenerator): void {
     this.id.push(add);
+    this.render();
+  }
+
+  render() {
+    const show: HTMLElement = document.querySelector(".show");
+    let html: string = "";
+    this.id.forEach((element) => {
+      html += `<div class = 'show-id'n onclick="btnDelete(event)"><h1> ${element.name}</h1>
+      <button class = 'delete'>X</button></div>`;
+    });
+
+    show.innerHTML = html;
+  }
+
+  deleteIds(ele) {
+    if (ele.classList.contains("delete")) {
+      ele.parentElement.remove();
+    }
   }
 }
 
 const ids = new Ids();
 
 const handleSubmit = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const name = event.target.elements.name.value;
+  const name: string = event.target.elements.name.value;
 
-    const generator = new IdsGenerator(name);
-}
+  const generator = new IdsGenerator(name);
+
+  console.log(generator);
+  ids.add(generator);
+  event.target.reset();
+};
+
+const btnDelete = (event) => {
+  ids.deleteIds(event.target);
+};
