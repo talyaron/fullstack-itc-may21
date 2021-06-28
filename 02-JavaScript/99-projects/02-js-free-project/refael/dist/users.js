9ac81f343e5157b22c8bbb7d66994d9b49efddb2
Rafa91as <refael.assor@gmail.com>
2021-06-28 21:12:31 +0300

    push

--- 02-JavaScript/99-projects/02-js-free-project/refael/dist/users.js
+++ 02-JavaScript/99-projects/02-js-free-project/refael/dist/users.js
@@ -1,4 +1,17 @@
+var User = /** @class */ (function () {
+    function User(name, age, gender, desc, intrested) {
+        this.id = "id" + Math.random();
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.desc = desc;
+        this.intrested = intrested;
+    }
+    return User;
+}());
 var users = JSON.parse(localStorage.getItem("creatUsers"));
+console.log(users);
+console.log(users);
 var usersHTML = users.map(function (user) {
     var userName = user.name;
     var userAge = user.age;
@@ -7,5 +20,18 @@ var usersHTML = users.map(function (user) {
     var userIntrested = user.intrested;
     var userId = user.id;
     var container = document.querySelector(".container");
-    container.innerHTML += "<div class=\"user\"><h1><a href=\"user.html?userId=" + userId + "\">Profile: " + userName + " is a " + userAge + " years old " + userGender + " who likes to " + userDesc + " and intresten in " + userIntrested + "</a></h1></div>";
+    container.innerHTML += "<div class=\"user\"><h1><a href=\"user.html?userId=" + userId + "\">Profile:\n   " + userName + " is a " + userAge + " years old " + userGender + " who describes himself as " + userDesc + " and intresten in\n   " + userIntrested + "</a></h1><button class=\"removeFromDOM\">Remove ID:" + userId + "</button></div>";
+    var removeBTN = document.querySelectorAll(".removeFromDOM");
+    var _loop_1 = function (button) {
+        button.addEventListener("click", function (event) {
+            if ((button.innerHTML === "Remove ID:", "" + userId)) {
+                button.parentElement.remove();
+            }
+        });
+    };
+    for (var _i = 0, removeBTN_1 = removeBTN; _i < removeBTN_1.length; _i++) {
+        var button = removeBTN_1[_i];
+        _loop_1(button);
+    }
 });
+// button.setAttribute("alt", userId)
--- 02-JavaScript/99-projects/02-js-free-project/refael/users.ts
+++ 02-JavaScript/99-projects/02-js-free-project/refael/users.ts
@@ -1,4 +1,22 @@
+class User {
+  name: string;
+  age: number;
+  gender: string;
+  desc: string;
+  intrested: string;
+  id: string = "id" + Math.random();
+  constructor(name, age, gender, desc, intrested) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.desc = desc;
+    this.intrested = intrested;
+  }
+}
+
 let users = JSON.parse(localStorage.getItem("creatUsers"));
+console.log(users);
+console.log(users);
 
 let usersHTML = users.map((user) => {
   let userName = user.name;
@@ -8,5 +26,17 @@ let usersHTML = users.map((user) => {
   let userIntrested = user.intrested;
   let userId = user.id;
   const container = document.querySelector(".container");
-  container.innerHTML += `<div class="user"><h1><a href="user.html?userId=${userId}">Profile: ${userName} is a ${userAge} years old ${userGender} who likes to ${userDesc} and intresten in ${userIntrested}</a></h1></div>`;
+  container.innerHTML += `<div class="user"><h1><a href="user.html?userId=${userId}">Profile:
+   ${userName} is a ${userAge} years old ${userGender} who describes himself as ${userDesc} and intresten in
+   ${userIntrested}</a></h1><button class="removeFromDOM">Remove ID:${userId}</button></div>`;
+  const removeBTN = document.querySelectorAll(".removeFromDOM");
+  for (const button of removeBTN) {
+    button.addEventListener("click", (event) => {
+      if ((button.innerHTML === "Remove ID:", `${userId}`)) {
+        button.parentElement.remove();
+      }
+    });
+  }
 });
+
+// button.setAttribute("alt", userId)
