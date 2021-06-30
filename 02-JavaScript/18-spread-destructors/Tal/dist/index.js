2d24d790b327b0104bd0558a657114eff167606e
Tal <tal.yaron@gmail.com>
2021-06-30 19:48:41 +0300

    functions deconstructing

--- 02-JavaScript/18-spread-destructors/Tal/dist/index.js
+++ 02-JavaScript/18-spread-destructors/Tal/dist/index.js
@@ -10,3 +10,14 @@ var name2 = person.name2, street = person.street;
 console.log(name2, street);
 var firstCity = cities[0], secondCity = cities[1];
 console.log(firstCity, secondCity);
+var residents = [{ name: 'aa', street: 'Hanevel' }, { name: 'bb', street: 'blo' }, { name: 'cc', street: 'Bla' }];
+var rs1 = residents[0], rs2 = residents[1], rs3 = residents[2];
+function addToResidents(_a) {
+    var name = _a.name, street = _a.street;
+    residents.push({ name: name, street: street });
+}
+//create a function which get height, width, deepth (all of type number). the function returns the qubick volume
+//which equals to height * width * deepth
+//do it with destucotr
+addToResidents({ name: "Yossi", street: "Borchov" });
+console.log(residents);
