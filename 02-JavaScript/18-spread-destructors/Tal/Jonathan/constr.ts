100efa74e8891d74e1b45f9cdabb0be5c1c86376
Jonathan Eliel Nisenbaum <47113013+jnisen@users.noreply.github.com>
2021-06-30 20:17:08 +0300

    comit
    adas

--- 02-JavaScript/18-spread-destructors/Tal/Jonathan/constr.ts
+++ 02-JavaScript/18-spread-destructors/Tal/Jonathan/constr.ts
@@ -1,21 +1,21 @@
 const person = {
-    name2:"Dan",
+    name2: "Dan",
     street: "Herzel"
 }
 
-const {name2, street} = person;
+const { name2, street } = person;
 
 console.log("name:", name2)
 console.log("street", street)
 
 
 
-let [fi,se] = "John S".split(' ')
+let [fi, se] = "John S".split(' ')
 
 
-let x,y;
-[x,y] = [10,20]
-console.log(x+y)
+let x, y;
+[x, y] = [10, 20]
+console.log(x + y)
 
 
 
@@ -25,7 +25,26 @@ console.log(x+y)
 
 
 
-const volume{
-    height:120,
-    
+const volume = {
+    height: 1,
+    width: 2,
+    depth: 3
 }
+
+const { height, width, depth } = volume
+
+function calculateVolume({height,width,depth}:{height:number,width:number,depth:number}):number {
+    const volume: number = height * width * depth;
+    return volume;
+}
+
+
+
+
+const BoardVolumeRoot:HTMLElement = document.querySelector('#boardVolume')
+
+let html:string = ""
+
+html+= `<div>The volume of the cubick area is: ${calculateVolume({height, width, depth})}</div>`
+
+BoardVolumeRoot.innerHTML = html
