0f1b85dcfcdca836f69388542803a00c304be98e
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-06-30 20:00:30 +0300

    .

---
+++ 02-JavaScript/18-spread-destructors/Yaniv/script.ts
@@ -0,0 +1,25 @@
+//create a function which get height, width, deepth (all of type number). the function returns the qubick volume
+//which equals to height * width * deepth
+//do it with destucotr
+
+interface ThreeD {
+    h: number,
+    w: number,
+    d: number
+}
+
+const getVol = (dimentions: ThreeD) : number => {
+    try {
+        const {h, w, d} = dimentions
+        const volume = h*w*d;
+        return volume;
+    } catch (error) {
+        
+    }
+}
+
+const myCube: ThreeD = {h:10,w:5,d:8};
+console.log(getVol(myCube));
+
+const mySecondCube: ThreeD = {h:3,w:19,d:6};
+console.log(getVol(mySecondCube));
