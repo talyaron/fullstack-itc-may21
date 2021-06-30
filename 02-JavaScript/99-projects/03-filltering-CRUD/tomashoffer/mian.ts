5d3cca874a5ab737d9779c269ca07079879ba975
tomashoffer <tomashoffer96@gmail.com>
2021-06-30 17:11:10 +0300

    responsive crud

--- 02-JavaScript/99-projects/03-filltering-CRUD/tomashoffer/mian.ts
+++ 02-JavaScript/99-projects/03-filltering-CRUD/tomashoffer/mian.ts
@@ -1,5 +1,5 @@
 // QUERIES SELECTORS
-const select:any = document.querySelector(".select");
+const select: any = document.querySelector(".select");
 const boton = document.querySelector(".boton_trash");
 const editBtn = document.getElementById('nameInput');
 const modal = document.getElementById('Mymodal')
@@ -39,7 +39,7 @@ class Producto {
 }
 
 // EVENTS LISTENERS
-select.addEventListener('change', selectFilter); 
+select.addEventListener('change', selectFilter);
 searchBar.addEventListener('keyup', search);
 
 // FUNCTIONS
@@ -60,13 +60,13 @@ function remove(id) {
 }
 
 // TAKING ID FOR EDIT, THEN I USED AT THE handleEdit()
-function edit(id){ 
+function edit(id) {
     updateID = id;
 }
 
 // FILTER BY SELECTS OPTIONS [see event]
-function selectFilter(){
-    try{
+function selectFilter() {
+    try {
         const selectedValue = Number(select.value);
         if (selectedValue > 300) throw new Error('You have an error in your value');
         console.log(typeof selectedValue);
@@ -76,38 +76,38 @@ function selectFilter(){
         arr2 = list;
         let arr3 = [];
         arr3 = list;
-    if(selectedValue === 100){
-        let priceFilter = arr.filter((prod)=>prod.price >= selectedValue);
-        arr = priceFilter;
-        renderData(arr);
-    }
-    else if(selectedValue === 200){
-        let priceFilter1 = arr2.filter((prod)=>prod.price >= selectedValue);
-        arr2 = priceFilter1;
-        renderData(arr2);
-    }
-    else if(selectedValue === 0){
-        renderData(list);
-    }
-    else{
-        let priceFilter2 = arr3.filter((prod)=>prod.price >= selectedValue);
-        arr3 = priceFilter2;
-        renderData(arr3);
-    }
-     }catch(e){
+        if (selectedValue === 100) {
+            let priceFilter = arr.filter((prod) => prod.price >= selectedValue);
+            arr = priceFilter;
+            renderData(arr);
+        }
+        else if (selectedValue === 200) {
+            let priceFilter1 = arr2.filter((prod) => prod.price >= selectedValue);
+            arr2 = priceFilter1;
+            renderData(arr2);
+        }
+        else if (selectedValue === 0) {
+            renderData(list);
+        }
+        else {
+            let priceFilter2 = arr3.filter((prod) => prod.price >= selectedValue);
+            arr3 = priceFilter2;
+            renderData(arr3);
+        }
+    } catch (e) {
         console.error(e);
     }
 }
- 
+
 // TAKING VALUE FROM INPUT FOR USE IT AT searchRegEx()
-function search(e){
-    try{
-        searchValue = e.target.value;  
-        searchRegEx(searchValue); 
-       }catch(e){
-           console.error(e);
-       }
+function search(e) {
+    try {
+        searchValue = e.target.value;
+        searchRegEx(searchValue);
+    } catch (e) {
+        console.error(e);
     }
+}
 
 // RegEx FOR FILTER BY PRODUCTS NAME
 function searchRegEx(inputSearch: string) {
@@ -138,8 +138,9 @@ function renderData(listado) {
                     <p>${element.description}</p>
                 </div>
                 <div class="container-icons item">
-                <button href="#" class="btn_icons" onclick='remove("${element.id}")'><i class="fas fa-trash icono_productos icono_productos_delete"></i></button>
-                <button href="#" class="btn_icons" data-toggle="modal" data-target="#myModal" onclick='edit("${element.id}")'><i class="fas fa-edit icono_productos icono_productos_edit"></i></button>
+                    <button href="#" class="btn_icons" onclick='remove("${element.id}")'><i class="fas fa-trash icono_productos icono_productos_delete"></i></button>
+                    
+                    <button href="#" class="btn_icons" data-toggle="modal" data-target="#myModal" onclick='edit("${element.id}")'><i class="fas fa-edit icono_productos icono_productos_edit"></i></button>
                 </div>`
     });
     container.innerHTML = html;
@@ -148,51 +149,51 @@ renderData(list);
 
 // HANDLE DATA FROM PRINCIPAL FORM
 const handleSubmit = (event) => {
-    try{
-    event.preventDefault();
+    try {
+        event.preventDefault();
 
-    const product: string = event.target.elements.producto.value;
-    const brand: string = event.target.elements.brand.value;
-    const price: number = event.target.elements.price.valueAsNumber;
-    const stock: number = event.target.elements.stock.valueAsNumber;
-    const description: string = event.target.elements.description.value;
+        const product: string = event.target.elements.producto.value;
+        const brand: string = event.target.elements.brand.value;
+        const price: number = event.target.elements.price.valueAsNumber;
+        const stock: number = event.target.elements.stock.valueAsNumber;
+        const description: string = event.target.elements.description.value;
 
 
-    const producto = new Producto(product, brand, price, stock, description);
-    // ADD NEW PRODUCT TO LIST ARRAY
-    list.unshift(producto);
-    renderData(list);
-    console.log(producto);
+        const producto = new Producto(product, brand, price, stock, description);
+        // ADD NEW PRODUCT TO LIST ARRAY
+        list.unshift(producto);
+        renderData(list);
+        console.log(producto);
 
-    event.target.reset(); 
-    }catch(e){
+        event.target.reset();
+    } catch (e) {
         console.error(e);
     }
 };
 
 // HANDLEING DATA FROM MODAL TO EDIT
 const handleEdit = (event) => {
-    try{
-    event.preventDefault();
+    try {
+        event.preventDefault();
 
-    const productModal: string = event.target.elements.productoModal.value;
-    const brandModal: string = event.target.elements.brandModal.value;
-    const priceModal: number = event.target.elements.priceModal.valueAsNumber;
-    const stockModal: number = event.target.elements.stockModal.valueAsNumber;
-    const descriptionModal: string = event.target.elements.descriptionModal.value;
+        const productModal: string = event.target.elements.productoModal.value;
+        const brandModal: string = event.target.elements.brandModal.value;
+        const priceModal: number = event.target.elements.priceModal.valueAsNumber;
+        const stockModal: number = event.target.elements.stockModal.valueAsNumber;
+        const descriptionModal: string = event.target.elements.descriptionModal.value;
 
-    const edit = list.find((prod)=> prod.id === updateID);
-    edit.product = productModal;
-    edit.brand = brandModal;
-    edit.price = priceModal;
-    edit.stock = stockModal;
-    edit.description = descriptionModal;
-    
+        const edit = list.find((prod) => prod.id === updateID);
+        edit.product = productModal;
+        edit.brand = brandModal;
+        edit.price = priceModal;
+        edit.stock = stockModal;
+        edit.description = descriptionModal;
 
-    console.log(productModal, brandModal, priceModal, stockModal, descriptionModal);
-    console.log(edit);
-    renderData(list);
-    }catch(e){
+
+        console.log(productModal, brandModal, priceModal, stockModal, descriptionModal);
+        console.log(edit);
+        renderData(list);
+    } catch (e) {
         console.error(e);
     }
 }
