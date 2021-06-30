6bb47d127a5bd959049bcee3d02b603c4b286660
RazielAF <82554306+RazielAF@users.noreply.github.com>
2021-06-30 19:45:25 +0300

    עי

---
+++ 02-JavaScript/18-spread-destructors/Raziel/dist/index.js
@@ -0,0 +1,10 @@
+var city = {
+    name2: 'Holon',
+    population: "165000",
+    age: "70",
+    streets: ["eilat", "sokolov", "reafel eitan"]
+};
+var name2 = city.name2, age = city.age;
+console.log(name2, age);
+var streets = city.streets;
+console.log(streets);
