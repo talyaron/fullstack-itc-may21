61ec894a35d601a05cf5e8b29ec86a96f067de9f
leomza <leo_mza1991@hotmail.com>
2021-06-30 20:39:23 +0300

    Exercise

--- 02-JavaScript/18-spread-destructors/Tal/Leonardo/dist/script.js
+++ 02-JavaScript/18-spread-destructors/Tal/Leonardo/dist/script.js
@@ -27,3 +27,13 @@ var eq1 = equipos[0], eq2 = equipos[1], eq5 = equipos[4];
 console.log(eq1.name, eq2.name, eq5.name);
 var nuevaVariable = eq1.posicion;
 console.log('I create this new variable:', nuevaVariable);
+//create a function which get height, width, deep (all of type number). the function returns the quick volume
+//which equals to height * width * deepth
+//do it with destructor
+function getVolume(height, width, deep) {
+    return height * width * deep;
+}
+;
+var variables = { height: 15, width: 10, deep: 20 };
+var height = variables.height, width = variables.width, deep = variables.deep;
+console.log(getVolume(height, width, deep));
