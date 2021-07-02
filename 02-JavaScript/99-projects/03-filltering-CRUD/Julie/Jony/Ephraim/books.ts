33ab95be9ee4891c1cc046675e3162ba94714e6e
Julie Chajes <78927901+Juliechajes@users.noreply.github.com>
2021-07-02 09:42:07 +0300

    Friday Morning

---
+++ 02-JavaScript/99-projects/03-filltering-CRUD/Julie/Jony/Ephraim/books.ts
@@ -0,0 +1,76 @@
+const booksData: Array<any> = [
+  {
+    author: "Chinua Achebe",
+    country: "Nigeria",
+    title: "Things Fall Apart",
+    year: 1958,
+    bookId: "1",
+  },
+  {
+    author: "Hans Christian Andersen",
+    country: "Denmark",
+    title: "Fairy tales",
+    year: 1836,
+    bookId: "2",
+  },
+
+  {
+    author: "Jane Austen",
+    country: "United Kingdom",
+    title: "Pride and Prejudice",
+    year: 1813,
+    bookId: "3",
+  },
+
+  {
+    author: "Samuel Beckett",
+    country: "Republic of Ireland",
+    title: "Molloy, Malone Dies, The Unnamable, the trilogy",
+    year: 1952,
+    bookId: "4",
+  },
+  {
+    author: "Jorge Luis Borges",
+    country: "Argentina",
+    title: "Ficciones",
+    year: 1965,
+    bookId: "5",
+  },
+
+  {
+    author: "Albert Camus",
+    country: "Algeria, French Empire",
+    title: "The Stranger",
+    year: 1942,
+    bookId: "6",
+  },
+  {
+    author: "Paul Celan",
+    country: "Romania, France",
+    title: "Poems",
+    year: 1952,
+    bookId: "7",
+  },
+
+  {
+    author: "Miguel de Cervantes",
+    country: "Spain",
+    title: "Don Quijote De La Mancha",
+    year: 1610,
+    bookId: "8",
+  },
+  {
+    author: "Geoffrey Chaucer",
+    country: "England",
+    title: "The Canterbury Tales",
+    year: 1450,
+    bookId: "9",
+  },
+  {
+    author: "Joseph Conrad",
+    country: "United Kingdom",
+    title: "Nostromo",
+    year: 1904,
+    bookId: "10",
+  },
+];
