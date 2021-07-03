ebbca5e945603a62662d84f359d0c43937886561
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2021-07-04 00:03:48 +0300

    p

---
+++ 02-JavaScript/99-projects/04-Whatsapp/Tomas-Yarden/TS/chat.ts
@@ -0,0 +1,75 @@
+const messageValue: any = document.getElementById("text-input");
+
+class Chat {
+  message: string;
+
+  constructor(message: string) {
+    this.message = message;
+  }
+}
+
+// class Bot{
+//   messageBot:string;
+//   constructor(messageBot:string) {
+//     this.messageBot = messageBot;
+//   }
+// }
+
+class ChatProfile {
+  arrayChat: Array<Chat> = [];
+  // arrayBot:Array<Bot> =[];
+
+  add(add: Chat) {
+    this.arrayChat.push(add);
+    this.renderUser();
+    this.respondBot();
+  }
+
+  renderUser() {
+    const getUser: HTMLElement = document.querySelector("#chat-box");
+
+    let html: string = "";
+    console.log(this.arrayChat);
+    this.arrayChat.forEach((element) => {
+      html += `
+      <div class="userText">
+      <span>${element.message}</span>
+      </div>
+     
+     `;
+    });
+
+    getUser.innerHTML = html;
+  }
+  respondBot() {
+    const respon = String(messageValue.value);
+    const getBot: HTMLElement = document.querySelector("#chat-box");
+    let htmlBot = "";
+    this.arrayChat.forEach(() => {
+      if (respon === "Hola") {
+        htmlBot += `
+        <div class="botText">
+        <span>Hola</span>
+        </div>
+        `;
+      }
+    });
+
+    setTimeout(() => {
+      getBot.innerHTML = htmlBot;
+    }, 1000);
+  }
+}
+
+const chatProfile = new ChatProfile();
+
+const sendBtn = (event) => {
+  event.preventDefault();
+  const message = event.target.elements.message.value;
+
+  const generate = new Chat(message);
+  console.log(generate);
+
+  chatProfile.add(generate);
+  event.target.reset();
+};
