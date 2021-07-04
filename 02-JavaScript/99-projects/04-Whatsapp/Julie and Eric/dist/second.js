1ffa76630ebe2d34d606406b1054e10fa2827aee
Eric Kobrinsky <78773449+erickobrinsky@users.noreply.github.com>
2021-07-04 17:52:16 +0300

    wsp

--- 02-JavaScript/99-projects/04-Whatsapp/Julie and Eric/dist/second.js
+++ 02-JavaScript/99-projects/04-Whatsapp/Julie and Eric/dist/second.js
@@ -1,3 +1,5 @@
 //input render (take input class from the input inside the form in the second page)
 var convers = document.querySelector(".inputTyping");
+//render contacts 
+var contactZone = document.querySelector(".contactZone");
 //render what you type inside the input to the conversation space
