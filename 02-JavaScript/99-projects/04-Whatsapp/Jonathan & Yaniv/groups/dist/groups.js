082be51288d7ca595c69b6f5a354d86fd50b2595
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-07-04 20:20:30 +0300

    .

--- 02-JavaScript/99-projects/04-Whatsapp/Jonathan & Yaniv/groups/dist/groups.js
+++ 02-JavaScript/99-projects/04-Whatsapp/Jonathan & Yaniv/groups/dist/groups.js
@@ -1,5 +1,40 @@
-var loggedInUser = /** @class */ (function () {
-    function loggedInUser() {
+var LoggedInUser = /** @class */ (function () {
+    function LoggedInUser(userImg, userName, userPhone, userGroups) {
+        this.userImg = userImg;
+        this.userName = userName;
+        this.userPhone = userPhone;
+        this.userGroups = userGroups;
     }
-    return loggedInUser;
+    return LoggedInUser;
 }());
+var ContactList = /** @class */ (function () {
+    function ContactList() {
+    }
+    ContactList.prototype.findContact = function (contactPhone) {
+        var contact = this.allContacts.find(function (contactItem) { return contactItem.userPhone === contactPhone; });
+        return contact;
+    };
+    return ContactList;
+}());
+var allContacts = JSON.parse(localStorage.getItem('contactList'));
+var loggedInUser = JSON.parse(localStorage.getItem('currentUser'));
+var usersContainer = document.querySelector('.users');
+usersContainer.addEventListener('click', function (ev) { return userPicker(ev); });
+var userPicker = function (ev) {
+    if (ev.target.className === 'users')
+        return;
+    var userContainer;
+    if (ev.target.id.indexOf('user_') === -1)
+        userContainer = ev.target;
+    else
+        userContainer = ev.target.parentElement;
+    var userImg = userContainer.querySelector("#user_img").getAttribute('src');
+    var userNameContainer = userContainer.querySelector("#user_name");
+    var userName = userNameContainer.innerText;
+    var userPhoneContainer = userContainer.querySelector("#user_phone");
+    var userPhone = userPhoneContainer.innerText;
+    var userGroups = allContacts.findContact(userPhone).userGroups;
+    var pickedUser = new User(userImg, userName, userPhone, userGroups);
+    localStorage.setItem('currentUser', JSON.stringify(pickedUser));
+    window.location.href = "../groups/groups.html?" + pickedUser.userPhone;
+};
--- 02-JavaScript/99-projects/04-Whatsapp/Jonathan & Yaniv/groups/groups.ts
+++ 02-JavaScript/99-projects/04-Whatsapp/Jonathan & Yaniv/groups/groups.ts
@@ -1,3 +1,57 @@
-class loggedInUser {
+class LoggedInUser {
+    userImg: string;
+    userName: string;
+    userPhone: string;
+    userGroups: Array<string>;
+
+    constructor (userImg: string, userName: string, userPhone: string, userGroups: Array<string>) {
+        this.userImg = userImg;
+        this.userName = userName;
+        this.userPhone = userPhone;
+        this.userGroups = userGroups;
+    }
+}
+
+class ContactList {
+    allContacts: Array<User>;
+
+    findContact(contactPhone) {
+        const contact = this.allContacts.find(contactItem => contactItem.userPhone === contactPhone);
+        return contact;
+    }
+}
+
+const allContacts: ContactList = JSON.parse(localStorage.getItem('contactList'));
+
+const loggedInUser: LoggedInUser = JSON.parse(localStorage.getItem('currentUser'));
+
+
+
+
+
+const usersContainer: HTMLElement = document.querySelector('.users');
+
+usersContainer.addEventListener('click', ev => userPicker(ev));
+
+const userPicker = (ev: any) : void => {
+
+    if (ev.target.className === 'users') return;
     
+    let userContainer: HTMLElement;
+    if (ev.target.id.indexOf('user_') === -1) userContainer = ev.target;
+    else userContainer = ev.target.parentElement;
+    
+    const userImg: string = userContainer.querySelector("#user_img").getAttribute('src');
+    const userNameContainer: HTMLElement = userContainer.querySelector("#user_name");
+    const userName: string = userNameContainer.innerText;
+    const userPhoneContainer: HTMLElement = userContainer.querySelector("#user_phone");
+    const userPhone: string = userPhoneContainer.innerText;    
+    const userGroups: Array<string> = allContacts.findContact(userPhone).userGroups;
+    
+    const pickedUser: User = new User(userImg, userName, userPhone, userGroups);
+
+    localStorage.setItem('currentUser',JSON.stringify(pickedUser));
+
+    window.location.href = `../groups/groups.html?${pickedUser.userPhone}`;
+
 }
