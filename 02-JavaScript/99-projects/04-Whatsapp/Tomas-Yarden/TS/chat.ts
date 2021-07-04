6ebbb361b535a4b78c59952546d7afa11314da44
Tal <tal.yaron@gmail.com>
2021-07-04 19:09:09 +0300

    some error fixes

--- 02-JavaScript/99-projects/04-Whatsapp/Tomas-Yarden/TS/chat.ts
+++ 02-JavaScript/99-projects/04-Whatsapp/Tomas-Yarden/TS/chat.ts
@@ -24,84 +24,88 @@ class ChatProfile {
   // arrayBot:Array<Bot> =[];
 
   add(add: Chat) {
+    console.log('new chat')
     this.arrayChat.push(add);
     this.renderUser();
     // this.respondBot();
   }
-// addBot(addBot:Array<Chat|RespondsInterface>){
-// addBot.forEach((element) => {
-//   const res = new Chat(element.message)
-//   this.arrayChat.push(res);
-// });
+  // addBot(addBot:Array<Chat|RespondsInterface>){
+  // addBot.forEach((element) => {
+  //   const res = new Chat(element.message)
+  //   this.arrayChat.push(res);
+  // });
 
-// this.renderUser()
-// }
+  // this.renderUser()
+  // }
   renderUser() {
     const getUser: HTMLElement = document.querySelector("#chat-box");
-    const respon = String(messageValue.value);
+    // const respon = String(messageValue.value);
     let html: string = "";
     console.log(this.arrayChat);
     this.arrayChat.forEach((element) => {
+
+      console.log(element.message)
+
+
       html += `
       <p class="userText">
       <span>${element.message}</span>
       </p>
      
-     `;
-     if (respon.includes("Hola")) {
-      html += `
+     `
+      if (element.message.includes("Hola")) {
+        html += `
            <p class="botText">
             <span>Hola</span>
              </p>
              `;
-         
-        }else if (respon.includes("Como Estas?")) {
-          html += `
-               <p class="botText">
-                <span>Bien</span>
-                 </p>
-                 `;
-             
-            }
-      
-    });
-    setTimeout(() => {
-                  
-      getUser.innerHTML = html;
-                 }, 1000);
+      }
+      else if (element.message.includes("Como Estas?")) {
+        html += `
+                 <p class="botText">
+                  <span>Bien</span>
+                   </p>
+                   `;
 
+      }
+      setTimeout(() => {
+
+        getUser.innerHTML = html;
+      }, 1000);
+
+    })
+
+
+    // renderBot(){
+    //   const respon = String(messageValue.value);
+
+    //   if(respon === 'Hola') {
+    // this.arrayChat = this.arrayChat.filter((element) => element.message ===respon)
+    // this.renderUser()
+    //   }
+    // }
+
+    // respondBot() {
+    //   const respon = String(messageValue.value);
+    //   // const arrLength = this.arrayChat.length
+    //   const getBot: HTMLElement = document.querySelector(".botText");
+    //   let htmlBot = "";
+    //   this.arrayChat.forEach(() => {
+    //     if (respon.includes("Hola")) {
+    //       htmlBot += `
+    //       <div class="botText">
+    //       <span>Hola</span>
+    //       </div>
+    //       `;
+
+    //     }
+    //   });
+
+    //   setTimeout(() => {
+    //     getBot.innerHTML = htmlBot;
+    //   }, 1000);
+    // }
   }
-
-
-// renderBot(){
-//   const respon = String(messageValue.value);
-
-//   if(respon === 'Hola') {
-// this.arrayChat = this.arrayChat.filter((element) => element.message ===respon)
-// this.renderUser()
-//   }
-// }
-
-  // respondBot() {
-  //   const respon = String(messageValue.value);
-  //   // const arrLength = this.arrayChat.length
-  //   const getBot: HTMLElement = document.querySelector(".botText");
-  //   let htmlBot = "";
-  //   this.arrayChat.forEach(() => {
-  //     if (respon.includes("Hola")) {
-  //       htmlBot += `
-  //       <div class="botText">
-  //       <span>Hola</span>
-  //       </div>
-  //       `;
-        
-  //     }
-  //   });
-
-  //   setTimeout(() => {
-  //     getBot.innerHTML = htmlBot;
-  //   }, 1000);
-  // }
 }
 
 const chatProfile = new ChatProfile();
@@ -116,3 +120,4 @@ const sendBtn = (event) => {
   chatProfile.add(generate);
   event.target.reset();
 };
+
