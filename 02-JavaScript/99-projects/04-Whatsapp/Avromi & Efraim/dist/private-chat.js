6b161bffa359dfd0cd957b342e302f46b96bd55c
Avromi Russell <83745803+avromiRussell@users.noreply.github.com>
2021-07-04 13:07:52 +0300

    great working version

--- 02-JavaScript/99-projects/04-Whatsapp/Avromi & Efraim/dist/private-chat.js
+++ 02-JavaScript/99-projects/04-Whatsapp/Avromi & Efraim/dist/private-chat.js
@@ -9,11 +9,15 @@ var allContacts = JSON.parse(localStorage.getItem('contacts'));
 var thisContact = allContacts.filter(function (contact) { return contact.contactId === params.contactId; });
 var contactName = document.querySelector(".nav__contact h2");
 contactName.innerHTML = "" + thisContact[0].name;
+function updateLastSent() {
+    var lastSent = document.querySelector(".nav__contact p");
+    lastSent.innerHTML = "Last Sent: " + thisContact[0].chats[0].timeStamp;
+}
 console.log(thisContact);
 // const textInput = document.querySelector("#input")
 // textInput.addEventListener("change", myClassChange);
-// function myClassChange() {
-//     console.log("Hello");
+// function myClassChange(event) {
+//     console.log(event.key);
 //     const sendButton:HTMLElement = document.querySelector('.sendButton')
 //     sendButton.style.display === "none"
 // }
@@ -21,6 +25,16 @@ var form = document.querySelector('.form');
 form.addEventListener('submit', logSubmit);
 function logSubmit(event) {
     var message = event.target.elements.message.value;
-    console.log("Time stamp: " + event.timeStamp + message);
+    var timeStamp = new Date();
+    var obj = {};
+    obj["message"] = message;
+    obj["timeStamp"] = timeStamp;
+    thisContact[0].chats.unshift(obj);
+    console.log("Time stamp: timeStamp" + message);
     event.preventDefault();
+    event.target.reset();
+    updateLastSent();
+    console.log(thisContact);
+    console.log(allContacts);
+    localStorage.setItem('contacts', JSON.stringify(allContacts));
 }
