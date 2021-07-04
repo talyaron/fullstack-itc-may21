23f06f083a987ac190dc70e012567e5db12b67c3
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-07-04 17:18:08 +0300

    .

--- 02-JavaScript/99-projects/04-Whatsapp/Jonathan & Yaniv/users/dist/users.js
+++ 02-JavaScript/99-projects/04-Whatsapp/Jonathan & Yaniv/users/dist/users.js
@@ -1,9 +1,49 @@
 var User = /** @class */ (function () {
+    // userGroups: Array<string> = JSON.parse(localStorage.getItem("currentRunner")).runnerId; 
     function User(userImg, userName, userPhone) {
-        this.userId = "user" + Math.random().toString(16).slice(2);
+        this.userImg = userImg;
+        this.userName = userName;
+        this.userPhone = userPhone;
     }
     return User;
 }());
+var ContactList = /** @class */ (function () {
+    function ContactList(allContacts) {
+        var _this = this;
+        if (allContacts === null) {
+            this.allContacts = [];
+            var userItemsElements = document.querySelectorAll('.users__item');
+            var contactToPush_1;
+            userItemsElements.forEach(function (contact) {
+                var contactImg = contact.firstChild.parentElement.querySelector("#user_img").getAttribute('src');
+                var contactNameContainer = contact.firstChild.parentElement.querySelector("#user_name");
+                var contactName = contactNameContainer.innerText;
+                var contactPhoneContainer = contact.firstChild.parentElement.querySelector("#user_phone");
+                var contactPhone = contactPhoneContainer.innerText;
+                contactToPush_1 = new User(contactImg, contactName, contactPhone);
+                _this.allContacts.push(contactToPush_1);
+            });
+            return;
+        }
+        this.allContacts = allContacts;
+    }
+    ContactList.prototype.findContact = function (contactPhone) {
+        var contact = this.allContacts.find(function (contactItem) { return contactItem.userPhone === contactPhone; });
+        return contact;
+    };
+    ContactList.prototype.addContact = function (contact) {
+        this.allContacts.push(contact);
+    };
+    return ContactList;
+}());
+console.log(JSON.parse(localStorage.getItem('contactList')));
+var allContacts;
+if (JSON.parse(localStorage.getItem('contactList')) !== null)
+    allContacts = new ContactList(JSON.parse(localStorage.getItem('ContactList')).allContacts);
+else {
+    allContacts = new ContactList(null);
+    localStorage.setItem('contactList', JSON.stringify(allContacts));
+}
 var usersContainer = document.querySelector('.users');
 usersContainer.addEventListener('click', function (ev) { return userPicker(ev); });
 var userPicker = function (ev) {
@@ -19,8 +59,6 @@ var userPicker = function (ev) {
     var userName = userNameContainer.innerText;
     var userPhoneContainer = userContainer.querySelector("#user_phone");
     var userPhone = userPhoneContainer.innerText;
-    console.log(userImg, userName, userPhone);
     var pickedUser = new User(userImg, userName, userPhone);
-    userContainer.setAttribute('id', pickedUser.userId);
-    // localStorage.setItem('currentUser',)
+    localStorage.setItem('currentUser', JSON.stringify(pickedUser));
 };
--- 02-JavaScript/99-projects/04-Whatsapp/Jonathan & Yaniv/users/users.ts
+++ 02-JavaScript/99-projects/04-Whatsapp/Jonathan & Yaniv/users/users.ts
@@ -2,11 +2,56 @@ class User {
     userImg: string;
     userName: string;
     userPhone: string;
-    userId: string = "user" + Math.random().toString(16).slice(2);
+    // userGroups: Array<string> = JSON.parse(localStorage.getItem("currentRunner")).runnerId; 
 
     constructor (userImg: string, userName: string, userPhone: string) {
-
+        this.userImg = userImg;
+        this.userName = userName;
+        this.userPhone = userPhone;
     }
+}
+
+class ContactList {
+    allContacts: Array<User>;
+
+    constructor (allContacts: Array<User>) {
+        if (allContacts === null) {
+            this.allContacts = []; 
+            const userItemsElements: NodeList = document.querySelectorAll('.users__item');
+            let contactToPush: User;
+            userItemsElements.forEach(contact => {
+                const contactImg: string = contact.firstChild.parentElement.querySelector("#user_img").getAttribute('src');
+                const contactNameContainer: HTMLElement = contact.firstChild.parentElement.querySelector("#user_name");
+                const contactName: string = contactNameContainer.innerText;
+                const contactPhoneContainer: HTMLElement = contact.firstChild.parentElement.querySelector("#user_phone");
+                const contactPhone: string = contactPhoneContainer.innerText;    
+
+                contactToPush = new User(contactImg, contactName, contactPhone);
+                
+                this.allContacts.push(contactToPush);
+            });
+            return;
+        }
+        this.allContacts = allContacts;
+    }
+
+    findContact(contactPhone) {
+        const contact = this.allContacts.find(contactItem => contactItem.userPhone === contactPhone);
+        return contact;
+    }
+
+    addContact(contact: User) {
+        this.allContacts.push(contact);
+    }
+}
+
+console.log(JSON.parse(localStorage.getItem('contactList')));
+
+let allContacts: ContactList;
+if (JSON.parse(localStorage.getItem('contactList')) !== null) allContacts = new ContactList(JSON.parse(localStorage.getItem('ContactList')).allContacts);
+else {
+    allContacts = new ContactList(null);
+    localStorage.setItem('contactList',JSON.stringify(allContacts));
 }
 
 const usersContainer: HTMLElement = document.querySelector('.users');
@@ -16,6 +61,7 @@ usersContainer.addEventListener('click', ev => userPicker(ev));
 const userPicker = (ev: any) : void => {
 
     if (ev.target.className === 'users') return;
+    
     let userContainer: HTMLElement;
     if (ev.target.id.indexOf('user_') === -1) userContainer = ev.target;
     else userContainer = ev.target.parentElement;
@@ -24,11 +70,9 @@ const userPicker = (ev: any) : void => {
     const userNameContainer: HTMLElement = userContainer.querySelector("#user_name");
     const userName: string = userNameContainer.innerText;
     const userPhoneContainer: HTMLElement = userContainer.querySelector("#user_phone");
-    const userPhone: string = userPhoneContainer.innerText;
-    console.log(userImg,userName,userPhone);
+    const userPhone: string = userPhoneContainer.innerText;    
     
-    const pickedUser = new User(userImg, userName, userPhone);
-    userContainer.setAttribute('id',pickedUser.userId);
+    const pickedUser: User = new User(userImg, userName, userPhone);
 
-    // localStorage.setItem('currentUser',)
+    localStorage.setItem('currentUser',JSON.stringify(pickedUser));
 }
