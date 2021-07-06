616ae13af015545f84342490f1d12d444da7e949
Julie Chajes <78927901+Juliechajes@users.noreply.github.com>
2021-07-06 12:23:20 +0300

    Julie's folder

---
+++ 02-JavaScript/99-projects/04-Whatsapp/Eric and Julie (Julie)/second.ts
@@ -0,0 +1,84 @@
+const savedContacts = JSON.parse(localStorage.getItem("contactsData"));
+console.log(savedContacts);
+const conversation = <HTMLInputElement>(
+  document.querySelector(".envelope__typing--input-entry")
+);
+
+console.log(window.location.href);
+const currentUrl = window.location.href;
+const idIndex = currentUrl.indexOf("id=");
+const id = currentUrl.slice(idIndex + 3);
+
+// Understand this bit
+
+const currentUser = savedContacts.find((contact) => contact.contactId === id);
+
+const chatContainer: any = document.querySelector(".envelope__chat");
+
+const send = <HTMLInputElement>document.querySelector("#send");
+
+send.addEventListener("click", sendMessage);
+
+function sendMessage() {
+  const messagesArray = [];
+  const message = conversation.value;
+  messagesArray.push(message);
+  messagesArray.forEach((msg) => {
+    const messageElement = `<div>${msg}</div> `;
+    chatContainer.insertAdjacentHTML("beforeend", messageElement);
+  });
+  conversation.value="" //serves for refresh and delete what you typed before in the input bar
+}
+
+console.log(conversation);
+
+// class Message {
+//   content: string;
+//   msgID: string;
+
+//   constructor(content: string, phone: number) {
+//     this.content = content;
+//     this.msgID = "id" + Math.random().toString(16).slice(2);
+//   }
+// }
+
+// class MessageList {
+//   messageList: Array<Message> = [];
+
+//   addMessage(message: Message) {
+//     this.messageList.push(message);
+// this.renderChat();
+// }
+
+// renderChat() {
+//   let html: string = "";
+
+//   this.messageList.forEach((message) => {
+//     html +=
+//   });
+//   containerChat.innerHTML = html;
+// }
+// }
+
+const messageList = new MessageList();
+
+// btnMessage.addEventListener("click", sendMessage);
+
+function sendConversation() {
+  const inputMessage = conversation.value;
+
+  const message = new Message(inputMessage, "1234");
+
+  messageList.addMessage(message);
+
+  conversation.value = "";
+}
+
+function redirect2() {
+  try {
+      window.location.href = 'index.html'
+      if (!window.location.href) throw new Error('The page where you want to redirect it doesn´t exist!');
+  } catch (error) {
+      console.error(error)
+  }
+}
