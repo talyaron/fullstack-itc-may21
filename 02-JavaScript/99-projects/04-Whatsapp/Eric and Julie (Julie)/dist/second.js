c7b2ed93cef543eadfbd81576a3f7730c5b0746f
Julie Chajes <78927901+Juliechajes@users.noreply.github.com>
2021-07-06 14:55:49 +0300

    Update

--- 02-JavaScript/99-projects/04-Whatsapp/Eric and Julie (Julie)/dist/second.js
+++ 02-JavaScript/99-projects/04-Whatsapp/Eric and Julie (Julie)/dist/second.js
@@ -1,12 +1,27 @@
 var savedContacts = JSON.parse(localStorage.getItem("contactsData"));
-console.log(savedContacts);
+// So saved contacts is the list of contacts form the file
+// console.log(savedContacts);
 var conversation = (document.querySelector(".envelope__typing--input-entry"));
-console.log(window.location.href);
+// console.log(window.location.href);
+// This shows us the url, to check whether the id is showing
 var currentUrl = window.location.href;
+// You name the current url
 var idIndex = currentUrl.indexOf("id=");
+// give me the index of "id=" it's sliced off the "id="
+// You're getting the parameter that sent in the query
 var id = currentUrl.slice(idIndex + 3);
+// console.log(id);
 // Understand this bit
 var currentUser = savedContacts.find(function (contact) { return contact.contactId === id; });
+// console.log(currentUser);
+// current user is find the relevant user from among the list of contacts
+// The contact whose contact id matches the one we just took from the Query
+// When you console log current user it shows just one person.
+// conact here is a name that I am giving it.
+// Example: "For each element in the array" do this (callback function)
+// Here, the arrow function in one line, calls, and returns (currentUser?).
+// console.log(currentUser.contactName);
+document.querySelector("#userName").innerHTML = currentUser.contactName;
 var chatContainer = document.querySelector(".envelope__chat");
 var send = document.querySelector("#send");
 send.addEventListener("click", sendMessage);
@@ -21,30 +36,7 @@ function sendMessage() {
     conversation.value = ""; //serves for refresh and delete what you typed before in the input bar
 }
 console.log(conversation);
-// class Message {
-//   content: string;
-//   msgID: string;
-//   constructor(content: string, phone: number) {
-//     this.content = content;
-//     this.msgID = "id" + Math.random().toString(16).slice(2);
-//   }
-// }
-// class MessageList {
-//   messageList: Array<Message> = [];
-//   addMessage(message: Message) {
-//     this.messageList.push(message);
-// this.renderChat();
-// }
-// renderChat() {
-//   let html: string = "";
-//   this.messageList.forEach((message) => {
-//     html +=
-//   });
-//   containerChat.innerHTML = html;
-// }
-// }
 var messageList = new MessageList();
-// btnMessage.addEventListener("click", sendMessage);
 function sendConversation() {
     var inputMessage = conversation.value;
     var message = new Message(inputMessage, "1234");
@@ -53,11 +45,12 @@ function sendConversation() {
 }
 function redirect2() {
     try {
-        window.location.href = 'index.html';
+        window.location.href = "index.html";
         if (!window.location.href)
-            throw new Error('The page where you want to redirect it doesn´t exist!');
+            throw new Error("The page where you want to redirect it doesn´t exist!");
     }
     catch (error) {
         console.error(error);
     }
 }
+// This is a JS redirect, you could have done it with a link.
--- 02-JavaScript/99-projects/04-Whatsapp/Eric and Julie (Julie)/second.ts
+++ 02-JavaScript/99-projects/04-Whatsapp/Eric and Julie (Julie)/second.ts
@@ -1,17 +1,33 @@
 const savedContacts = JSON.parse(localStorage.getItem("contactsData"));
-console.log(savedContacts);
+// So saved contacts is the list of contacts form the file
+// console.log(savedContacts);
 const conversation = <HTMLInputElement>(
   document.querySelector(".envelope__typing--input-entry")
 );
 
-console.log(window.location.href);
+// console.log(window.location.href);
+// This shows us the url, to check whether the id is showing
 const currentUrl = window.location.href;
+// You name the current url
 const idIndex = currentUrl.indexOf("id=");
-const id = currentUrl.slice(idIndex + 3);
+// give me the index of "id=" it's sliced off the "id="
+// You're getting the parameter that sent in the query
 
+const id = currentUrl.slice(idIndex + 3);
+// console.log(id);
 // Understand this bit
 
 const currentUser = savedContacts.find((contact) => contact.contactId === id);
+// console.log(currentUser);
+// current user is find the relevant user from among the list of contacts
+// The contact whose contact id matches the one we just took from the Query
+// When you console log current user it shows just one person.
+// conact here is a name that I am giving it.
+// Example: "For each element in the array" do this (callback function)
+// Here, the arrow function in one line, calls, and returns (currentUser?).
+
+// console.log(currentUser.contactName);
+document.querySelector("#userName").innerHTML = currentUser.contactName;
 
 const chatContainer: any = document.querySelector(".envelope__chat");
 
@@ -27,42 +43,12 @@ function sendMessage() {
     const messageElement = `<div>${msg}</div> `;
     chatContainer.insertAdjacentHTML("beforeend", messageElement);
   });
-  conversation.value="" //serves for refresh and delete what you typed before in the input bar
+  conversation.value = ""; //serves for refresh and delete what you typed before in the input bar
 }
 
 console.log(conversation);
 
-// class Message {
-//   content: string;
-//   msgID: string;
-
-//   constructor(content: string, phone: number) {
-//     this.content = content;
-//     this.msgID = "id" + Math.random().toString(16).slice(2);
-//   }
-// }
-
-// class MessageList {
-//   messageList: Array<Message> = [];
-
-//   addMessage(message: Message) {
-//     this.messageList.push(message);
-// this.renderChat();
-// }
-
-// renderChat() {
-//   let html: string = "";
-
-//   this.messageList.forEach((message) => {
-//     html +=
-//   });
-//   containerChat.innerHTML = html;
-// }
-// }
-
 const messageList = new MessageList();
-
-// btnMessage.addEventListener("click", sendMessage);
 
 function sendConversation() {
   const inputMessage = conversation.value;
@@ -76,9 +62,12 @@ function sendConversation() {
 
 function redirect2() {
   try {
-      window.location.href = 'index.html'
-      if (!window.location.href) throw new Error('The page where you want to redirect it doesn´t exist!');
+    window.location.href = "index.html";
+    if (!window.location.href)
+      throw new Error("The page where you want to redirect it doesn´t exist!");
   } catch (error) {
-      console.error(error)
+    console.error(error);
   }
 }
+
+// This is a JS redirect, you could have done it with a link.
