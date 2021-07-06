5864df1b0e2b6967bdbe0bdb6f919d0ce895b081
Jonathan Eliel Nisenbaum <47113013+jnisen@users.noreply.github.com>
2021-07-06 12:08:55 +0300

    changes

--- 02-JavaScript/99-projects/04-Whatsapp/Jonathan & Yaniv/groups/dist/helpers.js
+++ 02-JavaScript/99-projects/04-Whatsapp/Jonathan & Yaniv/groups/dist/helpers.js
@@ -2,6 +2,7 @@ var pageTitle = document.querySelector('title');
 pageTitle.innerText = loggedInUser.userName + "'s chats";
 var profileImg = document.querySelector('.controls__item--profile_img');
 profileImg.setAttribute('src', loggedInUser.userImg);
+loggedInUser.renderChatsToChatsList();
 var addChatBtn = document.querySelector('.controls__item--plus');
 addChatBtn.addEventListener('click', function (ev) { return showNewChatMenu(ev); });
 var showNewChatMenu = function (ev) {
@@ -32,10 +33,10 @@ var directToChat = function (ev) {
     var contactToChatName = contactToChatNameContainer.innerText;
     var chatUsers = [loggedInUser.userPhone, contactToChat.id];
     var group = new Group(contactToChatPhone, contactToChatImg, contactToChatName, chatUsers);
-    loggedInUser.addGroupIfNew(group.groupId);
+    loggedInUser.addGroup(group);
     localStorage.setItem('currentUser', JSON.stringify(loggedInUser));
     allContacts[allContacts.findContactIndex(loggedInUser.userPhone)] = loggedInUser;
-    localStorage.setItem('contactList', JSON.stringify(loggedInUser));
+    localStorage.setItem('contactList', JSON.stringify(allContacts));
     localStorage.setItem('contactListUser', JSON.stringify(allContacts));
     localStorage.setItem('contactId', JSON.stringify(contactToChat.id));
     window.location.href = "../chat/chat.html?" + loggedInUser.userPhone + "&" + contactToChat.id;
@@ -53,42 +54,32 @@ var hideNewGroupMenu = function (ev) {
     var newGroupMenu = document.querySelector('.new_group');
     newGroupMenu.style.display = 'none';
 };
-var newGroupSubmit = document.querySelector('.options__item--submit');
+var newGroupSubmit = document.querySelector('#new_group_submit');
 newGroupSubmit.addEventListener('submit', function (ev) { return createNewGroup(ev); });
 var createNewGroup = function (ev) {
     try {
         ev.preventDefault();
-        console.log(ev.target.elements);
-        var groupId = "group" + Math.random().toString(16).slice(2);
-        var groupImg = ev.target.elements.groupImg.value; // ??
+        var groupId = null;
+        var imgLabel = document.querySelector('#add_photo');
+        var groupImg = imgLabel.getAttribute('alt');
         var groupName = ev.target.elements.groupName.value;
-        // const groupUsers: Array<string> = ev.target.elements.??.value; // how to fetch only checked checkboxes?
-        // const group: Group = new Group(groupId, groupImg, groupName, groupUsers);
-        // loggedInUser.addGroupIfNew(group.groupId);
-        // localStorage.setItem('currentUser',JSON.stringify(loggedInUser));
-        // allContacts[allContacts.findContactIndex(loggedInUser.userPhone)] = loggedInUser;
-        // localStorage.setItem('contactList',JSON.stringify(loggedInUser));
+        var contactsCheckboxes = ev.target.querySelectorAll('.checkbox');
+        var groupUsers_1 = [];
+        contactsCheckboxes.forEach(function (contact) {
+            if (contact.checked)
+                groupUsers_1.push(contact.value);
+        });
+        var group = new Group(groupId, groupImg, groupName, groupUsers_1);
+        loggedInUser.addGroup(group);
+        localStorage.setItem('currentUser', JSON.stringify(loggedInUser));
+        allContacts[allContacts.findContactIndex(loggedInUser.userPhone)] = loggedInUser;
+        localStorage.setItem('contactList', JSON.stringify(allContacts));
+        hideNewGroupMenu(ev);
+        hideNewChatMenu(ev);
         ev.target.reset();
     }
     catch (er) {
         console.error(er);
-    }
-};
-var readURL = function (input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            var label = document.querySelector('#add_photo');
-            label.setAttribute('alt', "" + e.target.result);
-            label.style.backgroundImage = "url(\"" + e.target.result + "\")";
-            label.style.backgroundSize = '100% 100%';
-            label.innerText = '';
-            label.style.padding = '0';
-            label.style.height = '200px';
-            label.style.width = '200px';
-            return e.target.result;
-        };
-        reader.readAsDataURL(input.files[0]);
     }
 };
 var logOutBtn = document.querySelector('.controls__item--ellipsis');
