08e6de1dc376fc328fd95806a5822e5563f5aacd
Golan Blumenkrantz <83172219+Golan281@users.noreply.github.com>
2021-07-07 16:13:31 +0300

    v8

--- 02-JavaScript/99-projects/04-Whatsapp/Tomas-Golan/ts/chat.ts
+++ 02-JavaScript/99-projects/04-Whatsapp/Tomas-Golan/ts/chat.ts
@@ -2,16 +2,9 @@
 //method to add individual msg
 class Mensaje {
     text: string;
-<<<<<<< Updated upstream
     textId: string = "id" + Math.random().toString(16).slice(2);
     constructor(text: string) {
         this.text = text;
-=======
-    id: string;
-    constructor(text: string, textId: string) {
-        this.text = text;
-        this.id = Math.random().toString(16).slice(2);;
->>>>>>> Stashed changes
     }
 }
 //class to provide an array of all texts to local storage
@@ -32,9 +25,8 @@ function handleSubmit(ev) {
     const text = ev.target.elements.text.value;
     const msg: Mensaje = new Mensaje(text);
     allOfMsgs.addNewMsg(msg);
+    window.location.reload();
     
-    console.log(text, 'should catch text');
-    console.log(msg, 'should display msg');
     
     
 }
@@ -42,11 +34,54 @@ function handleSubmit(ev) {
 function renderOnDOM() {
     const data :HTMLElement = document.querySelector(".data");
     allOfMsgs.msgs.forEach(msg => {
-        data.insertAdjacentHTML('beforeend',`<div class="allmsgs">${msg.text}</div>`);
-        // data.innerHTML += `<div class="allmsgs">${msg.text}</div>`;
+        data.insertAdjacentHTML('beforeend',`<div class="allmsgs chat_wrapper--user1"><p>${msg.text}</p></div>`);
+        
     });
+
+
     
 }
 
+// function showPersonaOnDOM() {
+//     const allPpl = JSON.parse(localStorage.getItem('contactos'));
+//     // const lastPersonaIndex = allPpl.length - 1;
+//     const lastPersona = allPpl[personClicked];
+//     const data = document.querySelector(".data");
+//     data.insertAdjacentHTML('beforeend',`<div class="allmsgs chat_wrapper--user1"><p>${msg.text}</p></div>`);
+// }
 
 renderOnDOM();
+// showPersonaOnDOM();
+
+function idContactForChat(id){
+    let getContactChat = JSON.parse(localStorage.getItem("contactos"));
+    const idContact = getContactChat.find((contact) => contact.id === id);
+    getContactSelected.push(idContact);
+
+    renderContactInChat(getContactSelected);
+    console.log(idContact);
+    console.log(idContact.name);
+}
+
+function renderContactInChat(element) {
+    const containerChat: HTMLElement = document.querySelector(".header_chat");
+    // containerChat.appendChild(div)
+    let htmlChat= "";
+ 
+        htmlChat += `
+        <img class="header_img_profile"
+        src="${element.profileImg}"
+        alt="">
+      <h1>${element.name}</h1>
+      <div class="header_icon">
+        <a class="btn_icons" href=""><i
+            class="fa fa-search fa fa-2x search_icon"></i></a>
+        <a class="btn_icons" href=""><i class="fas fa-ellipsis-v fa-2x"></i></a>
+      </div>`
+
+      containerChat.innerHTML = htmlChat;
+
+}
+
+
+    
