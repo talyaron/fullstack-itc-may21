a4c00919ac0e81e2795f77b0bd881b163c7482bc
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-07-14 21:11:48 +0300

    .

--- 02-JavaScript/20-clousers/Yaniv/dist/index.js
+++ 02-JavaScript/20-clousers/Yaniv/dist/index.js
@@ -1,29 +1,33 @@
-// 1) create a clouser function.
+// 1) create a closure function.
 // the function will get the name of the resident
 // welcome('Efraim')
 // -> Hello Efraim, You are resident number 1
-// the function will return "hello resdient <Name>, you are resident number X", and it will count the number of residents
-// 2) create a function closer the hols in its clouser, all the name of the residents.
-// if the input arguement is 'l' retuen an array with all the residents
-// welocome('l')...
+// the function will return "Hello <Name>, you are resident number X", and it will count the number of residents
+// 2) create a function that will hold all the name of the residents in its closure.
+// if the input arguement is 'l' return an array with all the residents
+// welcome('l')...
 // 3) create it for bulding A, and for bulding B;
-function welcome(building) {
+function welcome() {
     var residents = [];
     function personalWelcome(resident) {
+        if (resident === 'l')
+            return residents;
         residents.push(resident);
         return "Welcome " + resident + ", you are resident number " + residents.length;
     }
-    if ()
-        return personalWelcome;
+    return personalWelcome;
 }
-var addZ = add(10);
-var addX = add(5);
-console.log(addZ(2));
-console.log(addZ(20));
-console.log(addZ(2));
-console.log(addZ(20));
-console.log(addX(1));
-console.log(addX(1));
-console.log(addX(1));
-console.log(addX(1));
-console.log(addX(1));
+var buildingA = welcome();
+var buildingB = welcome();
+console.log(buildingA('Eric'));
+console.log(buildingA('Jonathan'));
+console.log(buildingA('Leonardo'));
+console.log(buildingA('Tomas'));
+console.log(buildingA('l'));
+console.log(buildingB('Raziel'));
+console.log(buildingB('Yarden'));
+console.log(buildingB('Avromi'));
+console.log(buildingB('Julie'));
+console.log(buildingB('Efraim'));
+console.log(buildingB('Yaniv'));
+console.log(buildingB('l'));
--- 02-JavaScript/20-clousers/Yaniv/index.ts
+++ 02-JavaScript/20-clousers/Yaniv/index.ts
@@ -1,38 +1,40 @@
-
-// 1) create a clouser function.
+// 1) create a closure function.
 // the function will get the name of the resident
 
 // welcome('Efraim')
 // -> Hello Efraim, You are resident number 1
 
-// the function will return "hello resdient <Name>, you are resident number X", and it will count the number of residents
+// the function will return "Hello <Name>, you are resident number X", and it will count the number of residents
 
-// 2) create a function closer the hols in its clouser, all the name of the residents.
-// if the input arguement is 'l' retuen an array with all the residents
-// welocome('l')...
+// 2) create a function that will hold all the name of the residents in its closure.
+// if the input arguement is 'l' return an array with all the residents
+// welcome('l')...
 
 // 3) create it for bulding A, and for bulding B;
 
-function welcome(building:string) {
+function welcome() {
     let residents: Array<string> = [];
-    function personalWelcome(resident: string) {       
+    function personalWelcome(resident: string) {
+        if (resident === 'l') return residents;
         residents.push(resident);
         return `Welcome ${resident}, you are resident number ${residents.length}`;
     }
-    if 
     return personalWelcome
 }
 
 
-const addZ = add(10);
-const addX = add(5);
-console.log(addZ(2));
-console.log(addZ(20));
-console.log(addZ(2));
-console.log(addZ(20));
+const buildingA = welcome();
+const buildingB = welcome();
+console.log(buildingA('Eric'));
+console.log(buildingA('Jonathan'));
+console.log(buildingA('Leonardo'));
+console.log(buildingA('Tomas'));
+console.log(buildingA('l'));
 
-console.log(addX(1));
-console.log(addX(1));
-console.log(addX(1));
-console.log(addX(1));
-console.log(addX(1));
+console.log(buildingB('Raziel'));
+console.log(buildingB('Yarden'));
+console.log(buildingB('Avromi'));
+console.log(buildingB('Julie'));
+console.log(buildingB('Efraim'));
+console.log(buildingB('Yaniv'));
+console.log(buildingB('l'));
