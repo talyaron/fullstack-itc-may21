5e16852a4cd8727878467bd15347e70bdc4b4b05
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-07-14 21:05:26 +0300

    .

--- 02-JavaScript/20-clousers/Yaniv/dist/index.js
+++ 02-JavaScript/20-clousers/Yaniv/dist/index.js
@@ -1,10 +1,20 @@
-function welcome() {
+// 1) create a clouser function.
+// the function will get the name of the resident
+// welcome('Efraim')
+// -> Hello Efraim, You are resident number 1
+// the function will return "hello resdient <Name>, you are resident number X", and it will count the number of residents
+// 2) create a function closer the hols in its clouser, all the name of the residents.
+// if the input arguement is 'l' retuen an array with all the residents
+// welocome('l')...
+// 3) create it for bulding A, and for bulding B;
+function welcome(building) {
     var residents = [];
     function personalWelcome(resident) {
         residents.push(resident);
         return "Welcome " + resident + ", you are resident number " + residents.length;
     }
-    return personalWelcome;
+    if ()
+        return personalWelcome;
 }
 var addZ = add(10);
 var addX = add(5);
