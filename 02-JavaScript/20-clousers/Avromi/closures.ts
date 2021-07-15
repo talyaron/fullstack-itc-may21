6dc66db0479f4edf89aaf7733b268198e1ed3164
Avromi Russell <83745803+avromiRussell@users.noreply.github.com>
2021-07-15 20:18:16 +0300

    push

--- 02-JavaScript/20-clousers/Avromi/closures.ts
+++ 02-JavaScript/20-clousers/Avromi/closures.ts
@@ -1,3 +1,4 @@
+
 function add(resident) {
     let residentNumber = 0;
     function _add(name: string) {
@@ -11,12 +12,12 @@ function add(resident) {
 
 const addResBldg1 = add("Building1");
 const addResBldg2 = add("building2");
-console.log(addResBldg1("sam"));
-console.log(addResBldg1("mitch"));
-console.log(addResBldg1("abe"));
-console.log(addResBldg2("mike"));
-console.log(addResBldg2("jim"));
-console.log(addResBldg2("steve"));
+// console.log(addResBldg1("sam"));
+// console.log(addResBldg1("mitch"));
+// console.log(addResBldg1("abe"));
+// console.log(addResBldg2("mike"));
+// console.log(addResBldg2("jim"));
+// console.log(addResBldg2("steve"));
 
 // const addX = add(5);
 
@@ -31,6 +32,42 @@ function someFnc(a) {
 
 const someClosure = someFnc("hey")
 const someClosure2 = someFnc("hey")
-console.log(someClosure("somethingElse"));
-console.log(someClosure("somethingElseHere"));
-console.log(someClosure2("somethin"));
+// console.log(someClosure("somethingElse"));
+// console.log(someClosure("somethingElseHere"));
+// console.log(someClosure2("somethin"));
+
+
+function addArrayOfResidemt() {
+
+    let residentNumber = 0;
+    let residentsArray = [];
+    console.log(residentsArray);
+    function _add(name: string) {
+
+        if (name === "l") {
+            residentsArray.map(res => { console.log(`hello resdient ${res.name}, you are resident number ${res.residentNumber}`); })
+
+        }
+        if (name !== "l") {
+            residentNumber++;
+            residentsArray.push({ name, residentNumber })
+            return `hello resdient ${name}, you are resident number ${residentNumber}`
+        }
+    }
+    return _add
+}
+
+
+const addResToArray1 = addArrayOfResidemt()
+const addResToArray2 = addArrayOfResidemt()
+console.log(addResToArray1("AVROMI"));
+console.log(addResToArray1("SARAH"));
+console.log(addResToArray1("AMICHAI"));
+addResToArray1("l");
+
+console.log(addResToArray2("Mike"));
+console.log(addResToArray2("David"));
+console.log(addResToArray2("Jim"));
+addResToArray2("l");
+
+
--- 02-JavaScript/20-clousers/Avromi/dist/closures.js
+++ 02-JavaScript/20-clousers/Avromi/dist/closures.js
@@ -8,12 +8,12 @@ function add(resident) {
 }
 var addResBldg1 = add("Building1");
 var addResBldg2 = add("building2");
-console.log(addResBldg1("sam"));
-console.log(addResBldg1("mitch"));
-console.log(addResBldg1("abe"));
-console.log(addResBldg2("mike"));
-console.log(addResBldg2("jim"));
-console.log(addResBldg2("steve"));
+// console.log(addResBldg1("sam"));
+// console.log(addResBldg1("mitch"));
+// console.log(addResBldg1("abe"));
+// console.log(addResBldg2("mike"));
+// console.log(addResBldg2("jim"));
+// console.log(addResBldg2("steve"));
 // const addX = add(5);
 function someFnc(a) {
     var something = "something";
@@ -24,6 +24,32 @@ function someFnc(a) {
 }
 var someClosure = someFnc("hey");
 var someClosure2 = someFnc("hey");
-console.log(someClosure("somethingElse"));
-console.log(someClosure("somethingElseHere"));
-console.log(someClosure2("somethin"));
+// console.log(someClosure("somethingElse"));
+// console.log(someClosure("somethingElseHere"));
+// console.log(someClosure2("somethin"));
+function addArrayOfResidemt() {
+    var residentNumber = 0;
+    var residentsArray = [];
+    console.log(residentsArray);
+    function _add(name) {
+        if (name === "l") {
+            residentsArray.map(function (res) { console.log("hello resdient " + res.name + ", you are resident number " + res.residentNumber); });
+        }
+        if (name !== "l") {
+            residentNumber++;
+            residentsArray.push({ name: name, residentNumber: residentNumber });
+            return "hello resdient " + name + ", you are resident number " + residentNumber;
+        }
+    }
+    return _add;
+}
+var addResToArray1 = addArrayOfResidemt();
+var addResToArray2 = addArrayOfResidemt();
+console.log(addResToArray1("AVROMI"));
+console.log(addResToArray1("SARAH"));
+console.log(addResToArray1("AMICHAI"));
+addResToArray1("l");
+console.log(addResToArray2("Mike"));
+console.log(addResToArray2("David"));
+console.log(addResToArray2("Jim"));
+addResToArray2("l");
