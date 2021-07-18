2e0175e0e8c4a17b8a52b7104e8ec6d430ca52bf
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2021-07-18 16:44:42 +0300

    up

---
+++ 02-JavaScript/20-clousers/TomasLeonPatlayan/index.ts
@@ -0,0 +1,34 @@
+const getResidents = (): any => {
+  let resident: Array<string> = [];
+
+  const showResident = (residents: string): Array<string> | string => {
+    if(residents === 'l') return resident
+    resident.push(residents);
+    return `Welcome ${residents}, now you are the number ${resident.length}`;
+  };
+
+  return showResident;
+};
+
+const buildingA = getResidents();
+
+
+const buildingB = getResidents();
+
+
+const handleSubmitA = (event) => {
+  event.preventDefault();
+  const name = event.target.elements.name.value;
+  console.log(buildingA(name));
+};
+const handleSubmitB = (event) => {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    console.log(buildingB(name));
+  };
+  
+// const handleSubmitB = (event) => {
+//     event.preventDefault();
+//     const name = event.target.elements.name.value;
+//     console.log(buildingA(name));
+//   };
