dee790d767b68b80dbe451882cd5cc11efa0e966
Golan Blumenkrantz <83172219+Golan281@users.noreply.github.com>
2021-07-20 00:01:04 +0300

    Add files via upload

---
+++ 02-JavaScript/20-clousers/Golan/script.ts
@@ -0,0 +1,25 @@
+//1) create a closure function.
+// the function will get the name of the resident
+
+// welcome('Efraim')
+// -> Hello Efraim, You are resident number 1
+
+// the function will return "hello resident <Name>, you are resident number X", and it will count the number of residents
+
+// 2) create a  closure function to the holds in its closure, all the names of the residents.
+// if the input argument is 'l' return an array with all the residents
+// welcome('l')...
+
+// 3) create it for building A, and for building B;
+
+
+function hello(): any {
+    try {
+        function helloWelcome() {
+
+        }
+        return helloWelcome;
+    } catch (error) {
+        console.error(error);
+    }
+}
