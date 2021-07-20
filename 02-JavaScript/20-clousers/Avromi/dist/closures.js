d289cecfc9565c1515313dc124ac3465ebfab419
Avromi Russell <83745803+avromiRussell@users.noreply.github.com>
2021-07-20 11:23:16 +0300

    working ?

--- 02-JavaScript/20-clousers/Avromi/dist/closures.js
+++ 02-JavaScript/20-clousers/Avromi/dist/closures.js
@@ -1,29 +1,51 @@
-function add(resident) {
+var BuildingRoot = document.querySelector("#builidingRoot");
+var listOfResidents = document.querySelector("#listOfResidents");
+var closeButton = document.querySelector("#close").addEventListener('click', function () {
+    listOfResidents.style.display = 'none';
+    BuildingRoot.style.display = 'flex';
+});
+function addArrayOfResidemt(building) {
     var residentNumber = 0;
+    var residentsArray = [];
     function _add(name) {
-        residentNumber++;
-        return "hello resdient " + name + ", you are resident number " + residentNumber;
+        if (name === "l") {
+            var listRes = document.querySelector("#listRes");
+            listOfResidents.style.display = 'block';
+            BuildingRoot.style.display = 'none';
+            var resHtml_1 = "";
+            residentsArray.forEach(function (res) {
+                resHtml_1 += "<div>hello resdient " + res.name + ", you are \n                resident number " + res.residentNumber + " of " + res.building + "</div>";
+            });
+            listRes.innerHTML = resHtml_1;
+        }
+        if (name !== "l") {
+            var bldgOne = document.querySelector("#buildingOne");
+            var bldgTwo = document.querySelector("#buildingTwo");
+            residentNumber++;
+            residentsArray.push({ name: name, residentNumber: residentNumber, building: building });
+            var residnetHtml = "<div>hello resdient " + name + ", you are resident number " + residentNumber + " of " + building + "</div>";
+            if (building === "building one") {
+                bldgOne.insertAdjacentHTML("afterbegin", residnetHtml);
+            }
+            if (building === "building two") {
+                bldgTwo.insertAdjacentHTML("afterbegin", residnetHtml);
+            }
+        }
     }
     return _add;
 }
-var addResBldg1 = add("Building1");
-var addResBldg2 = add("building2");
-console.log(addResBldg1("sam"));
-console.log(addResBldg1("mitch"));
-console.log(addResBldg1("abe"));
-console.log(addResBldg2("mike"));
-console.log(addResBldg2("jim"));
-console.log(addResBldg2("steve"));
-// const addX = add(5);
-function someFnc(a) {
-    var something = "something";
-    function _insideSomeFnc(b) {
-        return (something = b);
+var form = document.querySelector("#form").addEventListener('submit', handleSubmit);
+function handleSubmit(ev) {
+    ev.preventDefault();
+    var name = ev.target.elements.name.value;
+    var building = ev.target.elements.building.value;
+    if (building === "building one") {
+        addResToArray1(name);
     }
-    return _insideSomeFnc;
+    if (building === "building two") {
+        addResToArray2(name);
+    }
+    ev.target.reset();
 }
-var someClosure = someFnc("hey");
-var someClosure2 = someFnc("hey");
-console.log(someClosure("somethingElse"));
-console.log(someClosure("somethingElseHere"));
-console.log(someClosure2("somethin"));
+var addResToArray1 = addArrayOfResidemt("building one");
+var addResToArray2 = addArrayOfResidemt("building two");
