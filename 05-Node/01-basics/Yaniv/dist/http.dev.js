9f590e11791d2e006440e97f77ce70151531f39b
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-07-21 20:19:10 +0300

    .

--- 05-Node/01-basics/Yaniv/dist/http.dev.js
+++ 05-Node/01-basics/Yaniv/dist/http.dev.js
@@ -4,7 +4,7 @@ var http = require('http');
 
 var requestListener = function requestListener(req, res) {
   res.writeHead(200);
-  res.end("\n  <h1>Hello, I'm an h1!</h1>\n  <h2>Hello, I'm an h2!</h2>\n  <h3>Hello, I'm an h3!</h3>\n  <p>Hello, I'm a p!</p>\n  <p style=\"font-color:pink\">Hello, I'm a pink p!</p>\n  ");
+  res.end("\n  <h1>Hello, I'm an h1!</h1>\n  <h2>Hello, I'm an h2!</h2>\n  <h3>Hello, I'm an h3!</h3>\n  <p>Hello, I'm a p!</p>\n  <p style=\"color:pink\">Hello, I'm a pink p!</p>\n  ");
 };
 
 var server = http.createServer(requestListener);
