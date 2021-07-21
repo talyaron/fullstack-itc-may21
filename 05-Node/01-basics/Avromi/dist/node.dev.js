daa1c742eed5389931957f2cf691fabe421b0c6d
Avromi Russell <83745803+avromiRussell@users.noreply.github.com>
2021-07-21 20:41:11 +0300

    server running

--- 05-Node/01-basics/Avromi/dist/node.dev.js
+++ 05-Node/01-basics/Avromi/dist/node.dev.js
@@ -4,14 +4,13 @@ console.log("hi you foo");
 
 var http = require('http');
 
-var html = "<body>\n<h1>Welcome</h1>\n\n<p>here I can write some more HTML looking code and use it as a variable!!! YAY</p>\n\n</body>";
-
 var requestListener = function requestListener(req, res) {
   res.writeHead(200);
 
   var fs = require('fs');
 
-  var file = fs.res.end(html);
+  var file = fs.readFileSync('./index.html');
+  res.end(file);
 };
 
 var server = http.createServer(requestListener);
