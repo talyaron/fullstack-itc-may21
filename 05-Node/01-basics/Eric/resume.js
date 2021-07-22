4533728e81cab58b48658bbd9cb748b17a52a7a9
Eric Kobrinsky <78773449+erickobrinsky@users.noreply.github.com>
2021-07-22 15:59:37 +0300

    node

---
+++ 05-Node/01-basics/Eric/resume.js
@@ -0,0 +1,24 @@
+/*
+create a server
+create an HTML file with your CV (resume).
+Make it stylish and beautiful, as you deserve!
+serve the file using the server.
+
+80 points
+
+//top-notch:
+add your image to the index.html
+
++20 points
+*/ 
+
+const http = require('http');
+const fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('resume.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
+
