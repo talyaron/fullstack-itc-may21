9b427f55176ec2da5f27543f36b5c8d4de1a2b6b
Eric Kobrinsky <78773449+erickobrinsky@users.noreply.github.com>
2021-07-22 21:12:55 +0300

    nodeResume

--- 05-Node/01-basics/Eric/resume.js
+++ 05-Node/01-basics/Eric/resume.js
@@ -12,13 +12,25 @@ add your image to the index.html
 +20 points
 */ 
 
-const http = require('http');
-const fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('resume.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+  
+const express = require('express')
+
+// const http = require('http');
+// const fs = require('fs');
+// http.createServer(function (req, res) {
+//   fs.readFile('resume.html', function(err, data) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write(data);
+//     return res.end();
+//   });
+// }).listen(8080);
+
+const app=express()
+
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', function(req,res){
+  res.sendFile(__dirname + "/resume.html")
+})
+app.listen(8080)
 
