8d13e90c1ba62b32ce564e3e5a308ad8f836e129
Avromi Russell <83745803+avromiRussell@users.noreply.github.com>
2021-07-23 10:05:17 +0300

    not workin:(

---
+++ 05-Node/02-httpserver/Avromi/dist/index.dev.js
@@ -0,0 +1,47 @@
+"use strict";
+
+var http = require('http');
+
+var port = process.env.PORT || 3001;
+
+var fs = require('fs');
+
+var server = http.createServer();
+server.on('request', function (req, res) {
+  try {
+    var method = req.method,
+        url = req.url,
+        headers = req.headers;
+
+    switch (url) {
+      case '/':
+        res.writeHead(200, {
+          'Content-Type': 'text/plain'
+        });
+        res.end('ello');
+
+      case '/about':
+        res.writeHead(200, {
+          'Content-Type': 'text/html'
+        });
+        var file = fs.readFileSync('about.html');
+        res.end(file);
+
+      default:
+        throw new Error("boooooooooooom");
+        res.writeHead(200, {
+          'Content-Type': 'text/plain'
+        });
+        res.end('OK 3');
+    }
+  } catch (e) {
+    console.log(e);
+    res.writeHead(500, {
+      'Content-Type': 'text/plain'
+    });
+    res.end("There is a sever error: ".concat(e.message));
+  }
+});
+server.listen(port, function () {
+  console.log('Server listen on port', port);
+});
---
+++ 05-Node/02-httpserver/Avromi/index.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const port = process.env.PORT || 3001;
+const fs = require('fs');
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    try {
+
+        const {
+            method,
+            url,
+            headers
+        } = req;
+
+        switch (url) {
+            case '/':
+                res.writeHead(200, {
+                    'Content-Type': 'text/plain'
+                });
+                res.end('ello')
+            case '/about':
+                res.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                const file = fs.readFileSync('about.html')
+                res.end(file)
+            case '/contact':
+                res.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                const file = fs.readFileSync('contact.html')
+                res.end(file)
+
+
+            default:
+                throw new Error("boooooooooooom");
+                res.writeHead(200, {
+                    'Content-Type': 'text/plain'
+                });
+
+                res.end('OK 3');
+        }
+    } catch (e) {
+        console.log(e);
+
+        res.writeHead(500, {
+            'Content-Type': 'text/plain'
+        });
+
+        res.end(`There is a sever error: ${e.message}`);
+    }
+
+})
+
+server.listen(port, () => {
+    console.log('Server listen on port', port)
+})
