78122b2a6fd914b802bce8d609e766510f453403
RazielAF <82554306+RazielAF@users.noreply.github.com>
2021-07-24 17:54:43 +0300

    L

--- 05-Node/01-basics/Raziel-CV-Server/cv.js
+++ 05-Node/01-basics/Raziel-CV-Server/cv.js
@@ -1,12 +1,2 @@
-const http = require('http');
-const fs = require('fs');
-
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    //read the file from the computer, and send it to the client
-    const file = fs.readFileSync("./cv.html");
-    res.end(file);
-}
-
-const server = http.createServer(requestListener);
-server.listen(7800, () => { console.log('Listen on port 7800') });
+const http=require('http');
+const path=require('path');
