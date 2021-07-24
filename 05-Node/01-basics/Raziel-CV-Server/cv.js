440d85cdcba3b926bd736457813ed587d5488ccd
RazielAF <82554306+RazielAF@users.noreply.github.com>
2021-07-24 20:06:11 +0300

    update

--- 05-Node/01-basics/Raziel-CV-Server/cv.js
+++ 05-Node/01-basics/Raziel-CV-Server/cv.js
@@ -1,7 +1,43 @@
-const http=require('http');
-const path=require('path');
-const fs=require('fs');
+const http = require('http');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
 
-const server=http.createServer((req,res)=>{
-    if()
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    try {
+
+        const {url} = req;
+
+        switch (url) {
+
+            case '/':
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                const main = fs.readFileSync('./cv.html')
+                res.end(main)
+                break;
+
+            case '/nodejs':
+                res.writeHead(200, { 'Content-Type': 'image/png' });
+                const nodejs = fs.readFileSync('./profile.png')
+                res.end(nodejs)
+                break
+
+            
+
+            default:
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('The requested resource was not found');
+        }
+    } catch (e) {
+        console.log(e);
+
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(`There is a sever error: ${e.message}`);
+    }
+
+})
+
+server.listen(port, () => {
+    console.log('Server listen on port', port)
 })
