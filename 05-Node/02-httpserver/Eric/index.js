1839ba2b2ca0162ba0530659a79274c2bdf4ae27
Eric Kobrinsky <78773449+erickobrinsky@users.noreply.github.com>
2021-07-25 17:39:39 +0300

    node

--- 05-Node/02-httpserver/Eric/index.js
+++ 05-Node/02-httpserver/Eric/index.js
@@ -1,47 +1,42 @@
 const http = require('http');
-const port = process.env.PORT || 3001;
+const fs = require('fs')
+const port = process.env.PORT || 3008;
 
 
-const server = http.createServer((req,res)=>{
-    switch(req.url){
-        case '/':
-//add read file
-    
-    res.end(`
-        <html>
-            <head>
-                <title>first node app</title>
-            </head>
-                <body>
-                <h1>Welcometo my node app</h1>
-                <a href="/admin">Admin</a>
-                <a href="">Contact</a>
-                <a href="">About</a>
-                </body>
-        </html>
-        `);
+const server = http.createServer();
+
+
+server.on('request', (req, res)=>{
+
+    const {method, url, headers, body} = req;
+
+    switch(url){
+        case '/main':
+
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        const main = fs.readFileSync('./index.html');
+        res.end(main)
         break;
-        case '/admin':
-            res.end('welcome to the admin page');
-        break;
+
+        case '/about':
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            const about = fs.readFileSync('./about.html')
+            res.end(about)
+            break;
+
         case '/contact':
-            res.end('welcome to contact page');
-        break;
-        default:
-            res.end(`
-        <html>
-            <head>
-                <title>first node app</title>
-            </head>
-                <body>
-                <h1>Welcometo my node app</h1>
-                </body>
-        </html>
-        `);    
+            res.readFileSync(200,{'Content-Type' : 'text/html'})
+            const contact  = fs.readFileSync('./contact.html')
+            res.end(contact)
+            break;
+        
+            default:
+                res.writeHead(404, {'Content-Type': 'text/plain'})
+                res.end('There is a sever error')
+
     }
-})
+})     
 
-
-server.listen(port, () => {
-    console.log(`server now is on port ${port}`)
+server.listen(port,()=> {
+    console.log('server listen on port', port)
 })
