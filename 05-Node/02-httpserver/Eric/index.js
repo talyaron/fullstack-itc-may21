6e1319fb2738bc84a78ccba27c554b549693476e
Eric Kobrinsky <78773449+erickobrinsky@users.noreply.github.com>
2021-07-25 17:50:33 +0300

    dsad

--- 05-Node/02-httpserver/Eric/index.js
+++ 05-Node/02-httpserver/Eric/index.js
@@ -8,31 +8,31 @@ const server = http.createServer();
 
 server.on('request', (req, res)=>{
 
-    const {method, url, headers, body} = req;
+    const {method, url, headers} = req;
 
     switch(url){
-        case '/main':
+        case "/":
 
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        const main = fs.readFileSync('./index.html');
-        res.end(main)
+        res.writeHead(200, {'Content-Type': "text/html"})
+        const mainN = fs.readFileSync('./index.html');
+        res.end(mainN)
         break;
 
         case '/about':
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            const about = fs.readFileSync('./about.html')
-            res.end(about)
+            res.writeHead(200, {'Content-Type': "text/html"});
+            const aboutT = fs.readFileSync('./about.html')
+            res.end(aboutT)
             break;
 
         case '/contact':
-            res.readFileSync(200,{'Content-Type' : 'text/html'})
-            const contact  = fs.readFileSync('./contact.html')
-            res.end(contact)
+            res.readFileSync(200,{'Content-Type' : "text/html"})
+            const contactT  = fs.readFileSync('./contact.html')
+            res.end(contactT)
             break;
         
             default:
-                res.writeHead(404, {'Content-Type': 'text/plain'})
-                res.end('There is a sever error')
+                res.writeHead(404, {'Content-Type': "text/plain"})
+                res.end('Page doesnt Found')
 
     }
 })     
