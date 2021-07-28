0af09b97dfb497dd109475f3447e57aefed099db
Avromi Russell <83745803+avromiRussell@users.noreply.github.com>
2021-07-28 17:54:08 +0300

    HW commit

--- 05-Node/03-express/01-basics/HW/Avromi/server.js
+++ 05-Node/03-express/01-basics/HW/Avromi/server.js
@@ -19,21 +19,17 @@ function read() {
 app.use(express.json());
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
     const readData = read()
     res.send(readData)
-=======
-
-    res.send(students) //YS: Where is this variable coming from? 
->>>>>>> c347afc173738aaea66ac149f3dfd0a28bec4354
 })
 
 app.get('/getStudents', (req, res) => {
-    res.send({students}) //YS: Where is this variable coming from? 
+    res.send({
+        students
+    })
 })
 
 //get by name
-<<<<<<< HEAD
 app.get('/getStudent', (req, res) => {
     const readData = read()
   
@@ -47,18 +43,6 @@ app.get('/getStudent', (req, res) => {
 })
 
 app.post('/addStudent', (req, res) => {
-=======
-app.get('/getStudent', (req, res) => { 
-    console.log(req.query); //YS: You should be doing this by id and setting the route to:  /getStudent/:id
-    let searchedStudents = students.filter(student => student.name === req.query.name); //YS: Use find instead of filter. 
-    res.send({
-        ok: true,
-        students: searchedStudents
-    })
-})
-
-app.post('/getStudent', (req, res) => { //YS: This route should be /addStudent
->>>>>>> c347afc173738aaea66ac149f3dfd0a28bec4354
     const readData = read()
 
     const {
@@ -82,7 +66,7 @@ app.post('/getStudent', (req, res) => { //YS: This route should be /addStudent
     }
 })
 
-app.put('/editStudent', (req, res) => { //YS: This route should be /editStudent/:id  (I know that we did this together, but its better if you start using it this way)
+app.put('/editStudent', (req, res) => {
     const readData = read()
  
     const {
@@ -99,11 +83,10 @@ app.put('/editStudent', (req, res) => { //YS: This route should be /editStudent/
             studentToEdit
         );
     } catch (error) {
-        res.status(400).send(error.message)  
+        res.status(400).send(error.message)
     }
 })
 
-<<<<<<< HEAD
 app.delete('/delStudent', (req, res) => {
     const readData = read()
   
@@ -121,15 +104,6 @@ app.delete('/delStudent', (req, res) => {
         res.status(400).send(error.message)
     }
 })
-=======
-// YS: Why wasnt this implemented? 
-
-// app.delete('/delStudent', (req, res)=>{
-//     console.log(req.query);
-//     let searchedStudents = students.filter(student=>student.name !== req.query.name);
-//     res.send({ok:true, students:searchedStudents})
-// })
->>>>>>> c347afc173738aaea66ac149f3dfd0a28bec4354
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
