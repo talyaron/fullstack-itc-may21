b4937f4e1ddbc9e8309f31bff2d940c3ca1f1d34
Efraim Dimbleby <82100352+EfraimDim@users.noreply.github.com>
2021-07-28 21:17:18 +0300

    finish

--- 05-Node/03-express/params-query/task2/JulieEfraim/dist/app.dev.js
+++ 05-Node/03-express/params-query/task2/JulieEfraim/dist/app.dev.js
@@ -6,8 +6,15 @@ app = express();
 var port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express["static"]('public'));
-app.post('/add', function (req, res) {
-  console.log(req.body);
+var jokesArray = [];
+app.post('/addJoke', function (req, res) {
+  jokesArray.push(req.body);
+  console.log(jokesArray);
+});
+app.get('/getJoke', function (req, res) {
+  res.send({
+    jokesArray: jokesArray
+  });
 });
 app.listen(port, function () {
   console.log('Server listen on port', port);
