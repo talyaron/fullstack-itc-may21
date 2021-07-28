6f05d09c2c95cd7674218ea9e02e6e2588be0a43
RazielAF <82554306+RazielAF@users.noreply.github.com>
2021-07-28 20:25:38 +0300

    v

--- 05-Node/03-express/params-query/task1/Eric and Raziel/server.js
+++ 05-Node/03-express/params-query/task1/Eric and Raziel/server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app = express();
 const fs=require('fs');
 const port =process.env.PORT || 7000;
-
+app.use(express.json());
 app.use(express.static('public'));
 
 
