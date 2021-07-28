13a80d9115177d5d1fd494f1aa7c0a257482dc9e
Avromi Russell <83745803+avromiRussell@users.noreply.github.com>
2021-07-28 17:50:21 +0300

    commit HW node server

--- 05-Node/03-express/01-basics/HW/Avromi/server.js
+++ 05-Node/03-express/01-basics/HW/Avromi/server.js
@@ -1,9 +1,13 @@
-const { randomUUID } = require('crypto')
+const {
+    randomUUID
+} = require('crypto')
 const express = require('express')
 const app = express()
 const port = 3000
 const fs = require('fs')
-const { v4: uuidv4 } = require('uuid');
+const {
+    v4: uuidv4
+} = require('uuid');
 
 
 
@@ -15,8 +19,8 @@ function read() {
 app.use(express.json());
 
 app.get('/', (req, res) => {
-
-    res.send(students)
+    const readData = read()
+    res.send(readData)
 })
 
 app.get('/getStudents', (req, res) => {
@@ -27,26 +31,35 @@ app.get('/getStudents', (req, res) => {
 
 //get by name
 app.get('/getStudent', (req, res) => {
-    console.log(req.query);
-    let searchedStudents = students.filter(student => student.name === req.query.name);
-    res.send({
-        ok: true,
-        students: searchedStudents
-    })
+    const readData = read()
+  
+    let searchedStudents = readData.filter(student => student.name === req.query.name);
+    
+        res.send({
+            ok: true,
+            students: searchedStudents
+        })
+  
 })
 
-app.post('/getStudent', (req, res) => {
+app.post('/addStudent', (req, res) => {
     const readData = read()
-    console.log(readData);
-    const {name} = req.body
-    const student = {name, id:uuidv4()}
+
+    const {
+        name
+    } = req.body
+    const student = {
+        name,
+        id: uuidv4()
+    }
     readData.push(student);
     try {
 
         fs.writeFileSync('./students.json', JSON.stringify(readData))
 
         res.status(200).send({
-            ok: true, readData
+            ok: true,
+            readData
         });
     } catch (error) {
         res.status(400).send(error.message)
@@ -55,27 +68,42 @@ app.post('/getStudent', (req, res) => {
 
 app.put('/editStudent', (req, res) => {
     const readData = read()
-    console.log(readData);
-    const {id, newName} = req.body
-    const studentToEdit = readData.find(student=>student.id === id)
+ 
+    const {
+        id,
+        newName
+    } = req.body
+    const studentToEdit = readData.find(student => student.id === id)
     studentToEdit.name = newName
     try {
 
         fs.writeFileSync('./students.json', JSON.stringify(readData))
 
         res.status(200).send(
-           studentToEdit
+            studentToEdit
         );
     } catch (error) {
         res.status(400).send(error.message)
     }
 })
 
-// app.delete('/delStudent', (req, res)=>{
-//     console.log(req.query);
-//     let searchedStudents = students.filter(student=>student.name !== req.query.name);
-//     res.send({ok:true, students:searchedStudents})
-// })
+app.delete('/delStudent', (req, res) => {
+    const readData = read()
+  
+    const studentId = req.query.id;
+    const newStudentData = readData.filter(student => student.id !== studentId)
+
+    try {
+
+        fs.writeFileSync('./students.json', JSON.stringify(newStudentData))
+
+        res.status(200).send(
+            newStudentData, 'student has been deleted'
+        );
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
