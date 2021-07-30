7b78a9bee1c530e7b088e188109c9ca0f719c7ec
leomza <leo_mza1991@hotmail.com>
2021-07-30 14:25:33 +0300

    Update

---
+++ 05-Node/04-async/task1/Leonardo/dist/app.dev.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/* On the server stored data of beverages. The data should contain an image of the beverages
+On the client get the data and print it to the DOM with the image and text.
+One function will be with promise and the other function will be with async-await */
+var express = require('express');
+
+app = express();
+var port = process.env.PORT || 3000;
+var beverages = [{
+  name: 'Fernet Branca',
+  src: "https://cdn11.bigcommerce.com/s-abmjjefojj/images/stencil/640w/products/2630/12221/Fernet-Argentina_Select__23847.1598921968.jpg?c=1"
+}, {
+  name: "Gancia",
+  src: "https://www.espaciovino.com.ar/media/default/0001/56/thumb_55057_default_big.jpeg"
+}];
+app.use(express.json());
+app.use(express["static"]('public'));
+app.get('/getBeverages', function (req, res) {
+  res.send(beverages);
+});
+app.listen(port, function () {
+  console.log('Server listen on port', port);
+});
