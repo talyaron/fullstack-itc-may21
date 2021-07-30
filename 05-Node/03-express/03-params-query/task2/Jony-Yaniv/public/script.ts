0d8a15cd81c5b2c43a436c74bb9d36efb7195192
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-07-30 11:10:51 +0300

    .

--- 05-Node/03-express/03-params-query/task2/Jony-Yaniv/public/script.ts
+++ 05-Node/03-express/03-params-query/task2/Jony-Yaniv/public/script.ts
@@ -6,14 +6,14 @@ function handleJoke(ev: any): void {
     ev.preventDefault();
 
     const joke: string = ev.target.elements.newJoke.value;
-    console.log(joke)
+    console.log(joke);
 
     axios.post('/addJoke',{ joke })
     .then(res=>{
-        console.log(res.data)
+        console.log(res.data);
     })
     .catch(e=>{
-        console.error(e)
+        console.error(e);
     })
 
     ev.target.reset();
