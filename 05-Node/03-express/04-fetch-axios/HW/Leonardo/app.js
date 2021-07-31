fa77448e0fc2fbe38c2de962f3fc5da56b544b6b
leomza <leo_mza1991@hotmail.com>
2021-08-01 00:30:20 +0300

    Update

--- 05-Node/03-express/04-fetch-axios/HW/Leonardo/app.js
+++ 05-Node/03-express/04-fetch-axios/HW/Leonardo/app.js
@@ -42,22 +42,110 @@ app.post('/addStudent', (req, res) => {
 
 //This route is to show all the students
 app.get('/getStudents', (req, res) => {
-    res.send(students.list);
+    try {
+        res.send(students.list);
+    } catch (error) {
+        res.status(400).send({ error: e.message });
+    }
 });
 
 
 //This route will redirect the user to see the information of a student by params
 app.get('/showStudentParam/:id', (req, res) => {
-    let searchedStudent = students.list.find(student => student.id === req.params.id);
-    res.send([searchedStudent]);
-})
+    try {
+        let searchedStudent = students.list.find(student => student.id === req.params.id);
+        res.send([searchedStudent]);
+    } catch (error) {
+        res.status(400).send({ error: e.message });
+    }
+});
 
 //This route will redirect the user to see the information of a student by querys
 app.get('/showStudentQuery', (req, res) => {
-    let searchedStudent = students.list.find(student => student.id === req.query.id);
-    res.send([searchedStudent]);
+    try {
+        let searchedStudent = students.list.find(student => student.id === req.query.id);
+        res.send([searchedStudent]);
+    } catch (error) {
+        res.status(400).send({ error: e.message });
+    }
 })
 
-app.listen(port, () => {
-    console.log(`The server is running at port:${port}`)
+//This route is to delete a student
+app.delete('/deleteStudent/:id', (req, res) => {
+    try {
+        let { id } = req.params
+        students.list = students.list.filter(student => student.id !== id);
+        res.send(students.list);
+    } catch (e) {
+        res.status(400).send({ error: e.message });
+    }
 });
+
+
+//This route is to sort the table (I tried not to DRY so I created a Switch, please Yona let me know if you have another idea on how to do it👏 )
+//Create the next boolean to control the sort when user clicks:
+let order = true;
+
+app.get('/sortTable/:orderBy', (req, res) => {
+    try {
+        let { orderBy } = req.params
+        switch (orderBy) {
+            case 'firstname':
+                if (order === true) {
+                    const sort = students.list.sort((a, b) => a.firstname.localeCompare(b.firstname))
+                    res.send(sort);
+                    order = false;
+                } else if (order === false) {
+                    const sort = students.list.sort((a, b) => b.firstname.localeCompare(a.firstname))
+                    res.send(sort);
+                    order = true;
+                }
+                break;
+            case 'lastname':
+                if (order === true) {
+                    const sort = students.list.sort((a, b) => a.lastname.localeCompare(b.lastname))
+                    res.send(sort);
+                    order = false;
+                } else if (order === false) {
+                    const sort = students.list.sort((a, b) => b.lastname.localeCompare(a.lastname))
+                    res.send(sort);
+                    order = true;
+                }
+                break;
+            case 'age':
+                if (order === true) {
+                    const sort = students.list.sort((a, b) => a.age - b.age)
+                    res.send(sort);
+                    order = false;
+                } else if (order === false) {
+                    const sort = students.list.sort((a, b) => b.age - a.age)
+                    res.send(sort);
+                    order = true;
+                }
+                break;
+            case 'average':
+                if (order === true) {
+                    const sort = students.list.sort((a, b) => a.averageGrade - b.averageGrade)
+                    res.send(sort);
+                    order = false;
+                } else if (order === false) {
+                    const sort = students.list.sort((a, b) => b.averageGrade - a.averageGrade)
+                    res.send(sort);
+                    order = true;
+                }
+                break;
+        }
+    } catch (error) {
+        res.status(400).send({ error });
+    }
+})
+
+//This function is to listen to the port
+app.listen(port, () => {
+    try {
+        console.log(`The server is running at port:${port}`)
+    } catch (error) {
+        res.status(500).send({ error: 'There is an error in the port' });
+    }
+});
+
