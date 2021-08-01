8fb365205473c03cb3277ccb5126a9e19d7d371f
Avromi Russell <83745803+avromiRussell@users.noreply.github.com>
2021-08-01 08:21:32 +0300

    pull code

--- 05-Node/04-async/task1/Avromi/public/index.ts
+++ 05-Node/04-async/task1/Avromi/public/index.ts
@@ -1,25 +1,25 @@
 
-async function getData(){
+async function getData() {
 
- const r = await fetch('/');
- console.log('waited');
- const cocktails = await r.json();
- console.log(cocktails);
-render(cocktails)
+    const r = await fetch('/');
+    console.log('waited');
+    const cocktails = await r.json();
+    console.log(cocktails);
+    render(cocktails)
 
 }
 
 
-    getData();
+getData();
 
 
-function render(data){
+function render(data) {
 
     let html = '';
-    data.forEach(data=>{
+    data.forEach(data => {
         html += `<h4>${data.strDrink}</h4>
         <img src="${data.strDrinkThumb}">`
     })
     document.querySelector(`.coctails`).innerHTML = html
-   
+
 }
