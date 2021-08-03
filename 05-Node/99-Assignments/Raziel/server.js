bc255a897a08b9c8bfe8ed731a1e52273e5a5381
RazielAF <82554306+RazielAF@users.noreply.github.com>
2021-08-03 23:46:33 +0300

    v

--- 05-Node/99-Assignments/Raziel/server.js
+++ 05-Node/99-Assignments/Raziel/server.js
@@ -2,4 +2,9 @@ const express = require('express');
 const app = express();
 const port= process.env.const || 0007;
 
+
+
+class Task{
+    
+}
 app.listen(port, () => { console.log('Server listen on port', port) })
