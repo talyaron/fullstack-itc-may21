d2d85b22459ac406b8c821b66a1cc82ce618d81c
leomza <leo_mza1991@hotmail.com>
2021-08-05 02:41:41 +0300

    Update

--- 05-Node/99-Assignments/01-CRUD-Express/Leonardo/public/dist/script.dev.js
+++ 05-Node/99-Assignments/01-CRUD-Express/Leonardo/public/dist/script.dev.js
@@ -272,45 +272,4 @@ function handleEdit(ev) {
   }, null, null, [[1, 19]]);
 }
 
-; ///////////////////
-
-function onDragStart(event) {
-  event.dataTransfer.setData('text/plain', event.target.id);
-  event.currentTarget.style.backgroundColor = 'yellow';
-  event.currentTarget.style.borderRadius = '0px';
-}
-
-function onDragOver(event) {
-  event.preventDefault();
-}
-
-function onDrop(event) {
-  var idDraggable = event.dataTransfer.getData('text');
-  var idDropeable = event.target.id;
-  var draggableElement = document.getElementById(idDraggable);
-  handleStyleBackground(draggableElement, idDropeable);
-  var dropzone = event.target;
-  dropzone.appendChild(draggableElement);
-  event.dataTransfer.clearData();
-}
-
-function handleStyleBackground(draggableElement, idDropeable) {
-  switch (idDropeable) {
-    case 'toDo':
-      draggableElement.style.backgroundColor = '#32cd32';
-      draggableElement.style.borderRadius = '50px';
-      break;
-
-    case 'inProgress':
-      draggableElement.style.backgroundColor = '#1e90ff';
-      draggableElement.style.borderRadius = '50px';
-      break;
-
-    case 'done':
-      draggableElement.style.backgroundColor = 'violet';
-      draggableElement.style.borderRadius = '50px';
-      break;
-  }
-}
-
 ;
