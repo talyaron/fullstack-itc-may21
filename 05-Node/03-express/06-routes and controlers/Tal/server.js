6417805ec794d7e926da2ed2aafcb5bf17424c88
Tal <tal.yaron@gmail.com>
2021-08-04 13:33:42 +0300

    routes init

---
+++ 05-Node/03-express/06-routes and controlers/Tal/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+app.use(static)
+// Home page route.
+router.get('/', function (req, res) {
+  res.send('Wiki home page');
+})
+
+// About page route.
+router.get('/about', function (req, res) {
+  res.send('About this wiki');
+})
+
+module.exports = router;
