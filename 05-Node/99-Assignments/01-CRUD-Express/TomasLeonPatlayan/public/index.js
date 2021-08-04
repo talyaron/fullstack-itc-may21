4fdfda91c64b0d699d94a44dbe52ce1e43b6a859
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2021-08-04 13:32:59 +0300

    up

--- 05-Node/99-Assignments/01-CRUD-Express/TomasLeonPatlayan/public/index.js
+++ 05-Node/99-Assignments/01-CRUD-Express/TomasLeonPatlayan/public/index.js
@@ -4,42 +4,44 @@ event.preventDefault();
 let {name}= event.target.elements;
 name= name.value;
 
-const addTask = await fetch(`/addTask`,{method:"POST",data:{name}});
+// const addTask = await fetch(`/addTask`,{method:"POST",data:{name}, headers: {
+//     "Content-Type": "application/json",
+//   }});
 
-const add = await addTask.json();
+// const add = await addTask.json();
 
-renderTask(add)
-// axios({
-//     method: "post",
-//     url: `/addTask`,
-//     data: {
-//       name
-//     },
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   })
-//     .then(({ data }) => data)
-//     .catch((err) => {
-//       console.log(err);
-//     });
-
-
-
-
-
-}
+// renderTask(add)
+axios({
+    method: "post",
+    url: `/addTask`,
+    data: {
+      name
+    },
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(({ data }) => renderTask(data))
+    .catch((err) => {
+      console.log(err);
+    });
 
 
-function renderTask(data) { 
+
+
+
+ }
+
+
+ function renderTask(data) { 
     let html = "";
     data.forEach((task) => {
-      html += `<p>${task.name}</p>
-      <p>${task.age}</p>
-      <p>${task.avgrade}</p>
-                <button onclick='deletePromise(${task.id})'>Delete</button>`; //YS: Very nice! 
+      html += `<p>${task.name}</p>`
+    //   <p>${task.age}</p>
+    //   <p>${task.avgrade}</p>
+    //             <button onclick='deletePromise(${task.id})'>Delete</button>; //YS: Very nice! 
     });
   
     document.getElementById("root").innerHTML = html;
   }
-  
+//   window.onload= renderTask(data)
