2aa6663f9bcc3be252528b4cbceeaeceaff1e054
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2021-08-04 13:07:26 +0300

    up

---
+++ 05-Node/99-Assignments/01-CRUD-Express/TomasLeonPatlayan/public/index.js
@@ -0,0 +1,45 @@
+const postTask = async (event) => {
+event.preventDefault();
+
+let {name}= event.target.elements;
+name= name.value;
+
+const addTask = await fetch(`/addTask`,{method:"POST",data:{name}});
+
+const add = await addTask.json();
+
+renderTask(add)
+// axios({
+//     method: "post",
+//     url: `/addTask`,
+//     data: {
+//       name
+//     },
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   })
+//     .then(({ data }) => data)
+//     .catch((err) => {
+//       console.log(err);
+//     });
+
+
+
+
+
+}
+
+
+function renderTask(data) { 
+    let html = "";
+    data.forEach((task) => {
+      html += `<p>${task.name}</p>
+      <p>${task.age}</p>
+      <p>${task.avgrade}</p>
+                <button onclick='deletePromise(${task.id})'>Delete</button>`; //YS: Very nice! 
+    });
+  
+    document.getElementById("root").innerHTML = html;
+  }
+  
---
+++ 05-Node/99-Assignments/01-CRUD-Express/TomasLeonPatlayan/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+//JSON
+const localJson = () => {
+  const fileJson = fs.readFileSync("./task.db.json");
+  return JSON.parse(fileJson);
+};
+//Settings
+app.set("port", 3500 || process.env.PORT);
+
+//MidelWare
+app.use(express.json());
+
+//Routes
+
+//?GET
+
+app.get("/getTask", (req, res) => {
+  const students = localJson();
+  res.send(students);
+});
+
+//TODO: POST
+app.post("/addTask", (req, res) => {
+  const { name } = req.body;
+  const task = localJson();
+  const addTask = {
+    name: name,
+  };
+
+  task.push(addTask);
+  fs.writeFileSync("./task.db.json", JSON.stringify(task));
+  res.send(task);
+});
+
+app.use(express.static("public"));
+
+//Listen
+app.listen(app.get("port"), () => {
+  console.log(`app listening at http://localhost:${app.get("port")}`);
+});
