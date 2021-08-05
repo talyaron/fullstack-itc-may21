2b6f9f31bc2cf7e4c29e6d58ad919d3bc9ec943d
Eric Kobrinsky <78773449+erickobrinsky@users.noreply.github.com>
2021-08-05 18:20:39 +0300

    d

--- 05-Node/99-Assignments/Eric/public/dist/main.dev.js
+++ 05-Node/99-Assignments/Eric/public/dist/main.dev.js
@@ -1,8 +1,37 @@
 "use strict";
 
-// window.onload = function () {
-// }
-//render
+//Get the employes information:
+function getAllEmployes() {
+  var employesData;
+  return regeneratorRuntime.async(function getAllEmployes$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.prev = 0;
+          _context.next = 3;
+          return regeneratorRuntime.awrap(axios.get('/getEmployes'));
+
+        case 3:
+          employesData = _context.sent;
+          render(employesData.data);
+          _context.next = 10;
+          break;
+
+        case 7:
+          _context.prev = 7;
+          _context.t0 = _context["catch"](0);
+          console.log(_context.t0);
+
+        case 10:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, null, null, [[0, 7]]);
+}
+
+; //render
+
 function render(array) {
   if ($("#addEmployeeModal")) {
     $("#addEmployeeModal").modal("hide"); //se agrega si esta vacio, modificarlo!!
@@ -17,8 +46,9 @@ function render(array) {
       var name = elem.name,
           email = elem.email,
           address = elem.address,
-          phone = elem.phone;
-      html += "<tr>\n\t\t\t<td></td>\n\t\t\t<td>".concat(name, "</td>\n\t\t\t<td>").concat(email, "</td>\n\t\t\t<td>").concat(address, "</td>\n\t\t\t<td>").concat(phone, "</td>\n\t\t\t<td>\n\t\t\t\t<a href=\"#editEmployeeModal\" class=\"edit\" data-toggle=\"modal\"><i class=\"material-icons\" data-toggle=\"tooltip\" title=\"Edit\">&#xE254;</i></a>\n\t\t\t\t<a href=\"#deleteEmployeeModal\" class=\"delete\" data-toggle=\"modal\"><i class=\"material-icons\" data-toggle=\"tooltip\" title=\"Delete\">&#xE872;</i></a>\n\t\t\t</td>\n\t\t</tr>");
+          phone = elem.phone,
+          id = elem.id;
+      html += "<tr>\n\t\t\t<td></td>\n\t\t\t<td>".concat(name, "</td>\n\t\t\t<td>").concat(email, "</td>\n\t\t\t<td>").concat(address, "</td>\n\t\t\t<td>").concat(phone, "</td>\n\t\t\t<td>\n\t\t\t\t<a href=\"#editEmployeeModal\" class=\"edit\" data-toggle=\"modal\"><i class=\"material-icons\" data-toggle=\"tooltip\" title=\"Edit\"  onclick='updateEmploye(\"").concat(id, "\")' >&#xE254;</i></a>\n\t\t\t\t<i class=\"material-icons\" data-toggle=\"tooltip\" title=\"Delete\"  onclick='deleteEmploye(\"").concat(id, "\")' style=\"cursor:pointer\">&#xE872;</i>\n\t\t\t</td>\n\t\t</tr>");
     });
     html += "</tbody></table>";
   } else {
@@ -64,70 +94,60 @@ function handleSubmit(event) {
   } else {
     alert('You need to complete at least one field');
   }
-} //DELETE
-
-
-function deleteRecord(personId) {
-  axios["delete"]('/deleteEmployes', {
-    id: personId
-  }).then(function (res) {
-    console.log(employes);
-    render(res.array.allEmployes);
-  });
-} //tengo que guardar el id del que estoy clickeando para borrar y mandarlo a deleteRecord para eliminarlo
-
-
-function handleDelete(event) {
-  event.preventDefault();
-  deleteRecord();
-  console.log('hio');
-} //UPDATE
-
-
-function updateEmploye(personId) {
-  var newName = document.getElementById("".concat(personId, "name")).value;
-  axios.put('/updateEmploye', {
-    id: personId,
-    name: newName
-  }).then(function (res) {
-    console.log(res.data.message);
-    render(res.array.allEmployes);
-  });
 }
 
-function handleEdit(event) {
-  event.preventDefault();
-  updateEmploye();
-} //Get the employes information:
-
-
-function getAllEmployes() {
-  var employesData;
-  return regeneratorRuntime.async(function getAllEmployes$(_context) {
+function deleteEmploye(id) {
+  var option, employeData;
+  return regeneratorRuntime.async(function deleteEmploye$(_context2) {
     while (1) {
-      switch (_context.prev = _context.next) {
+      switch (_context2.prev = _context2.next) {
         case 0:
-          _context.prev = 0;
-          _context.next = 3;
-          return regeneratorRuntime.awrap(axios.get('/getEmployes'));
+          _context2.prev = 0;
+          option = confirm("Are you sure do you want to delete this employe?");
 
-        case 3:
-          employesData = _context.sent;
-          render(employesData.data);
-          _context.next = 10;
-          break;
+          if (!option) {
+            _context2.next = 7;
+            break;
+          }
+
+          _context2.next = 5;
+          return regeneratorRuntime.awrap(axios["delete"]("/deleteEmployes/".concat(id)));
+
+        case 5:
+          employeData = _context2.sent;
+          render(employeData.data.allEmployes);
 
         case 7:
-          _context.prev = 7;
-          _context.t0 = _context["catch"](0);
-          console.log(_context.t0);
+          _context2.next = 12;
+          break;
 
-        case 10:
+        case 9:
+          _context2.prev = 9;
+          _context2.t0 = _context2["catch"](0);
+          console.error(_context2.t0);
+
+        case 12:
         case "end":
-          return _context.stop();
+          return _context2.stop();
       }
     }
-  }, null, null, [[0, 7]]);
+  }, null, null, [[0, 9]]);
 }
 
-;
+; //UPDATE
+
+function updateEmploye(id) {} // function updateEmploye(personId) {
+// 	const newName = document.getElementById(`${personId}name`).value;
+// 	axios.put('/updateEmploye', {
+// 			id: personId,
+// 			name: newName
+// 		})
+// 		.then(res => {
+// 			console.log(res.data.message)
+// 			render(res.array.allEmployes)
+// 		})
+// }
+// function handleEdit(event){
+// 	event.preventDefault()
+// 	updateEmploye()
+// }
--- 05-Node/99-Assignments/Eric/public/main.js
+++ 05-Node/99-Assignments/Eric/public/main.js
@@ -1,8 +1,12 @@
-
-
-// window.onload = function () {
-
-// }
+//Get the employes information:
+async function getAllEmployes() {
+    try {
+        const employesData = await axios.get('/getEmployes');
+        render(employesData.data);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 //render
 function render(array) {
@@ -39,7 +43,8 @@ function render(array) {
 				name,
 				email,
 				address,
-				phone
+				phone,
+				id
 			} = elem
 			
 			html += `<tr>
@@ -49,8 +54,8 @@ function render(array) {
 			<td>${address}</td>
 			<td>${phone}</td>
 			<td>
-				<a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-				<a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+				<a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit"  onclick='updateEmploye("${id}")' >&#xE254;</i></a>
+				<i class="material-icons" data-toggle="tooltip" title="Delete"  onclick='deleteEmploye("${id}")' style="cursor:pointer">&#xE872;</i>
 			</td>
 		</tr>`
 		});
@@ -115,58 +120,51 @@ function handleSubmit(event) {
 	
 	}
 
-//DELETE
-function deleteRecord(personId) {
-	axios.delete('/deleteEmployes', {
-			id: personId
-		})
-		.then(res => {
-			console.log(employes)
-			render(res.array.allEmployes)
-		})
-}
-//tengo que guardar el id del que estoy clickeando para borrar y mandarlo a deleteRecord para eliminarlo
-function handleDelete(event){
 
-	event.preventDefault();
-	deleteRecord()
-	console.log('hio');
+	async function deleteEmploye (id) {
+		try {
+			const option = confirm(`Are you sure do you want to delete this employe?`);
+			if (option) {
+				const employeData = await axios.delete(`/deleteEmployes/${id}`);
+				render(employeData.data.allEmployes);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-}
 
 
 //UPDATE
-function updateEmploye(personId) {
-	const newName = document.getElementById(`${personId}name`).value;
-	axios.put('/updateEmploye', {
-			id: personId,
-			name: newName
-		})
-		.then(res => {
-			console.log(res.data.message)
-			render(res.array.allEmployes)
+
+ function updateEmploye (id){
+	 
+ }
+
+
+
+// function updateEmploye(personId) {
+// 	const newName = document.getElementById(`${personId}name`).value;
+// 	axios.put('/updateEmploye', {
+// 			id: personId,
+// 			name: newName
+// 		})
+// 		.then(res => {
+// 			console.log(res.data.message)
+// 			render(res.array.allEmployes)
 		
-		})
-}
+// 		})
+// }
 
-function handleEdit(event){
-	event.preventDefault()
-
-
+// function handleEdit(event){
+// 	event.preventDefault()
 
 
-	
-	updateEmploye()
 
 
-}
 
-//Get the employes information:
-async function getAllEmployes() {
-    try {
-        const employesData = await axios.get('/getEmployes');
-        render(employesData.data);
-    } catch (error) {
-        console.log(error);
-    }
-};
+// 	updateEmploye()
+
+
+// }
+
