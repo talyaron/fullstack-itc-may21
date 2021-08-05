db780790b666a6ee15536c2d00d3e200b3763ad5
RazielAF <82554306+RazielAF@users.noreply.github.com>
2021-08-05 18:30:37 +0300

    c

--- 05-Node/99-Assignments/Raziel/server.dev.js
+++ 05-Node/99-Assignments/Raziel/server.dev.js
@@ -7,46 +7,48 @@ var port = process.env["const"] || 3500;
 
 var fs = require("fs");
 
+var _require = require("uuid"),
+    uuidv4 = _require.v4;
+
+var path = require("path");
+
+var filepath = path.resolve(__dirname, "EntryList.json");
 app.use(express["static"]("public"));
 app.use(express.json());
 
 var readAllEntery = function readAllEntery() {
-  var allEntery = fs.readFileSync("./EntryList.json");
+  var allEntery = fs.readFileSync(filepath);
+  console.log(allEntery);
   return JSON.parse(allEntery);
 };
 
-var allEntery = readAllEntery();
+var allEntery = fs.readFileSync(filepath);
+console.log(allEntery);
 app.post('/addExpense', function (req, res) {
-  var _req$body = req.body,
-      type = _req$body.type,
-      title = _req$body.title,
-      amount = _req$body.amount;
-  var newExpense = {
-    type: 'expense',
-    title: title,
-    amount: amount
-  };
-  allEntery.push(newExpense);
-  fs.writeFileSync("./EntryList.json", JSON.stringify(allEntery));
-  console.log(allEntery);
-  res.send({
-    ok: "Added Expense",
-    task: allEntery
-  });
-}); // app.post('/addIncome',(req, res) => {
-//     const {type, title,amount} = req.body;
-//     const newIncome={
-//         type: 'income',
-//         title: title,
-//         amount: amount
-//     }
-//  const allEntery = readAllEntery();
-//  allEntery.push(newIncome);
-//  fs.writeFileSync("./EntryList.json", JSON.stringify(allEntery ));
-// console.log(allEntery);
-//         res.send({ ok: "Added Expense", task:allEntery  });
-// })
+  try {
+    var _req$body = req.body,
+        type = _req$body.type,
+        title = _req$body.title,
+        amount = _req$body.amount;
+    var newExpense = {
+      type: type,
+      title: title,
+      amount: amount,
+      id: uuidv4()
+    };
+    console.log(newExpense);
 
+    var _allEntery = readAllEntery();
+
+    _allEntery.push(newExpense);
+
+    console.log(_allEntery);
+    fs.writeFileSync("./EntryList.json", JSON.stringify(_allEntery));
+    res.send(_allEntery);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 app.listen(port, function () {
   console.log('Server listen on port', port);
 });
