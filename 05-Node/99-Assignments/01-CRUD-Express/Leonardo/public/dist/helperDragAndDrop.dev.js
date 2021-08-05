04fb26e6f47944bf7ca7246466e3da38276ff29d
leomza <leo_mza1991@hotmail.com>
2021-08-05 23:30:56 +0300

    Update

--- 05-Node/99-Assignments/01-CRUD-Express/Leonardo/public/dist/helperDragAndDrop.dev.js
+++ 05-Node/99-Assignments/01-CRUD-Express/Leonardo/public/dist/helperDragAndDrop.dev.js
@@ -1,14 +1,30 @@
 "use strict";
 
 function onDragStart(event) {
-  event.dataTransfer.setData('text/plain', event.target.id);
-  event.currentTarget.style.backgroundColor = 'yellow';
-  event.currentTarget.style.borderRadius = '0px';
+  try {
+    event.dataTransfer.setData('text/plain', event.target.id);
+    event.currentTarget.style.backgroundColor = 'yellow';
+    event.currentTarget.style.borderRadius = '0px';
+  } catch (error) {
+    console.error(error);
+  }
+
+  ;
 }
 
+;
+
 function onDragOver(event) {
-  event.preventDefault();
+  try {
+    event.preventDefault();
+  } catch (error) {
+    console.error(error);
+  }
+
+  ;
 }
+
+;
 
 function onDrop(event) {
   var idDraggable, idDropeable, draggableElement, dropzone, tasksData, tasks;
@@ -16,24 +32,37 @@ function onDrop(event) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
+          _context.prev = 0;
           idDraggable = event.dataTransfer.getData('text');
           idDropeable = event.target.id;
           draggableElement = document.getElementById(idDraggable);
           dropzone = event.target;
           dropzone.appendChild(draggableElement);
           event.dataTransfer.clearData();
-          _context.next = 8;
+          _context.next = 9;
           return regeneratorRuntime.awrap(axios.put("/editStatusTask/".concat(idDraggable, "/").concat(idDropeable)));
 
-        case 8:
+        case 9:
           tasksData = _context.sent;
           tasks = tasksData.data.tasks;
           renderTask(tasks);
+          _context.next = 17;
+          break;
 
-        case 11:
+        case 14:
+          _context.prev = 14;
+          _context.t0 = _context["catch"](0);
+          console.error(_context.t0);
+
+        case 17:
+          ;
+
+        case 18:
         case "end":
           return _context.stop();
       }
     }
-  });
+  }, null, null, [[0, 14]]);
 }
+
+;
--- 05-Node/99-Assignments/01-CRUD-Express/Leonardo/public/helperDragAndDrop.js
+++ 05-Node/99-Assignments/01-CRUD-Express/Leonardo/public/helperDragAndDrop.js
@@ -1,23 +1,35 @@
 function onDragStart(event) {
-    event.dataTransfer.setData('text/plain', event.target.id);
-    event.currentTarget.style.backgroundColor = 'yellow';
-    event.currentTarget.style.borderRadius = '0px';
-}
+    try {
+        event.dataTransfer.setData('text/plain', event.target.id);
+        event.currentTarget.style.backgroundColor = 'yellow';
+        event.currentTarget.style.borderRadius = '0px';
+    } catch (error) {
+        console.error(error);
+    };
+};
 
 function onDragOver(event) {
-    event.preventDefault();
-}
+    try {
+        event.preventDefault();
+    } catch (error) {
+        console.error(error);
+    };
+};
 
 async function onDrop(event) {
-    const idDraggable = event.dataTransfer.getData('text');
-    const idDropeable = event.target.id;
+    try {
+        const idDraggable = event.dataTransfer.getData('text');
+        const idDropeable = event.target.id;
 
-    const draggableElement = document.getElementById(idDraggable);
-    const dropzone = event.target;
-    dropzone.appendChild(draggableElement);
-    event.dataTransfer.clearData();
+        const draggableElement = document.getElementById(idDraggable);
+        const dropzone = event.target;
+        dropzone.appendChild(draggableElement);
+        event.dataTransfer.clearData();
 
-    let tasksData = await axios.put(`/editStatusTask/${idDraggable}/${idDropeable}`);
-    const { tasks } = tasksData.data;
-    renderTask(tasks);
-}
+        let tasksData = await axios.put(`/editStatusTask/${idDraggable}/${idDropeable}`);
+        const { tasks } = tasksData.data;
+        renderTask(tasks);
+    } catch (error) {
+        console.error(error);
+    };
+};
