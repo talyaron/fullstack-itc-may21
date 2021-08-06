1b4eb9ae5a081e81d909059aca51e20765b61117
Yarden-Tal <yardental1989@gmail.com>
2021-08-06 04:02:17 -0400

    commit

--- 05-Node/99-Assignments/01-CRUD-Express/yarden/public/dist/app.dev.js
+++ 05-Node/99-Assignments/01-CRUD-Express/yarden/public/dist/app.dev.js
@@ -2,16 +2,33 @@
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
 /*
 This is the client side main js file.
 */
-var Task = function Task(text) {
-  _classCallCheck(this, Task);
+var Task =
+/*#__PURE__*/
+function () {
+  function Task(text) {
+    _classCallCheck(this, Task);
 
-  this.text = text;
-  this.id = Math.random().toString(16).slice(2);
-  this.isDone = false;
-}; // The tasks array
+    this.text = text;
+    this.id = Math.random().toString(16).slice(2);
+    this.isDone = false;
+  }
+
+  _createClass(Task, [{
+    key: "update",
+    value: function update(newTask) {
+      this.text = newTask.text;
+    }
+  }]);
+
+  return Task;
+}(); // The tasks array
 
 
 var tasks = [];
@@ -26,10 +43,11 @@ function handleSubmit(ev) {
   var taskText = ev.target[0].value;
   if (!taskText) throw new Error('Please enter description.');
   var newTask = new Task(taskText);
-  tasks.unshift(newTask);
+  tasks.push(newTask);
   console.log(tasks);
   renderTaskList();
   modal.classList.add('hide');
+  ev.target.reset();
 }
 /* Render */
 
@@ -42,22 +60,46 @@ var renderTaskList = function renderTaskList() {
   }
 
   var taskHtml = tasks.map(function (task) {
-    return "\n<div class=\"task\" draggable=\"true\">  \n    <div class=\"task__done-button\">\n        <img src=\"./images/sword.png\" alt=\"Done\" onclick='toggleDone(\"".concat(task.id, "\")'>\n    </div>\n    <p class=\"task__text ").concat(checkIsDone(task.isDone), "\">").concat(task.text, "</p>\n    <div class=\"task__buttons-wrapper\">\n        <div class=\"edit-button-wrapper\">\n            <i onclick='handleUpdate(\"").concat(task.id, "\")' class=\"fas fa-pencil-alt fa-lg\"></i>\n        </div>\n        <div class=\"delete-button-wrapper\">\n            <i onclick='handleDelete(\"").concat(task.id, "\")' class=\"fas fa-trash fa-lg\"></i>\n        </div>\n    </div>\n</div> \n");
+    return "\n<div class=\"task\" draggable=\"true\">  \n    <div class=\"task__done-button\">\n        <img src=\"./images/sword.png\" alt=\"Done\" onclick='toggleDone(\"".concat(task.id, "\")'>\n    </div>\n    <p class=\"task__text ").concat(checkIsDone(task.isDone), "\">").concat(task.text, "</p>\n    <div class=\"task__buttons-wrapper\">\n        <div class=\"edit-button-wrapper\">\n            <i onclick='getIdForUpdate(\"").concat(task.id, "\")' class=\"fas fa-pencil-alt fa-lg\"></i>\n        </div>\n        <div class=\"delete-button-wrapper\">\n            <i onclick='handleDelete(\"").concat(task.id, "\")' class=\"fas fa-trash fa-lg\"></i>\n        </div>\n    </div>\n</div> \n");
   }).join('');
   document.querySelector('.list__body').innerHTML = taskHtml;
-};
+}; // async function getAllTasks() {
+//     try {
+//         const tasksData = await axios.get('/getAllTasks');
+//         renderTask(tasksData.data);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
 /* Update */
 
 
-function handleUpdate(ev) {}
+function getIdForUpdate(taskId) {
+  openEditModal();
+  var taskToUpdate = tasks.find(function (task) {
+    return task.id === taskId;
+  });
+  return taskToUpdate.id;
+}
 
-var updateTask = function updateTask(ev) {};
+function handleUpdate(ev) {
+  ev.preventDefault();
+  ev.target.reset();
+}
 /* Delete */
 
 
-function handleDelete(ev) {}
-
-var deleteTask = function deleteTask(ev) {};
+function handleDelete(taskId) {
+  var taskToDelete = tasks.find(function (task) {
+    return task.id === taskId;
+  });
+  var filteredTasks = tasks.filter(function (task) {
+    return task.id !== taskToDelete.id;
+  });
+  tasks = filteredTasks;
+  renderTaskList();
+}
 /* Send info to server with axios */
 // Handle done:
 
--- 05-Node/99-Assignments/01-CRUD-Express/yarden/server.js
+++ 05-Node/99-Assignments/01-CRUD-Express/yarden/server.js
@@ -8,6 +8,28 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.static('public'))
 
+let tasksOnServer = []
+
+// GET (CRUD:READ) tasks
+app.get('/getAllTasks', (req, res) => {
+    try {
+        res.send(tasksOnServer)
+    } catch (er) {
+        console.error(er)
+    }
+})
+app.get('/tasks/:id', (req, res)=>{
+    res.send(tasksOnServer)
+})
+
+// POST (CRUD:Create) tasks
+
+
+// PUT (CRUD:Update) tasks
+
+
+// DELETE (CRUD:Delete) tasks
+
 
 
 //Listen on port:
