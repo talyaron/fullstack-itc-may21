32ab3e61222ad17b65d075845318923798bf39b7
Eric Kobrinsky <78773449+erickobrinsky@users.noreply.github.com>
2021-08-06 18:24:01 +0300

    node

---
+++ 05-Node/03-express/07-cookies/eric/server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+
+
+// app.use(cookieParser())
+// // set a cookie
+// app.use(function (req, res, next) {
+//   // check if client sent cookie
+//   // console.log(req.cookies)
+//   const {cookieName} = req.cookies;
+//   if (cookieName === undefined) {
+//     console.log("cookie wasn't set")
+//     // no: set a new cookie
+//     var randomNumber=Math.random().toString();
+//     randomNumber=randomNumber.substring(2,randomNumber.length);
+//     res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
+//     console.log('cookie created successfully');
+//   } else {
+//     // yes, cookie was already present 
+//     console.log('cookie exists', cookieName);
+//   } 
+//   next(); // <-- important!
+// });
+
+app.use(cookieParser());
+
+
+app.get('/getData', (req, res) => {
+
+
+
+    //read cookies
+        //  console.log(req.cookies);
+        //  const { cookieName } = req.cookies;
+        //  const cookie = JSON.parse(cookieName);
+        //  console.log(cookie);
+        //  const {name} = cookie;
+        //  console.log(name);
+
+    //write a cookie
+
+     const name1 = JSON.stringify({ name: 'Raziel' })
+     res.cookie('cookieName', name1, { maxAge: 3000, httpOnly: true });
+
+     //res.cookie('cookieName',"hi", { maxAge: 3000, httpOnly: true });
+     res.send({ ok: 'Funca' }) //data
+})
+
+app.get('/user', (req, res)=>{
+
+    //get name from the cookie
+
+    //read cookies
+    console.log(req.cookies);
+    const { cookieName } = req.cookies
+    const cookie = JSON.parse(cookieName);
+    console.log(cookie)
+    const {name} = cookie;
+    console.log(name)
+
+    res.send({name:name});
+
+})
+
+app.use(express.static('public'));
+app.listen(3000, function () { console.log('listen on 3000'); });
