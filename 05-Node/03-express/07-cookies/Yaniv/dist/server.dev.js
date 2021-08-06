f294c836454879a7320a5e38a1b6470f8cdb748e
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-08-06 11:11:25 +0300

    ..

---
+++ 05-Node/03-express/07-cookies/Yaniv/dist/server.dev.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var express = require('express');
+
+var app = express();
+
+var cookieParser = require('cookie-parser');
+
+var port = process.env.PORT || 5555;
+app.get('/getData', function (req, res) {
+  res.cookie('myFirstCookie', "Eat me!", {
+    maxAge: 60000,
+    httpOnly: true
+  });
+  res.send({
+    success: true
+  });
+});
+app.use(express["static"]('public'));
+app.listen(port, function () {
+  console.log("listening on port ".concat(port, "..."));
+});
---
+++ 05-Node/03-express/07-cookies/Yaniv/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const port = process.env.PORT || 5555;
+
+app.get('/getData', (req ,res)=>{
+
+
+    res.cookie('myFirstCookie',"Eat me!", { maxAge: 60000, httpOnly: true });
+    res.send({success:true})
+})
+
+app.use(express.static('public'));
+
+app.listen(port, () => { console.log(`listening on port ${port}...`); });
