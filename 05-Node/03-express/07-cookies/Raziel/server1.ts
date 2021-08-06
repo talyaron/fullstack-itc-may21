4c077e6cbb90489c3d9ff8d94a707297f64a3188
RazielAF <82554306+RazielAF@users.noreply.github.com>
2021-08-06 11:33:38 +0300

    cookies

---
+++ 05-Node/03-express/07-cookies/Raziel/server1.ts
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = require('express')()
+const cookieParser = require('cookie-parser');
+
+app.use(express.static('public'))
+
+app.use(cookieParser());
+app.get('/getData', (req ,res)=>{
+
+
+    console.log(req.cookie);
+    //   const {cookieName}=req.cookie;
+      
+
+    const name =JSON.stringify({name:"Cookie MOnster"});
+    res.cookie('cookieName',name, { maxAge: 8000000, httpOnly: true });
+    res.send({ok:true})
+
+})
+
+
+
+
+app.listen(3000, ()=>{console.log('listen on 3000')})
