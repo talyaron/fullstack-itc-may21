4cd3a089efc20c9bcc88bf3547e26f943c8d34af
Eric Kobrinsky <78773449+erickobrinsky@users.noreply.github.com>
2021-08-08 16:49:26 +0300

    ss

--- 05-Node/03-express/07-cookies/eric/server.js
+++ 05-Node/03-express/07-cookies/eric/server.js
@@ -2,49 +2,26 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 
+app.use(express.json());
 
-// app.use(cookieParser())
-// // set a cookie
-// app.use(function (req, res, next) {
-//   // check if client sent cookie
-//   // console.log(req.cookies)
-//   const {cookieName} = req.cookies;
-//   if (cookieName === undefined) {
-//     console.log("cookie wasn't set")
-//     // no: set a new cookie
-//     var randomNumber=Math.random().toString();
-//     randomNumber=randomNumber.substring(2,randomNumber.length);
-//     res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
-//     console.log('cookie created successfully');
-//   } else {
-//     // yes, cookie was already present 
-//     console.log('cookie exists', cookieName);
-//   } 
-//   next(); // <-- important!
-// });
 
 app.use(cookieParser());
 
 
-app.get('/getData', (req, res) => {
+app.post('/getData', (req, res) => {
 
 
 
-    //read cookies
-        //  console.log(req.cookies);
-        //  const { cookieName } = req.cookies;
-        //  const cookie = JSON.parse(cookieName);
-        //  console.log(cookie);
-        //  const {name} = cookie;
-        //  console.log(name);
+  
+    const {userName, password} = req.body
 
-    //write a cookie
 
-     const name1 = JSON.stringify({ name: 'Raziel' })
-     res.cookie('cookieName', name1, { maxAge: 3000, httpOnly: true });
 
-     //res.cookie('cookieName',"hi", { maxAge: 3000, httpOnly: true });
-     res.send({ ok: 'Funca' }) //data
+
+     const loginData = JSON.stringify({ name: userName, password: password })
+     res.cookie('cookieName', loginData, { maxAge: 3000, httpOnly: true });
+
+     res.send({ ok: 'Press accept to see your login data'})
 })
 
 app.get('/user', (req, res)=>{
@@ -56,10 +33,9 @@ app.get('/user', (req, res)=>{
     const { cookieName } = req.cookies
     const cookie = JSON.parse(cookieName);
     console.log(cookie)
-    const {name} = cookie;
+    const {name, password} = cookie;
     console.log(name)
-
-    res.send({name:name});
+    res.send({name:name, password: password});
 
 })
 
