8b963b3ae23d70023835507daf4bb70cf4cc8fd6
RazielAF <82554306+RazielAF@users.noreply.github.com>
2021-08-09 09:15:40 +0300

    K

--- 05-Node/03-express/07-cookies/HW/Raziel/server.dev.js
+++ 05-Node/03-express/07-cookies/HW/Raziel/server.dev.js
@@ -1,31 +1,94 @@
 "use strict";
 
-var express = require('express');
+var express = require("express");
 
 var app = express();
 
-var fs = require('fs');
+var fs = require("fs");
 
-var port = process.env["const"] || 3000; // const cookieParser = require("cookie-parser");
-// app.use(cookieParser());
-// app.use(express.json());
+var port = process.env["const"] || 3000;
 
-app.use(express["static"]('public')); // function readUserData(){
-//     const userData = fs.readFileSync("./userData.json");
-//     return JSON.parse(userData);
-// }
-// app.post("/signUp", (req, res) => {
-//     const { name,password } = req.body;
-//     const users = readUserData();
-//     const addUser = {
-//       name: name,
-//       password: password,
-//     };
-//     users.push(addUser);
-//     fs.writeFileSync("./userData.json", JSON.stringify(users));
-//     res.send(users);
-//   });
+var cookieParser = require("cookie-parser");
 
+var e = require("express");
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express["static"]("public"));
+
+function readUserData() {
+  var userData = fs.readFileSync("./userData.json");
+  return JSON.parse(userData);
+}
+
+app.post("/signUp", function (req, res) {
+  try {
+    var _req$body = req.body,
+        name = _req$body.name,
+        password = _req$body.password;
+    var userData = readUserData();
+    var addUser = {
+      name: name,
+      password: password
+    };
+    userData.push(addUser);
+    fs.writeFileSync("./userData.json", JSON.stringify(users));
+    res.send(users);
+  } catch (error) {
+    console.error(error);
+  }
+});
+app.post("/loginUser", function (req, res) {
+  try {
+    var _req$body2 = req.body,
+        email = _req$body2.email,
+        password = _req$body2.password;
+    var userData = readUserData();
+    var isUserPassOK = userData.some(function (elem) {
+      return elem.email === email && elem.password === password;
+    });
+
+    if (isUserPassOK) {
+      var userLogin = userData.find(function (elem) {
+        return elem.email === email && elem.password === password;
+      });
+      res.cookie("cookieName", JSON.stringify(userLogin), {
+        maxAge: 3000,
+        httpOnly: true
+      });
+      res.send({
+        ok: "Hello Raziel"
+      });
+    } else {
+      throw new Error("wrong email or password");
+    }
+
+    app.get("/getCookie", function (req, res) {
+      try {
+        var cookieName = req.cookies.cookieName;
+        var cookie = JSON.parse(cookieName);
+        res.send(cookie);
+      } catch (e) {
+        res.status(500).send({
+          error: "".concat(e.message)
+        });
+      }
+    });
+  } catch (e) {
+    console.error(e);
+  }
+});
+app.get("/getCookie", function (req, res) {
+  try {
+    var cookieName = req.cookies.cookieName;
+    var cookie = JSON.parse(cookieName);
+    res.send(cookie);
+  } catch (e) {
+    res.status(500).send({
+      error: "".concat(e.message)
+    });
+  }
+});
 app.listen(port, function () {
-  console.log('Server listen on port', port);
+  console.log("Server listen on port", port);
 });
--- 05-Node/03-express/07-cookies/HW/Raziel/server.js
+++ 05-Node/03-express/07-cookies/HW/Raziel/server.js
@@ -1,35 +1,79 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs');
-const port= process.env.const || 3000;
-// const cookieParser = require("cookie-parser");
+const fs = require("fs");
+const port = process.env.const || 3000;
+const cookieParser = require("cookie-parser");
+const e = require("express");
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.static("public"));
 
+function readUserData() {
+  const userData = fs.readFileSync("./userData.json");
+  return JSON.parse(userData);
+}
 
+app.post("/signUp", (req, res) => {
+  try {
+    const { name, password } = req.body;
+    const userData = readUserData();
+    const addUser = {
+      name: name,
+      password: password,
+    };
+    userData.push(addUser);
+    fs.writeFileSync("./userData.json", JSON.stringify(users));
+    res.send(users);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-// app.use(cookieParser());
+app.post("/loginUser", function (req, res) {
+  try {
+    const { email, password } = req.body;
 
-// app.use(express.json());
-app.use(express.static('public'));
+    const userData = readUserData();
 
+    const isUserPassOK = userData.some(
+      (elem) => elem.email === email && elem.password === password
+    );
 
-// function readUserData(){
-//     const userData = fs.readFileSync("./userData.json");
-//     return JSON.parse(userData);
-// }
+    if (isUserPassOK) {
+      const userLogin = userData.find(
+        (elem) => elem.email === email && elem.password === password
+      );
+      res.cookie("cookieName", JSON.stringify(userLogin), {
+        maxAge: 3000,
+        httpOnly: true,
+      });
+      res.send({ ok: "Hello Raziel" });
+    } else {
+      throw new Error("wrong email or password");
+    }
+    app.get("/getCookie", function (req, res) {
+      try {
+        const { cookieName } = req.cookies;
+        const cookie = JSON.parse(cookieName);
+        res.send(cookie);
+      } catch (e) {
+        res.status(500).send({ error: `${e.message}` });
+      }
+    });
+  } catch (e) {
+    console.error(e);
+  }
+});
 
-// app.post("/signUp", (req, res) => {
-//     const { name,password } = req.body;
-//     const users = readUserData();
-//     const addUser = {
-      
-//       name: name,
-//       password: password,
-     
-//     };
-//     users.push(addUser);
-//     fs.writeFileSync("./userData.json", JSON.stringify(users));
-//     res.send(users);
-//   });
-  
-
-app.listen(port, () => { console.log('Server listen on port', port) })
+app.get("/getCookie", function (req, res) {
+  try {
+    const { cookieName } = req.cookies;
+    const cookie = JSON.parse(cookieName);
+    res.send(cookie);
+  } catch (e) {
+    res.status(500).send({ error: `${e.message}` });
+  }
+});
+app.listen(port, () => {
+  console.log("Server listen on port", port);
+});
