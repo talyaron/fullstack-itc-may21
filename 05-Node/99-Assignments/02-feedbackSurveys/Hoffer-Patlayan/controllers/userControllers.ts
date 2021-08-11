7a3c5f8a3aafd6b44131afc511af4adaf5d14fc3
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2021-08-11 19:09:35 +0300

    ip

--- 05-Node/99-Assignments/02-feedbackSurveys/Hoffer-Patlayan/controllers/userControllers.ts
+++ 05-Node/99-Assignments/02-feedbackSurveys/Hoffer-Patlayan/controllers/userControllers.ts
@@ -1,10 +1,12 @@
 export function getUsers(req: any, res: any) {
-    console.log(req)
+    // console.log(req)
 
-    const { cookieName } = req.cookies;
-    console.log(JSON.stringify(req.cookies));
+    // const { cookieName } = req.cookies;
+    // console.log(JSON.stringify(req.cookies));
     
-    const cookie = JSON.parse(cookieName);
+    // const cookie = JSON.parse(cookieName);
+    console.log(req.cookies);
+    
 
-    res.send(cookie);
+    res.send({ok:"okay"});
 };  
