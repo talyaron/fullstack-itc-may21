6be4479594cd3b173565da3c85f1dd8f3071322c
Jony Nisen <jnisen@gmail.com>
2021-08-11 19:33:25 +0300

    change

--- 05-Node/99-Assignments/02-feedbackSurveys/Jonathan-Raziel/routes/routesSurveys.ts
+++ 05-Node/99-Assignments/02-feedbackSurveys/Jonathan-Raziel/routes/routesSurveys.ts
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 import {addSurveys} from '../controllers/controllersSurvey'
+//import {deleteSurveys} from '../controllers/controllersSurvey'
 
 router.post('/add', addSurveys)
+//router.delete('/user/:id/:email', deleteSurveys)
 
 
 
