1ba9e097b43f753c28408b6f9b7aa0de569e30bc
tomashoffer <tomashoffer96@gmail.com>
2021-08-12 00:05:06 +0300

    cvxcv

--- 05-Node/99-Assignments/02-feedbackSurveys/Hoffer-Patlayan/models/suverys.ts
+++ 05-Node/99-Assignments/02-feedbackSurveys/Hoffer-Patlayan/models/suverys.ts
@@ -1,6 +1,10 @@
-export class Suvery {   
+export class Suvery {  
+    title: string;
+    email: string;
+    question: Array<any>;
+    id: string; 
     constructor() {
-    
+        
     }
 }
 
