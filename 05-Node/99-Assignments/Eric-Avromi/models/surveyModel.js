7e027d0cc781255fbfd8514577ac6722b410e573
Eric Kobrinsky <78773449+erickobrinsky@users.noreply.github.com>
2021-08-14 23:26:36 +0300

    ..

--- 05-Node/99-Assignments/Eric-Avromi/models/surveyModel.js
+++ 05-Node/99-Assignments/Eric-Avromi/models/surveyModel.js
@@ -13,10 +13,7 @@ function addSurvey(newSurvey) {
     console.log("hello surbey ");
     const allSurveys = getAllSurveys();
     console.log("hello surbey 3 ");
-    // const task = {
-    //     title,
-    //     id: uuidv4()
-    // }
+  
    
     allSurveys.push(newSurvey);
     
@@ -33,88 +30,5 @@ function getAllSurveys() {
 }
 
 exports.addSurvey = addSurvey
-
-
-
-
-//splice routes and models for each .json (one for surveys and other for questions)
-
-
-
-
-
-
-
-
-// function addQuestion(title) { 
-
-//     const allTasks = getAllTasks();
-//     const task = {
-//         title,
-//         id: uuidv4()
-//     }
-
-//     allTasks.push(task);
-
-//     fs.writeFileSync(filePath, JSON.stringify(allTasks));
-//     return allTasks
-// }
-
-
-// function deleteSurvey(id) { 
-
-//     const allTasks = getAllTasks();
-
-//     const filteredTasks = allTasks.filter(task => task.id !== id)
-
-//     fs.writeFileSync(filePath, JSON.stringify(filteredTasks));
-//     return filteredTasks
-// }
-
-
-
-// function deleteQuestion(id) { 
-
-//     const allTasks = getAllTasks();
-
-//     const filteredTasks = allTasks.filter(task => task.id !== id)
-
-//     fs.writeFileSync(filePath, JSON.stringify(filteredTasks));
-//     return filteredTasks
-// }
-
-
-
-// function editQuestion(id, newTitle) { 
-//     console.log("inside model");
-//     const allTasks = getAllTasks();
-//     const taskToEdit = allTasks.filter(task => task.id === id); 
-//     taskToEdit[0].title = newTitle;
-//     console.log(taskToEdit)
-//     fs.writeFileSync(filePath, JSON.stringify(allTasks));
-//     return allTasks
-// }
-
-
-// function editSurvey(id, newTitle) { 
-//     console.log("inside model");
-//     const allTasks = getAllTasks();
-//     const taskToEdit = allTasks.filter(task => task.id === id); 
-//     taskToEdit[0].title = newTitle;
-//     console.log(taskToEdit)
-//     fs.writeFileSync(filePath, JSON.stringify(allTasks));
-//     return allTasks
-// }
-
-
-
-
-// exports.getAllSurveys = getAllSurveys
-// exports.deleteSurvey = deleteSurvey
-// exports.editSurvey = editSurvey 
-// exports.addQuestion = addQuestion
-// exports.editQuestion = editQuestion
-// exports.deleteQuestion = deleteQuestion
-
 
 
