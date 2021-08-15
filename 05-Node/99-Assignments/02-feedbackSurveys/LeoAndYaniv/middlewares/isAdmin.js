08b5ba48ebdd480e52175e8608f20f258a107ab9
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-08-15 14:29:39 +0300

    .

--- 05-Node/99-Assignments/02-feedbackSurveys/LeoAndYaniv/middlewares/isAdmin.js
+++ 05-Node/99-Assignments/02-feedbackSurveys/LeoAndYaniv/middlewares/isAdmin.js
@@ -1,13 +1,25 @@
-// export {};
-// import { Users } from "../models/users";
-// export function isAdmin(req, res, next) {
-//   try {
-//       if () {
-//         next();
-//       } else {
-//         res.status(401).send({error:'You are not authorized to open this page'});
-//       }
-// } catch (error) {
-//     res.status(500).send(error.message);
-//   }
-// }
+"use strict";
+exports.__esModule = true;
+exports.isAdmin = void 0;
+var users_1 = require("../models/users");
+function isAdmin(req, res, next) {
+    try {
+        var isUserAdmin = false;
+        var allUsers = new users_1.Users();
+        var userIndex = allUsers.users.findIndex(function (user) { return user.email === req.email; });
+        if ((userIndex !== -1)) { // user exists
+            var userPass = allUsers.users[userIndex].password;
+            if (userPass) { // have password => admin
+                isUserAdmin = true;
+                next();
+            }
+        }
+        else {
+            res.status(401).send({ error: 'You are not authorized to open this page' });
+        }
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+exports.isAdmin = isAdmin;
--- 05-Node/99-Assignments/02-feedbackSurveys/LeoAndYaniv/routes/routeSurveys.js
+++ 05-Node/99-Assignments/02-feedbackSurveys/LeoAndYaniv/routes/routeSurveys.js
@@ -6,13 +6,11 @@ var UserCookie_1 = require("../middlewares/UserCookie");
 var controlerSurvey_1 = require("../controlers/controlerSurvey");
 //When the user click to start a new survey I call this method
 router.route('/survey/:uuid')
-    .post(UserCookie_1.userCookieRead, controlerSurvey_1.newSurvey) // for admins
-    .get(UserCookie_1.userCookieRead, controlerSurvey_1.getSurveys) // for admins
-["delete"](UserCookie_1.userCookieRead, controlerSurvey_1.deleteSurvey); // for admins
+    .post(UserCookie_1.userCookieRead, /* isAdmin,*/ controlerSurvey_1.newSurvey)
+    .get(UserCookie_1.userCookieRead, /* isAdmin,*/ controlerSurvey_1.getSurveys)["delete"](UserCookie_1.userCookieRead, /* isAdmin,*/ controlerSurvey_1.deleteSurvey);
 router.route('/question/:uuid/:qUuid')
-    .post(controlerSurvey_1.addQuestion) // for admins
-    .put(UserCookie_1.userCookieRead, controlerSurvey_1.editQuestion) // for admins
-["delete"](UserCookie_1.userCookieRead, controlerSurvey_1.deleteQuestion); // for admins
+    .post(UserCookie_1.userCookieRead, /* isAdmin,*/ controlerSurvey_1.addQuestion)
+    .put(UserCookie_1.userCookieRead, /* isAdmin,*/ controlerSurvey_1.editQuestion)["delete"](UserCookie_1.userCookieRead, /* isAdmin,*/ controlerSurvey_1.deleteQuestion);
 router.route('/questions/:uuid')
     .get(controlerSurvey_1.getQuestionsSurvey)
     .put(controlerSurvey_1.updateQuestionsSurvey);
