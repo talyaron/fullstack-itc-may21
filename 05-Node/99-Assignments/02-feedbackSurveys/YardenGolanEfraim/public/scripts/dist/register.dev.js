836361edf0609e298bee10fa7723965ac5ede79b
Efraim Dimbleby <82100352+EfraimDim@users.noreply.github.com>
2021-08-15 20:35:24 +0300

    sdf

--- 05-Node/99-Assignments/02-feedbackSurveys/YardenGolanEfraim/public/scripts/dist/register.dev.js
+++ 05-Node/99-Assignments/02-feedbackSurveys/YardenGolanEfraim/public/scripts/dist/register.dev.js
@@ -1,47 +1,28 @@
 "use strict";
 
 function handleRegister(ev) {
-  var username, password, email, result;
-  return regeneratorRuntime.async(function handleRegister$(_context) {
-    while (1) {
-      switch (_context.prev = _context.next) {
-        case 0:
-          ev.preventDefault();
-          _context.prev = 1;
-          username = ev.target.elements.username.value;
-          password = ev.target.elements.password.value;
-          email = ev.target.elements.email.value;
-          _context.next = 7;
-          return regeneratorRuntime.awrap(axios.post('/users', {
-            username: username,
-            password: password,
-            email: email
-          }));
+  ev.preventDefault();
 
-        case 7:
-          result = _context.sent;
+  try {
+    var username = ev.target.elements.username.value;
+    var password = ev.target.elements.password.value;
+    var email = ev.target.elements.email.value;
+    var result = axios.post('/users', {
+      username: username,
+      password: password,
+      email: email
+    });
 
-          if (result.data === "Email already taken!") {
-            alert("".concat(result.data));
-          } else {
-            alert("Register succesful, ".concat(username, "!"));
-          }
-
-          ev.target.reset();
-          _context.next = 15;
-          break;
-
-        case 12:
-          _context.prev = 12;
-          _context.t0 = _context["catch"](1);
-          console.error(_context.t0);
-
-        case 15:
-        case "end":
-          return _context.stop();
-      }
+    if (result.data === "Email already taken!") {
+      alert("".concat(result.data));
+    } else {
+      alert("Register succesful, ".concat(username, "!"));
     }
-  }, null, null, [[1, 12]]);
+
+    ev.target.reset();
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 document.querySelector(".form-field").addEventListener("submit", handleRegister);
