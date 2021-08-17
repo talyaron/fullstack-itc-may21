f50adcfaff4a24b851a19b2884458a22313beeb4
leomza <leo_mza1991@hotmail.com>
2021-08-17 17:39:00 +0300

    Added comments, clean the code

--- 05-Node/99-Assignments/02-feedbackSurveys/LeoAndYaniv/middlewares/userCookie.ts
+++ 05-Node/99-Assignments/02-feedbackSurveys/LeoAndYaniv/middlewares/userCookie.ts
@@ -21,15 +21,18 @@ export function userCookieRead(req, res, next) {
 
   export function userCookieWrite(req, res, next) {
     try {
+      //Get the information from the body
       const { username, email } = req.body;
       if ((!username) || (!email)) throw new Error("User details processing issues");
 
+      //Here I set the cookie
       const cookieToWrite: string = JSON.stringify({ username, email });
       res.cookie("userDetails", cookieToWrite, { maxAge: 900000, httpOnly: true });
 
       req.username = username;
       req.email = email;
 
+      //"Next" means that I will continue with the Route
       next();
 
     } catch (error) {
