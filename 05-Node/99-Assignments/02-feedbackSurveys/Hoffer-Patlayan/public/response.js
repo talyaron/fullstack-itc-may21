3b72e1fa3746323bc9a7a5a7211b4a67b4fe58cd
leomza <leo_mza1991@hotmail.com>
2021-08-17 17:39:33 +0300

    Changes

--- 05-Node/99-Assignments/02-feedbackSurveys/Hoffer-Patlayan/public/response.js
+++ 05-Node/99-Assignments/02-feedbackSurveys/Hoffer-Patlayan/public/response.js
@@ -2,12 +2,10 @@
 const renderWelcome = (name) => {
   const root = document.querySelector(".root");
 
-  const renderIt = `<h5>Survey conducted by user ${name}</h5>`;
+  const renderIt = `<h5 style='margin-right: 10rem; color: white;'>Survey conducted by user ${name}</h5>`;
   root.innerHTML = renderIt;
 };
 renderWelcome();
-
-
 
 window.onload = function () {
   var params = new URLSearchParams(window.location.search);
@@ -29,7 +27,6 @@ async function getResp() {
 }
 getResp();
 
-
 const render = (survey) => {
   const rootTtile = document.querySelector(".rootTitle");
   const rootQuestion = document.querySelector(".rootQuestion");
@@ -43,31 +40,23 @@ const render = (survey) => {
   let htmlQuestions = "";
   arrPregs.forEach((preg) => {
     htmlQuestions += `
-        <div class="respondses">
-        <h3>${preg}</h3>
+        <div class="questions">
+        <h3 class="questions_title">${preg}</h3>
         <select name="responds" class="form-select" id="respondses">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
+            <option class="form-option" value="1">1</option>
+            <option class="form-option" value="2">2</option>
+            <option class="form-option" value="3">3</option>
+            <option class="form-option" value="4">4</option>
+            <option class="form-option" value="5">5</option>
         </select>
-        </div>
-    
-    
-        
-        `;
+        </div>`;
   });
   rootQuestion.innerHTML = htmlQuestions;
 };
-
-const handleSubmit =  (event) => {
- 
+const handleSubmit =  (event) => { 
   event.preventDefault();
-
   const params = new URLSearchParams(window.location.search);
   const id = params.get('id');
-
   const arr = []
   const responds = event.target.elements.responds;
   responds.forEach(element => {
@@ -77,19 +66,25 @@ const handleSubmit =  (event) => {
   axios({
     method: "post",
     url: `/response/postResponds`,
-    data: {
-      id,
-      arr,
-    },
-  })
-    .then(({ data }) => {
+    data: {id, arr},
+  }).then(({ data }) => {
       return data;
-    })
-
-    .catch((err) => {
+    }).catch((err) => {
       console.log(err);
     });
 };
 async function sendRes(newRes){
   const response = await axios.post(`/response/postResponds`, newRes);
+}
+const btn = document.querySelector('.btnSubmit');
+btn.addEventListener('click', alertSubmit)
+
+function alertSubmit(){
+  const root = document.querySelector('.alert');
+  let html = '<div class="alert alert-info" role="alert">Thank you, your survey was sent!</div>';
+  root.innerHTML = html;
+  setTimeout(() =>{
+    html = '';
+    root.innerHTML = html;
+  }, 3000)
 }
--- 05-Node/99-Assignments/02-feedbackSurveys/Hoffer-Patlayan/public/survey.js
+++ 05-Node/99-Assignments/02-feedbackSurveys/Hoffer-Patlayan/public/survey.js
@@ -9,7 +9,7 @@ const getUser = async () => {
 const render = (name) => {
   const root = document.querySelector(".root");
 
-  const renderIt = `<h5>Welcome ${name}!</h5>`;
+  const renderIt = `<h5 style="color: white;">Welcome ${name}!</h5>`;
   root.innerHTML = renderIt;
 };
 getUser();
@@ -32,10 +32,10 @@ function renderSurvay(surveyData){
   let html = "";
   surveyData.forEach((survey) => {
     html += `   <div class="survey_info">
-    <a class="title_survey" onclick='selectedSurv("${survey.id}")' href="selectedSurvey.html"><h3 class="title_surveys">${survey.title}</h3></a>
+    <a class="title_survey" onclick='selectedSurv("${survey.id}")' href="selectedSurvey.html"><h4 class="title_surveys">Title: ${survey.title}</h4></a>
     <div class="survey_icons">
-      <a style="cursor: pointer;" data-bs-toggle="modal" data-bs-target="#exampleModal"><i onclick='shareId("${survey.id}")' class="icon_share fas fa-share-alt-square fa-lg"></i></a>
-      <a style="cursor: pointer"><i onclick='deleteSurvey("${survey.id}")' class="icon_delete fas fa-trash fa-lg"></i></a>
+      <a style="cursor: pointer;" data-bs-toggle="modal" data-bs-target="#exampleModal"><i onclick='shareId("${survey.id}")' class="icon_share fas fa-share-alt-square fa-lg" style="cursor: pointer; color: black;"></i></a>
+      <a class="iconDelete" style="cursor: pointer; color: black;"><i onclick='deleteSurvey("${survey.id}")' class="icon_delete fas fa-trash fa-lg"></i></a>
     </div>
   </div>`;
   });root.innerHTML = html;
@@ -43,8 +43,15 @@ function renderSurvay(surveyData){
 
 function deleteSurvey(id){
   const getUser = axios.post(`/survey/deleteSurvey/${id}`);
-  window.location.reload();
   getLogInUser()
+  const root = document.querySelector('.alert_delete');
+  let html = '<div style="width: 30%; text-align: center; margin-top: 2rem;" class="container alert alert-danger" role="alert">Survey deleted!</div>';
+  root.innerHTML = html;
+  setTimeout(() =>{
+    html = '';
+    root.innerHTML = html;
+  }, 3000)
+  
 }
 
 async function selectedSurv(id){
@@ -69,4 +76,12 @@ function modalRender(id){
 
 async function resultsId(id){
   const gerServ = await axios.get(`/survey/results`, id);
+}
+
+// ALERT DELETE
+// const deleteBtn = document.querySelector('.icon_delete');
+// deleteBtn.addEventListener('click', alertSubmit)
+
+function alertSubmit(){
+ 
 }
