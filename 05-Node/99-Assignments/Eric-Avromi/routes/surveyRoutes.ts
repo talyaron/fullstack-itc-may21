d7f059cdc7dcba7c225cf58c4324a50b6c50af50
Eric Kobrinsky <78773449+erickobrinsky@users.noreply.github.com>
2021-08-18 14:29:50 +0300

    updates

--- 05-Node/99-Assignments/Eric-Avromi/routes/surveyRoutes.ts
+++ 05-Node/99-Assignments/Eric-Avromi/routes/surveyRoutes.ts
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-import {addSurveys,deleteSurveys,getUniqueId,getPreviousSurvey,getUniqueIdQuestions} from '../controllers/surveyControllers'
+import {addSurveys,deleteSurveys,getUniqueId,getPreviousSurvey,} from '../controllers/surveyControllers'
 
 
 router.post('/add', addSurveys)
 router.get('/surveys', getUniqueId)
-router.get('/questions', getUniqueIdQuestions)
+
 router.get('/getSurvey/:id',getPreviousSurvey)
 router.delete('/user/:id/:email', deleteSurveys)
 
