ab87812bd1b13850ca09f6550992dc6fe1d130f7
Efraim Dimbleby <82100352+EfraimDim@users.noreply.github.com>
2021-08-29 18:23:42 +0300

    dfs

--- 05-Node/99-Assignments/04-Online-Store/Efraim/routes/adminUserRoute.js
+++ 05-Node/99-Assignments/04-Online-Store/Efraim/routes/adminUserRoute.js
@@ -29,9 +29,9 @@ router.delete(
     adminUserController.deleteProduct
 )
 
-// don't know really why but if the upload.single wasn't the first middleware then the 
-// validateBody was breaking the code.. ideally I wanted upload.single last so the image isn't saved if 
-// one of the other middlewares ended the post
+// don't know really why but if the upload.single wasn't before the 
+// validateBody the code was breaking.. ideally I wanted upload.single last so the image isn't saved if 
+// the valiadte body breaks
 
 router.post(
     '/addProduct',
--- 05-Node/99-Assignments/04-Online-Store/Efraim/routes/dist/adminUserRoute.dev.js
+++ 05-Node/99-Assignments/04-Online-Store/Efraim/routes/dist/adminUserRoute.dev.js
@@ -23,9 +23,9 @@ var _require2 = require('../middleware/middleware'),
 var Schemas = require('../schemas/allSchemas');
 
 router.put('/updateProducts', validateBody(Schemas.updateProductsSchemaAJV), authorization, checkAdminForAllReq, adminUserController.updateProducts);
-router["delete"]('/deleteProduct/:productID', authorization, checkAdminForAllReq, adminUserController.deleteProduct); // don't know really why but if the upload.single wasn't the first middleware then the 
-// validateBody was breaking the code.. ideally I wanted upload.single last so the image isn't saved if 
-// one of the other middlewares ended the post
+router["delete"]('/deleteProduct/:productID', authorization, checkAdminForAllReq, adminUserController.deleteProduct); // don't know really why but if the upload.single wasn't before the 
+// validateBody the code was breaking.. ideally I wanted upload.single last so the image isn't saved if 
+// the valiadte body breaks
 
 router.post('/addProduct', authorization, checkAdminForAllReq, upload.single('image'), validateBody(Schemas.addProductSchemaAJV), adminUserController.addProduct);
 module.exports = router;
