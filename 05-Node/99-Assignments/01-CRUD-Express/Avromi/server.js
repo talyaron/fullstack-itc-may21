ebfdf8eb1907a39f52379e856a7597cabb61c013
Avromi <avromirussell@gmail.com>
2021-08-29 19:08:17 +0300

    changes

--- 05-Node/99-Assignments/01-CRUD-Express/Avromi/server.js
+++ 05-Node/99-Assignments/01-CRUD-Express/Avromi/server.js
@@ -4,9 +4,7 @@ const port = process.env.PORT || 3000;
 const fs = require('fs'); //YS: This is not being used.
 
 const taskRouter = require('./routes/tasksRoute.js')
-const {
-    v4: uuidv4 
-} = require('uuid');  //YS: This is not being used.
+
 
 app.use(express.json())
 app.use(express.static('public'))
