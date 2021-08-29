a38ce29906e3a06e7f9bb7c417ff35f09e40874c
Tal <tal.yaron@gmail.com>
2021-08-29 20:58:09 +0300

    walla

--- 05-Node/99-Assignments/03-checkup-01/tal/controlers/controlerStudents.ts
+++ 05-Node/99-Assignments/03-checkup-01/tal/controlers/controlerStudents.ts
@@ -1,5 +1,34 @@
 export { };
 
-export function getStudents (req, res) {
-    res.send({getStudents:true});
+import { Students, Student } from '../model/students';
+
+const students = new Students();
+
+export function getStudents(req, res) {
+    res.send({ students: students.studentsArray });
+}
+
+export function addStudent(req, res) {
+    try {
+        const { name } = req.body;
+        if (!name) throw new Error("No student was transferred");
+
+        const student = new Student(name);
+        console.log(student)
+        students.addStudent(student);
+        console.log('student.......');
+
+        res.send({ students: students.studentsArray });
+
+    } catch (error) {
+
+    }
+
+}
+
+export function getRandomStudents(req, res){
+    const number = req.body.number;
+    console.log('getRandomStudents')
+    const randomStudents = students.getRandomStudents(number);
+    res.send({students:randomStudents});
 }
--- 05-Node/99-Assignments/03-checkup-01/tal/routes/studentsRoute.js
+++ 05-Node/99-Assignments/03-checkup-01/tal/routes/studentsRoute.js
@@ -2,6 +2,13 @@
 exports.__esModule = true;
 var express = require('express');
 var router = express.Router();
-var getStudents = require('../controlers/controlerStudents').getStudents;
-router.get('/all_students', getStudents);
+var _a = require('../controlers/controlerStudents'), getStudents = _a.getStudents, addStudent = _a.addStudent, getRandomStudents = _a.getRandomStudents;
+router
+    .get('/all_students', getStudents)
+    .post('/random_students', isW, getRandomStudents)
+    .post('/add_student', addStudent);
+function isW(req, res, next) {
+    console.log('working');
+    next();
+}
 module.exports = router;
