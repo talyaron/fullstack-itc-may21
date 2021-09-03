98ec0548ae1d765b56464ede1366dd06f1a43438
Yaniv <79939950+YanivAf@users.noreply.github.com>
2021-09-03 11:45:56 +0300

    done

---
+++ 05-Node/99-Assignments/06-chekpoint/Yaniv/public/dist/helper-errors.js
@@ -0,0 +1,37 @@
+window.axios.interceptors.response.use(function (response) {
+    return response;
+}, function (error) {
+    var title;
+    var text = error.response.data.message;
+    switch (error.response.status) {
+        case 401:
+            title = "Authorization Issue";
+            break;
+        case 403:
+            title = "Permissions Issue";
+            break;
+        case 404:
+            title = "Not Found";
+            break;
+        case 409:
+            title = "Conflict";
+            if (!text)
+                text = error.response.data;
+            break;
+        case 500:
+            title = "Server Request Issue";
+            text = error.response.data;
+            break;
+    }
+    if (error.response.status) {
+        swal({
+            title: "Ops.. " + title,
+            text: text,
+            icon: "warning",
+            button: "Try again"
+        });
+    }
+    else {
+        return Promise.reject(error);
+    }
+});
