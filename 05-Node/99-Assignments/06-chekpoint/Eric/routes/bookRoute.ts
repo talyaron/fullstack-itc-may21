52b34729b29ae03f59685ba2a894af705835e604
Eric Kobrinsky <78773449+erickobrinsky@users.noreply.github.com>
2021-09-03 11:54:20 +0300

    token

--- 05-Node/99-Assignments/06-chekpoint/Eric/routes/bookRoute.ts
+++ 05-Node/99-Assignments/06-chekpoint/Eric/routes/bookRoute.ts
@@ -6,8 +6,11 @@ import {addBook, getBook, searchByTitle} from '../controllers/bookControllers'
 import {isTitleExist} from '../middleware/validationModel'
 import {validateBook} from '../middleware/validationSchemas'
 import { schemaBook } from '../schemas/allSchemas';
+import {writeCookie} from '../middleware/handleCookies'
 
-router.post('/addBook', validateBook(schemaBook) ,isTitleExist, addBook);
+
+
+router.post('/addBook', validateBook(schemaBook) ,isTitleExist, writeCookie, addBook);
 router.get('/getBooks', getBook)
 router.put('/searchByTitle', searchByTitle)
 
