1df84bcf8f0f2abdeebcf9eed6e4b7894e8ded69
leomza <leo_mza1991@hotmail.com>
2021-09-03 11:34:08 +0300

    Update Project Test

---
+++ 05-Node/99-Assignments/06-chekpoint/Leonardo/public/helperModals.js
@@ -0,0 +1,45 @@
+// Get the modal
+var modalUpload = document.getElementById("modalBook");
+// Get the button that opens the modal
+var buttonCreate = document.getElementById("buttonCreate");
+// Get the <span> element that closes the modal
+var closeUpload = document.getElementById("closeModal");
+// When the user clicks the button, open the modal
+if (buttonCreate) {
+    buttonCreate.addEventListener('click', openModal);
+}
+function openModal() {
+    try {
+        modalUpload.style.display = "block";
+        modalUpload.classList.add("showModal");
+    }
+    catch (error) {
+        console.error(error);
+    }
+    ;
+}
+;
+// When the user clicks on <span> (x), close the modal
+closeUpload.addEventListener('click', closeModal);
+function closeModal() {
+    try {
+        modalUpload.style.display = "none";
+    }
+    catch (error) {
+        console.error(error);
+    }
+    ;
+}
+;
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    try {
+        if (event.target === modalUpload) {
+            modalUpload.style.display = "none";
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+    ;
+};
